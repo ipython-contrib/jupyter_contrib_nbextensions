1c1e415a993ba0d73fffa2abf8a220b4e71cd9e3
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-01-18 17:41:35 +0100

    big cleaning

--- js/slide_meta.js
+++ js/slide_meta.js
@@ -53,8 +53,12 @@ var show_line = function(cell,line_number){
 }
 
 IPython = (function(IPython) {
+
+    /**
+     * @constructor
+     */
     var Presentation = function(){
-          this.ccell = 0;
+          this.ccell = 0
     }
 
     var is_cell_of_type = function(cell,type){
@@ -81,216 +85,218 @@ IPython = (function(IPython) {
     var is_marked_cell = is_slide
 
     Presentation.prototype.create_toolbar = function(){
-        var pt = $('<div/>').attr('id','toolbar_present').addClass('toolbar');
-        var that = this;
-        this.avc = $('<div/>').button({label:that.eta()});
-        pt.append(this.avc);
+        var that = this
+        this.progression = $('<div/>').button({label:that.eta()})
 
-        $('#maintoolbar').after(pt);
-        var ptoolbar = new IPython.ToolBar('#toolbar_present');
-        IPython.ptoolbar = ptoolbar;
-        ptoolbar.add_buttons_group([{label:'Stop', icon:'ui-icon-pause', callback:function(){that.pause()}}])
-        ptoolbar.add_buttons_group([{label:'Stop', icon:'ui-icon-stop', callback:function(){that.stop()}}])
+        var pt = $('<div/>').attr('id','toolbar_present')
+        pt.append(this.progression)
+
+        $('#maintoolbar').after(pt)
+        var ptoolbar = new IPython.ToolBar('#toolbar_present')
+
+
+        ptoolbar.add_buttons_group([{label:'Pause', icon:'ui-icon-pause', callback:function(){that.pause()}}])
+        ptoolbar.add_buttons_group([{label:'Stop' , icon:'ui-icon-stop' , callback:function(){that.stop() }}])
+
         ptoolbar.add_buttons_group([
                  {label:'Prev Slide', icon:'ui-icon-seek-prev', callback:function(){that.prev_group()}},
                  {label:'Next Slide', icon:'ui-icon-seek-next', callback:function(){that.next_group()}},
                                 ])
         ptoolbar.add_buttons_group([
-                 {label:'Step Next', icon:'ui-icon-play', callback:function(){that.next()}}
+                 {label:'Next', icon:'ui-icon-play', callback:function(){that.next()}}
                  ])
 
-        bind_remote('#toolbar_present');
+        bind_remote('#toolbar_present')
 
+        IPython.ptoolbar = ptoolbar
     }
 
-      Presentation.prototype.remove_toolbar = function(){
-          $('#toolbar_present').remove();
-      }
+    Presentation.prototype.remove_toolbar = function(){
+        $('#toolbar_present').remove()
+    }
 
-      Presentation.prototype.ngroups = function(){
-          var cells = IPython.notebook.get_cells();
-          var cnt =0
-          for( var i=0; i< cells.length;i++)
-              if(is_marked_cell(cells[i])) cnt++;
-          return cnt
-      }
+    // return the total number of slide
+    Presentation.prototype.ngroups = function(){
+        var cells = IPython.notebook.get_cells()
+        var cnt =0
+        for( var i=0; i< cells.length;i++)
+              if(is_marked_cell(cells[i])) cnt++
+        return cnt
+    }
 
-      Presentation.prototype.cgroups = function(){
-          var cells = IPython.notebook.get_cells();
-          var cnt =0
-          for( var i=0; i<= this.ccell ;i++)
-              if(is_marked_cell(cells[i])) cnt++;
-          return cnt
-      }
+    // return the number of the current slide
+    Presentation.prototype.cgroups = function(){
+        var cells = IPython.notebook.get_cells()
+        var cnt =0
+        for( var i=0; i<= this.ccell ;i++)
+            if(is_marked_cell(cells[i])) cnt++
+        return cnt
+    }
 
-      Presentation.prototype.eta = function(){
-          return this.cgroups()+'/'+this.ngroups()
-          }
+    // estimated time of arival
+    Presentation.prototype.eta = function(){
+        return this.cgroups()+'/'+this.ngroups()
+    }
 
-      Presentation.prototype.next_marked_cell_n = function()
-      {
-          for(var i=this.ccell+1; i< $('.cell').length; i++)
-          {
-              if(is_marked_cell(IPython.notebook.get_cell(i)))
-                { return i; }
-          }
-          return null;
-      }
+    // number of next marked cell, (artefact, deprecated)
+    Presentation.prototype.next_marked_cell_n = function() {
+        for(var i=this.ccell+1; i< $('.cell').length; i++) {
+            if(is_marked_cell(IPython.notebook.get_cell(i)))
+            { return i }
+        }
+        return null
+    }
 
-      Presentation.prototype.prev_marked_cell_n = function()
-      {
-          for(var i=this.ccell-1; i> 0; i--)
-          {
-              if(is_marked_cell(IPython.notebook.get_cell(i))){
-                 return i
-              }
-          }
-          return 0;
-      }
+    // number of prev marked cell, (artefact, deprecated)
+    Presentation.prototype.prev_marked_cell_n = function() {
+        for(var i=this.ccell-1; i> 0; i--) {
+            if(is_marked_cell(IPython.notebook.get_cell(i))){
+                return i
+            }
+        }
+        return 0
+    }
 
-      Presentation.prototype.start = function(){
-          this.restart();
-          this.resume();
-      }
+    Presentation.prototype.start = function(){
+        this.restart()
+        this.resume()
+    }
 
-      Presentation.prototype.restart = function(){
-          this.ccell = 0;
-          delete this.avc;
-      }
+    Presentation.prototype.restart = function(){
+        this.ccell = 0
+        delete this.progression
+    }
 
-      Presentation.prototype.resume = function(){
-          this.create_toolbar();
-          $('#menubar').addClass('pmode');
-          $('#pager_splitter').addClass('pmode');
-          $('#pager').addClass('pmode');
-          $('#menubar, #pager_splitter, #pager, #header,#maintoolbar').addClass('pmode');
-          $('#header').addClass('pmode');
-          $('#maintoolbar').addClass('pmode');
-          $('div#header').css('display','none');
-          IPython.layout_manager.do_resize();
-          $('.cell').fadeOut(tminus);
-          if(this.current_is_marked()){
-              $('.cell:nth('+this.ccell+')').fadeIn();
-          } else {
-              for( var i=this.prev_marked_cell_n() ; i<= this.ccell; i++){
-                  $('.cell:nth('+i+')').fadeIn();
-              }
-          }
-          var that=this;
-          if(this.avc != undefined)
-            $(this.avc).button('option','label',that.eta())
-          return this;
-      }
+    Presentation.prototype.resume = function(){
+        this.create_toolbar()
+        $('#menubar').addClass('pmode')
+        $('#pager_splitter').addClass('pmode')
+        $('#pager').addClass('pmode')
+        $('#menubar, #pager_splitter, #pager, #header,#maintoolbar').addClass('pmode')
+        $('#header').addClass('pmode')
+        $('#maintoolbar').addClass('pmode')
+        $('div#header').css('display','none')
+        IPython.layout_manager.do_resize()
+        $('.cell').fadeOut(tminus)
+        if(this.current_is_marked()){
+            $('.cell:nth('+this.ccell+')').fadeIn()
+        } else {
+            for( var i=this.prev_marked_cell_n() ; i<= this.ccell; i++){
+                $('.cell:nth('+i+')').fadeIn()
+            }
+        }
+        var that=this
+        if(this.progression != undefined)
+        $(this.progression).button('option','label',that.eta())
+        return this
+    }
 
-      Presentation.prototype.stop = function(){
-              this.restart();
-              this.pause();
-      }
+    Presentation.prototype.stop = function(){
+        this.restart()
+        this.pause()
+    }
 
-      Presentation.prototype.pause = function(){
-          $('.cell').fadeIn();
-          $('.pmode').removeClass('pmode');
-          $('div#header').css('display','block');
-          $('div#notebook').removeClass('pmode');
-          IPython.layout_manager.do_resize();
-          this.remove_toolbar();
-      }
+    Presentation.prototype.pause = function(){
+        $('.cell').fadeIn()
+        $('.pmode').removeClass('pmode')
+        $('div#header').css('display','block')
+        $('div#notebook').removeClass('pmode')
+        IPython.layout_manager.do_resize()
+        this.remove_toolbar()
+    }
 
-      Presentation.prototype.next = function(){
-          var current_cell_number = this.ccell
-          var number_next_cell = this.ccell+1
-          this.ccell = this.ccell+1
+    Presentation.prototype.next = function(){
+        var current_cell_number = this.ccell
+        var number_next_cell = this.ccell+1
+        this.ccell = this.ccell+1
 
-          var that = this;
-          if(this.ccell >= $('.cell').length ){
-              this.restart();
-              this.pause();
-              return;
-          }
-          var next_cell = IPython.notebook.get_cell(number_next_cell)
-          var look_ahead_cell = IPython.notebook.get_cell(number_next_cell+1)
+        var that = this
+        if(this.ccell >= $('.cell').length ){
+            this.restart()
+            this.pause()
+            return
+        }
+        var next_cell = IPython.notebook.get_cell(number_next_cell)
+        var look_ahead_cell = IPython.notebook.get_cell(number_next_cell+1)
 
-          if(is_marked_cell(next_cell)){
-              $('.cell').fadeOut(tminus);
-              setTimeout(function(){$('.cell:nth('+number_next_cell+')').fadeIn(tminus)},tplus);
-          } else {
-              if( !is_skip(next_cell) && !is_notes(next_cell) ){
-                setTimeout(function(){$('.cell:nth('+number_next_cell+')').fadeIn(tminus)},tplus);
-              }
-          }
-          $(this.avc).button('option','label',that.eta())
+        if(is_marked_cell(next_cell)){
+            $('.cell').fadeOut(tminus)
+            setTimeout(function(){$('.cell:nth('+number_next_cell+')').fadeIn(tminus)},tplus)
+        } else {
+            if( !is_skip(next_cell) && !is_notes(next_cell) ){
+                setTimeout(function(){$('.cell:nth('+number_next_cell+')').fadeIn(tminus)},tplus)
+            }
+        }
+        $(this.progression).button('option','label',that.eta())
 
-          if(is_undefined(look_ahead_cell) || is_skip(look_ahead_cell) || is_notes(look_ahead_cell)){
-              console.log('next...')
-              this.next()
-          }
-      }
+        if(is_undefined(look_ahead_cell) || is_skip(look_ahead_cell) || is_notes(look_ahead_cell)){
+            console.log('next...')
+            this.next()
+        }
+    }
 
-      Presentation.prototype.next_group = function(){
-          this.ccell = this.next_marked_cell_n();
-          var that = this;
-          $('.cell').fadeOut(tminus);
-          setTimeout(function(){
-              $('.cell:nth('+that.ccell+')').fadeIn(tminus)
-                  },tplus);
-          $(this.avc).button('option','label',that.eta())
-      }
+    Presentation.prototype.next_group = function(){
+        this.ccell = this.next_marked_cell_n()
+        var that = this
+        $('.cell').fadeOut(tminus)
+        setTimeout(function(){
+            $('.cell:nth('+that.ccell+')').fadeIn(tminus)
+        },tplus)
+        $(this.progression).button('option','label',that.eta())
+    }
 
-      Presentation.prototype.prev_group = function(){
-          this.ccell = this.prev_marked_cell_n();
-          var that = this
-          $('.cell').fadeOut(tminus);
-          setTimeout(function(){$('.cell:nth('+that.ccell+')').fadeIn(tminus)},tplus);
-          $(this.avc).button('option','label',that.eta())
-      }
+    Presentation.prototype.prev_group = function(){
+        this.ccell = this.prev_marked_cell_n()
+        var that = this
+        $('.cell').fadeOut(tminus)
+        setTimeout(function(){$('.cell:nth('+that.ccell+')').fadeIn(tminus)},tplus)
+        $(this.progression).button('option','label',that.eta())
+    }
 
-      Presentation.prototype.is_n_marked = function(n){
-          return is_marked_cell(IPython.notebook.get_cell(n))
-      }
+    Presentation.prototype.is_n_marked = function(n){
+        return is_marked_cell(IPython.notebook.get_cell(n))
+    }
 
-      Presentation.prototype.current_is_marked = function(n){
-          return is_marked_cell(IPython.notebook.get_cell(this.ccell));
-      }
+    Presentation.prototype.current_is_marked = function(n){
+        return is_marked_cell(IPython.notebook.get_cell(this.ccell))
+    }
 
-      Presentation.prototype.prev = function(){
-          if(is_marked_cell(IPython.notebook.get_cell(this.ccell))){
-              var pmcell = this.prev_marked_cell_n();
-              $('.cell').fadeOut(tminus);
-              for( var i=pmcell; i< this.ccell ; i++ ){
+    Presentation.prototype.prev = function(){
+        if(is_marked_cell(IPython.notebook.get_cell(this.ccell))){
+            var pmcell = this.prev_marked_cell_n()
+            $('.cell').fadeOut(tminus)
+            for( var i=pmcell; i< this.ccell ; i++ ){
                   (function(val){
                       return function(){
                                   setTimeout( function(){
                                       $('.cell:nth('+val+')').fadeIn(tminus)
-                                      },tplus);
+                                      },tplus)
                       }
-                   })(i)();
+                   })(i)()
               }
           } else {
-              $('.cell:nth('+this.ccell+')').fadeOut(tminus);
+              $('.cell:nth('+this.ccell+')').fadeOut(tminus)
           }
-          this.ccell = this.ccell -1;
-          return this;
+          this.ccell = this.ccell -1
+          return this
       }
 
-      IPython.Presentation = Presentation;
-      return IPython;
+      IPython.Presentation = Presentation
+      return IPython
 
-})(IPython);
+})(IPython)
 
-//$('body').append($('<style/>').text('.pmode{ display: none !important }'));
+//$('body').append($('<style/>').text('.pmode{ display: none !important }'))
 //
-IPython.slideshow = new IPython.Presentation();
+IPython.slideshow = new IPython.Presentation()
 
-var sid = 'start_pmode'
-if(($('#'+sid)).length == 0) {
-    IPython.toolbar.add_buttons_group([
-        {
-          'label'   : 'Start/Resume Slideshow',
-          'icon'    : 'ui-icon-image',
-          'callback': function(){IPython.slideshow.resume()},
-          'id'      : sid
-        },
-    ])
-}
+IPython.toolbar.add_buttons_group([
+    {
+        'label'   : 'Start/Resume Slideshow',
+        'icon'    : 'ui-icon-image',
+        'callback': function(){IPython.slideshow.resume()},
+        'id'      : 'start_pmode'
+    },
+])
 
-console.log('Live slideshow extension correctly loaded');
+console.log('Live slideshow extension correctly loaded')
