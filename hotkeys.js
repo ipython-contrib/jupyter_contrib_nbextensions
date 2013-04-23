69facf46082d41761bbbea9874b36964ac66ed61
Juergen Hasch <python@elbonia.de>
2013-04-23 18:54:27 +0200

    Add global hotkeys for notebook

--- hotkeys.js
+++ hotkeys.js
@@ -6,11 +6,13 @@
 //----------------------------------------------------------------------------
 
 //============================================================================
-// Hotkey extension - add some additional hotkeys to a codecell
+// Hotkey extension - add some additional hotkeys
+// All cells:
 // PGUP       - select previous cell
-// PGDOWN     - select previous cell
-// HOME       - select first cell
-// END        - select last cell
+// PGDOWN     - select following cell
+// CTRL+HOME  - select first cell
+// CTRL+END   - select last cell
+// Codecells:
 // CTRL+ENTER - execute all following codecells
 // SHIFT-TAB  - reduce TAB indent
 //============================================================================
@@ -23,37 +25,7 @@ var key   = IPython.utils.keycodes;
  *
  * @return {Boolean} returns true if hotkey is found
  */
-intercept_codemirror_keyevent = function (editor, event) {
-    if (event.type == 'keydown' && event.which == key.PGUP) {
-        IPython.notebook.select_prev();
-        return true;
-    }; 
-
-    if (event.type == 'keydown' && event.which == key.UPARROW && event.ctrlKey) {
-        IPython.notebook.select_prev();
-        return true;
-    }; 
-    
-    if (event.type == 'keydown' && event.which == key.PGDOWN) {
-        IPython.notebook.select_next();
-        return true;
-    };
-
-    if (event.type == 'keydown' && event.which == key.DOWNARROW && event.ctrlKey) {
-        IPython.notebook.select_next();
-        return true;
-    };
-    
-    if (event.type == 'keydown' && event.which == key.END && event.ctrlKey) {
-        var ncells = IPython.notebook.ncells();
-        IPython.notebook.select(ncells-1);
-        return true;
-    };
-
-    if (event.type == 'keydown' && event.which == key.HOME && event.ctrlKey) {
-        IPython.notebook.select(0);
-        return true;
-    };
+var intercept_codemirror_keyevent = function (editor, event) {
 
     if (event.type == 'keydown' && event.which == key.TAB && event.shiftKey) {
         var obj = editor.getCursor("start");
@@ -70,10 +42,42 @@ intercept_codemirror_keyevent = function (editor, event) {
 }
 
 /**
+ * Additional hotkeys for notebook
+ *
+ * @return {Boolean} return false to stop further key handling in notebook.js 
+ */
+var document_keydown = function(event) {
+
+    if (event.which == key.PGUP) {
+        IPython.notebook.select_prev();
+        return false;
+    }; 
+   
+    if (event.which == key.PGDOWN) {
+        IPython.notebook.select_next();
+        return false;
+    };
+    
+    if (event.which == key.END && event.ctrlKey) {
+        var ncells = IPython.notebook.ncells();
+        IPython.notebook.select(ncells-1);
+        return false;
+    };
+
+    if (event.which == key.HOME && event.ctrlKey) {
+        IPython.notebook.select(0);
+        return false;
+    };
+    
+   return true;
+};
+
+/**
  * Register onKeyEvent to codemirror for all cells
+ * and global keydown event
  *
  */
-var init_keyevent = function(){
+init_keyevent = function(){
     var cells = IPython.notebook.get_cells();
     for(var i in cells){
         var cell = cells[i];
@@ -81,6 +85,7 @@ var init_keyevent = function(){
             cell.code_mirror.setOption('onKeyEvent',$.proxy(intercept_codemirror_keyevent,cell));
         }
     }   
+    $(document).keydown( document_keydown );
 };
 
 /**
@@ -89,7 +94,7 @@ var init_keyevent = function(){
  * @param {Object} event
  * @param {Object} current notebook cell
  */
-var insert_cell = function (event,cell) {
+insert_cell = function (event,cell) {
     if ((cell instanceof IPython.CodeCell)) {
         cell.code_mirror.setOption('onKeyEvent',$.proxy(intercept_codemirror_keyevent,cell));        
    };  
