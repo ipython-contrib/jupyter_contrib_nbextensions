602208045ad7397116cf909e628eb287ea6f09ca
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-06-07 13:47:45 +0200

    add extension that control autoscroll threshold

--- custom.js
+++ custom.js
@@ -1,30 +1,7 @@
 "using strict";
 
-function endsWith(str, suffix) {
-    return str.indexOf(suffix, str.length - suffix.length) !== -1;
-}
-
-var load_ext = function(ext_name) {
-    var full_name = '';
-    if ( !endsWith(ext_name,'.js')){ 
-        full_name = ext_name+'/main.js';
-        var extra_css = $('<link/>')
-            .attr('href','/static/custom/'+ext_name+'/main.css')
-            .attr('type','text/css')
-            .attr('rel','stylesheet');
-        console.log($('head').append(extra_css));
-    } else {
-        full_name = ext_name;
-    }
-
-    var subfolder = '';
-    $.getScript('/static/custom/'+subfolder+full_name,function(){})
-     .done(function(){ console.log('Extension loading ......[OK] : '+ext_name+' loded correctly')})
-     .fail(function(){ console.log('Extension loading ...[ERROR] : '+ext_name+' not loaded')})
-
-}
-
-
+// notebook loaded is not perfect as it is re-triggerd on 
+// revert to checkpoint
 $([IPython.events]).on('notebook_loaded.Notebook', function(){
     // Full path if extensions are only one file
 
@@ -36,8 +13,13 @@ $([IPython.events]).on('notebook_loaded.Notebook', function(){
     //load_ext('gist_it.js');
     //
 
-    // Path to folder if extensions are folders
-    load_ext('slidemode');
+    //
+    require(['custom/slidemode/main'],function(slidemode){
+        slidemode.init()
+    })
+
+    require(['custom/autoscroll']);
+
     //load_ext('css_selector')
     //load_ext('nbviewer_theme')
     //
