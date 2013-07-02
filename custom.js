6079a7712c1cc6ef3326164997396f0df2a306e6
juhasch <python@elbonia.de>
2013-07-02 21:27:41 +0200

    Add new nb extensions

---
+++ custom.js
@@ -0,0 +1,22 @@
+// leave at least 2 line with only a star on it below, or doc generation fails
+/**
+ *
+ */
+
+ 
+//$.getScript('/static/js/css_selector.js')
+//$.getScript('/static/js/slideshow.js')
+//$.getScript('/static/js/slide_meta.js')
+//$.getScript('/static/js/init_cell.js')
+$.getScript('/static/custom/read-only.js')
+
+//$.getScript('/static/custom/breakpoint.js')
+$.getScript('/static/custom/hotkeys.js')
+//$.getScript('/static/custom/comment-uncomment.js')
+//$.getScript('/static/custom/shift-tab.js')
+//$.getScript('/static/custom/codefolding.js')
+//$.getScript('/static/custom/linenumbers.js')
+
+//$.getScript('/static/js/history.js')
+
+console.log("custom.js here")
