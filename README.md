571482af8a9fbdb582bcbeb05f14a73f2e75bba5
juhasch <python@elbonia.de>
2013-07-02 21:24:37 +0200

    Split hotkeys extension into several small extensions

--- README.md
+++ README.md
@@ -2,11 +2,18 @@
 
 breakpoint - allow setting of breakpoints for notebook cells
 
-hotkeys    - add hotkey functions for easier editing/navigation
+hotkeys    - add PGUP / PGDOWN / HOME / END hotkeys for fast scrolling in codecells
 
 slideshow  - add toolbar and visual hints to slideshow extension
 
-history - add individual cell history
+history - add individual cell history (just a test...)
 
 read-only  - allow codecells to be set read-only, so no editing or celle execution is possible
 
+shift-tab - assign shift-tab to dedent
+
+codefolding - fold code blocks using Alt-F
+
+linenumbers - display line numbers in codecells using Alt-N
+
+comment-uncomment - toggle comments in selected lines using Alt-C
--- read-only.js
+++ read-only.js
@@ -1,67 +1,100 @@
 //----------------------------------------------------------------------------
-//  Copyright (C) 2008-2011  The IPython Development Team
+//  Copyright (C) 2012  The IPython Development Team
 //
 //  Distributed under the terms of the BSD License.  The full license is in
 //  the file COPYING, distributed as part of this software.
 //----------------------------------------------------------------------------
 
-// make codecell read-only
+// add button to make codecell read-only
 
-set_readOnly = function (cell,val) {
-    if (val == undefined) {
-        console.log("undefined");
-        val = true;
-    }
-    cell.metadata.run_control.read_only = val;
-    cell.read_only = val;
-    var prompt = cell.element.find('div.input_area');
-    if (val == true) {
-        prompt.css("background-color","#ffffff"); 
-        cell.code_mirror.setOption('readOnly',true);
-    } else {
-        prompt.css("background-color","#f5f5f5"); 
-        cell.code_mirror.setOption('readOnly',false);
-    }
-};
+(function (IPython) {
+    var readonlyKey = { "Alt-R" : function(){toggleReadOnly();} };
+    
+    /**
+     * Set codecell to read-only 
+     * 
+     *  @param {Object} cell current notebook cell
+     *  @param {Boolean} val is cell read-only
+     */
+    setReadOnly = function (cell,val) {
+        if (val == undefined) {
+            val = true;
+        }
+        cell.metadata.run_control.read_only = val;
+        cell.read_only = val;
+        var prompt = cell.element.find('div.input_area');
+        if (val == true) {
+            prompt.css("background-color","#ffffff"); 
+        } else {
+            prompt.css("background-color","#f5f5f5"); 
+        }
+        cell.code_mirror.setOption('readOnly',val);
+        };
 
-/**
- * toogle read-only mode in codecell
- * 
- */
-toggle_readOnly = function () {
-    var cell = IPython.notebook.get_selected_cell();
-    if ((cell instanceof IPython.CodeCell)) {
-        if (cell.metadata.run_control == undefined){
-            cell.metadata.run_control = {};    }
-        set_readOnly(cell,!cell.metadata.run_control.read_only);
-    }
-};
-
-/**
- * Add run control buttons to toolbar
- * 
- */
-init_flowcontrol = function(){
-    IPython.toolbar.add_buttons_group([
-                {
-                    id : 'run_c',
-                    label : 'Toggle read-only cell',
-                    icon : 'icon-lock',
-                    callback : toggle_readOnly
-                }
-          ]);
-          
-    var cells = IPython.notebook.get_cells();
-    for(var i in cells){
-        var cell = cells[i];
+    toggleReadOnly = function () {
+        var cell = IPython.notebook.get_selected_cell();
         if ((cell instanceof IPython.CodeCell)) {
-            if (cell.metadata.run_control != undefined) {
-                    set_readOnly(cell,cell.metadata.run_control.read_only);
+            if (cell.metadata.run_control == undefined){
+                cell.metadata.run_control = {};    }
+            setReadOnly(cell,!cell.metadata.run_control.read_only);
+        }
+    };
+
+    /**
+     * Add run control buttons to toolbar and initialize codecells
+     * 
+     */
+    initReadOnly = function(){
+        IPython.toolbar.add_buttons_group([
+                    {
+                        id : 'read_only_codecell',
+                        label : 'Toggle read-only codecell',
+                        icon : 'icon-lock',
+                        callback : toggleReadOnly
+                    }
+              ]);
+        
+        /* loop through notebook and set read-only cells defined in metadata */
+        var cells = IPython.notebook.get_cells();
+        for(var i in cells){
+            var cell = cells[i];
+            if ((cell instanceof IPython.CodeCell)) {
+                if (cell.metadata.run_control != undefined) {
+                        setReadOnly(cell,cell.metadata.run_control.read_only);
+                }
+            }
+        }   
+    };
+    
+    /* http://stackoverflow.com/questions/2454295/javascript-concatenate-properties-from-multiple-objects-associative-array */
+    function collect() {
+        var ret = {};
+        var len = arguments.length;
+        for (var i=0; i<len; i++) {
+            for (p in arguments[i]) {
+                if (arguments[i].hasOwnProperty(p)) {
+                    ret[p] = arguments[i][p];
+                }
             }
         }
-    }   
-};
+        return ret;
+    }
+    
+    /**
+     * Register new extraKeys to codemirror for newly created cell
+     *
+     * @param {Object} event
+     * @param {Object} nbcell notebook cell
+     */
+    insert_cell = function (event,nbcell) {
+        var cell = nbcell.cell;
+        if (cell.cell_type == "code") {
+            var keys = cell.code_mirror.getOption('extraKeys');
+            cell.code_mirror.setOption('extraKeys', collect(keys, readonlyKey ));  
+        }
+    };
 
-$([IPython.events]).on('notebook_loaded.Notebook',init_flowcontrol);
-
-console.log("Read-only cell extension loaded correctly")
+    $([IPython.events]).on('insert_cell.Notebook',insert_cell);
+    $([IPython.events]).on('notebook_loaded.Notebook',initReadOnly);
+    console.log("Read-only cell extension loaded correctly");
+}(IPython));
