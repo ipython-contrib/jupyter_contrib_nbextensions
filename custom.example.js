f6339a901e9005f04803b94c48b4aeacbeddc91d
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-07-13 19:56:55 +0200

    cleanup custom.js example

--- custom.example.js
+++ custom.example.js
@@ -2,15 +2,17 @@
 // on ambiguous syntax
 "using strict";
 
-// notebook loaded is not perfect as it is re-triggerd on
+// do not use notebook loaded  event as it is re-triggerd on
 // revert to checkpoint but this allow extesnsion to be loaded
 // late enough to work.
-$([IPython.events]).on('notebook_loaded.Notebook', function(){
+//
+
+$([IPython.events]).on('app_initialized.NotebookApp', function(){
 
 
     /**  Use path to js file relative to /static/ dir without leading slash, or
      *  js extension.
-     *  Link directly to file is js extension isa simple file
+     *  Link directly to file is js extension.
      *
      *  first argument of require is a **list** that can contains several modules if needed.
      **/
@@ -30,39 +32,6 @@ $([IPython.events]).on('notebook_loaded.Notebook', function(){
     //     // do stuff
     // })
 
-    // require(['custom/autoscroll']);
 
-    // load_ext('css_selector')
-    // load_ext('nbviewer_theme')
-    //
 
 });
-
-/*
-$([IPython.events]).on('notebook_loaded.Notebook', function(){
-    IPython.toolbar.add_buttons_group([
-                    {
-                'label'   : 'run qtconsole',
-                'icon'    : 'ui-icon-calculator',
-                'callback': function(){IPython.notebook.kernel.execute('%qtconsole')}
-                }
-            ]);
-});
-*/
-
-//$([IPython.events]).on('notebook_loaded.Notebook', function(){
-//    mobile_preset = []
-//    var edit = function(div, cell) {
-//            var button_container = $(div);
-//            var button = $('<div/>').button({icons:{primary:'ui-icon-pencil'}});
-//                button.click(function(){
-//                    cell.edit()
-//                        })
-//            button_container.append(button);
-//    }
-//
-//    IPython.CellToolbar.register_callback('mobile.edit',edit);
-//    mobile_preset.push('mobile.edit');
-//
-//    IPython.CellToolbar.register_preset('Mobile',mobile_preset);
-//});
