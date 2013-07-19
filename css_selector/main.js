843d91c9b074a4adeae1e0746cbe6ef7564bdeaa
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-07-19 15:02:25 +0200

    strange behavior

--- css_selector/main.js
+++ css_selector/main.js
@@ -1,20 +1,35 @@
 
-var add_css_list = function (element,names) {
-    console.log(element)
-     var label = $('<label/>').text('Css:');
-     var select = $('<select/>')
-         .addClass('ui-widget-content')
-         .append($('<option/>').attr('value', 'style.min').text('default'));
-     element.append(label).append(select);
-     select.change(function() {
-            var val = $(this).val()
-            $('link:nth(7)').attr('href','/static/css/'+val+'.css')
-     });
-     for (var i=0; i<names.length; i++) {
-         var name = names[i];
-         select.append($('<option/>').attr('value', name).text(name));
-     }
-};
+define(["require"], function(require) {
+    "use strict";
 
-add_css_list(IPython.toolbar.element,['duck','dark'])
+    //console.log('here 1');
+    var add_css_list = function (element,names) {
+    //    console.log(element)
+         var label = $('<label/>').text('Css:');
+         var select = $('<select/>')
+             .addClass('ui-widget-content')
+             .append($('<option/>').attr('value', 'style.min').text('default'));
+         element.append(label).append(select);
+    //     select.change(function() {
+    //            var val = $(this).val()
+    //            if (val == 'default'){
+    //                $('.extracss').remove();
+    //                return
+    //            }
+    //            var link = $('<link/>')
+    //                .attr('href',require.toUrl('./css/'+val+'.css'))
+    //                .attr('rel','stylesheet')
+    //                .attr('type','text/css')
+    //                .attr('class','extracss')
+    //                
+    //            $('head').append(link)
+    //     });
+    //     for (var i=0; i<names.length; i++) {
+    //         var name = names[i];
+    //         select.append($('<option/>').attr('value', name).text(name));
+    //     }
+    };
+    //console.log('here 2') 
+    add_css_list(IPython.toolbar.element,['duck','dark'])
 
+});
