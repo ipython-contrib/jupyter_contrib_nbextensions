dff7b51ba7ef968e236a7fb1c558b62ff2371b8e
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-07-19 15:57:56 +0200

    update css_selector to work with require
    +add xkcd style

--- css_selector/main.js
+++ css_selector/main.js
@@ -1,35 +1,31 @@
 
 define(["require"], function(require) {
-    "use strict";
-
-    //console.log('here 1');
+    console.log('indefine')
     var add_css_list = function (element,names) {
-    //    console.log(element)
          var label = $('<label/>').text('Css:');
          var select = $('<select/>')
              .addClass('ui-widget-content')
-             .append($('<option/>').attr('value', 'style.min').text('default'));
+             .append($('<option/>').attr('value', 'default').text('default'));
          element.append(label).append(select);
-    //     select.change(function() {
-    //            var val = $(this).val()
-    //            if (val == 'default'){
-    //                $('.extracss').remove();
-    //                return
-    //            }
-    //            var link = $('<link/>')
-    //                .attr('href',require.toUrl('./css/'+val+'.css'))
-    //                .attr('rel','stylesheet')
-    //                .attr('type','text/css')
-    //                .attr('class','extracss')
-    //                
-    //            $('head').append(link)
-    //     });
-    //     for (var i=0; i<names.length; i++) {
-    //         var name = names[i];
-    //         select.append($('<option/>').attr('value', name).text(name));
-    //     }
+         select.change(function() {
+                var val = $(this).val()
+                if (val === 'default'){
+                    $('.extracss').remove();
+                    return
+                }
+                var link = $('<link/>')
+                    .attr('href',require.toUrl('./css/'+val+'.css'))
+                    .attr('rel','stylesheet')
+                    .attr('type','text/css')
+                    .attr('class','extracss')
+                    
+                $('head').append(link)
+         });
+         for (var i=0; i<names.length; i++) {
+             var name = names[i];
+             select.append($('<option/>').attr('value', name).text(name));
+         }
     };
-    //console.log('here 2') 
-    add_css_list(IPython.toolbar.element,['duck','dark'])
+    add_css_list(IPython.toolbar.element,['duck','dark','xkcd'])
 
 });
