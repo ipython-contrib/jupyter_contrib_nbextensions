811d5044bc363d90a66c4e5ccf44253b1be08f8c
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-07-29 02:34:18 +0200

    update css ext

--- css_selector/main.js
+++ css_selector/main.js
@@ -1,31 +1,65 @@
 
 define(["require"], function(require) {
-    console.log('indefine')
-    var add_css_list = function (element,names) {
+
+    /**
+     * @param element {}  DOM element to which add controls
+     * @param styles {Dict} key = css name, value = url
+     * @para field {}  place to store the css in json. 
+     */
+    var add_css_list = function (element, styles, field) {
+         // link to which we will append css
+         var $link = $('<link/>')
+         var $tlink = $('<link/>')
+         $('head').append($link)
+         $('head').append($tlink)
+        
+         // build UI
          var label = $('<label/>').text('Css:');
          var select = $('<select/>')
-             .addClass('ui-widget-content')
-             .append($('<option/>').attr('value', 'default').text('default'));
+             .append($('<option/>')
+             .attr('value', 'default')
+             .text('default'));
          element.append(label).append(select);
+         
+         // we'll loop on each element and create ui based on their name.
          select.change(function() {
                 var val = $(this).val()
                 if (val === 'default'){
-                    $('.extracss').remove();
+                    $link.attr('href','');
+                    for(c in IPython.notebook.get_cells()){
+                        IPython.notebook.get_cell(c).code_mirror.setOption('theme', 'ipython')
+                    }
                     return
                 }
-                var link = $('<link/>')
-                    .attr('href',require.toUrl('./css/'+val+'.css'))
+                styles[val] = styles[val]||{}
+                var cmtm = styles[val].cm || 'ipyhton'; 
+                if(cmtm != undefined){
+
+                    $tlink.attr('href',require.toUrl('../../components/codemirror/theme/'+cmtm+'.css'))
+                        .attr('rel','stylesheet')
+                        .attr('type','text/css')
+                    
+                    for(c in IPython.notebook.get_cells()){
+                        IPython.notebook.get_cell(c).code_mirror.setOption('theme', cmtm)
+                    }
+                }
+                $link.attr('href',require.toUrl('./css/'+val+'.css'))
                     .attr('rel','stylesheet')
                     .attr('type','text/css')
-                    .attr('class','extracss')
-                    
-                $('head').append(link)
+                md.css = ['val'];
+                
          });
-         for (var i=0; i<names.length; i++) {
-             var name = names[i];
+
+         for (var name in styles) {
              select.append($('<option/>').attr('value', name).text(name));
          }
     };
-    add_css_list(IPython.toolbar.element,['duck','dark','xkcd'])
+    var md = IPython.notebook.metadata
+        md.css = [];
+    add_css_list(IPython.toolbar.element,{'duck':null
+                                         ,'dark':{cm:'monokai'}
+                                         ,'xkcd':null
+                                         ,'foo':null}, md )
+    
 
 });
