1839e8009c1a33c3390bdeef70be12565f60471c
Bart Janssen <bart.j.janssen@gmail.com>
2013-08-24 21:38:32 +0200

    Add use strict and fix sloppy code. Enable work-around for non-triggering notebook_load.Notebook event.

--- hide_input.js
+++ hide_input.js
@@ -1,48 +1,54 @@
 // Adds a button to hide the input part of the currently selected cells
 
-
-var hide_input = function () {
-
-  // Find the selected cell
-  var cell = IPython.notebook.get_selected_cell();
-  // Toggle visibility of the input div
-  cell.element.find("div.input").toggle('slow')
-
-    if ( cell.metadata.input_collapsed ) {
-      cell.metadata.input_collapsed = false;
-    } else {
-      cell.metadata.input_collapsed = true;
-    }
-};
+// Prevent this script from cluttering the IPython namespace
+(function (IPython) {
+  "use strict";
 
 
-init_hide_input = function(){
+  var hide_input = function () {
 
-  // Add a button to the toolbar
-  IPython.toolbar.add_buttons_group([{
-    label:'hide input',
-    icon:'icon-chevron-up',
-    callback:hide_input,
-  }]);
+    // Find the selected cell
+    var cell = IPython.notebook.get_selected_cell();
+    // Toggle visibility of the input div
+    cell.element.find("div.input").toggle('slow')
 
-  // Collapse all cells that are marked as hidden
-  var cells = IPython.notebook.get_cells();
-  cells.forEach( function(cell){
-    if( cell.metadata.input_collapsed ){
-      cell.element.find("div.input").toggle(0);
-    }
+  if ( cell.metadata.input_collapsed ) {
+    cell.metadata.input_collapsed = false;
+  } else {
+    cell.metadata.input_collapsed = true;
   }
-  );
-
-  // Write a message to the console to confirm the extension loaded
-  console.log("hide_input cell extension loaded correctly");
-
-  return true;
-}
+  };
 
 
-// The following line somehow does not work,
-// i.e. init_hide_input never gets called.
-$([IPython.events]).on('notebook_loaded.Notebook', init_hide_input);
-// work-around:
-//init_hide_input();
+  var init_hide_input = function(){
+
+    // Add a button to the toolbar
+    IPython.toolbar.add_buttons_group([{
+      label:'hide input',
+      icon:'icon-chevron-up',
+      callback:hide_input,
+    }]);
+
+    // Collapse all cells that are marked as hidden
+    var cells = IPython.notebook.get_cells();
+    cells.forEach( function(cell){
+      if( cell.metadata.input_collapsed ){
+        cell.element.find("div.input").toggle(0);
+      }
+    }
+    );
+
+    // Write a message to the console to confirm the extension loaded
+    console.log("hide_input cell extension loaded correctly");
+
+    return true;
+  }
+
+
+  // The following line somehow does not work,
+  // i.e. init_hide_input never gets called.
+  $([IPython.events]).on('notebook_loaded.Notebook', init_hide_input);
+  // work-around:
+  init_hide_input();
+
+}(IPython));
