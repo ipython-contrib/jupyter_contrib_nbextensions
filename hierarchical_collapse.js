d1c3c57b1ccbc5ca9f4b0a93cde13858bfc1fc39
Bart Janssen <bart.j.janssen@gmail.com>
2013-08-25 15:44:39 +0200

    Fix toggle bug and enable toggling when non-heading cell is selected.

--- hierarchical_collapse.js
+++ hierarchical_collapse.js
@@ -22,6 +22,43 @@
 
 
   /**
+   * Find the closest heading cell above the currently
+   * selected cell which is not yet collapsed. If the
+   * currently selected cell is a heading cell, no 
+   * new cell is sought for.
+   */
+  var find_toggleable_cell = function () {
+
+    // Get selected cell
+    var cell = IPython.notebook.get_selected_cell();
+
+    // If the current cell is a heading cell return
+    if ( cell.cell_type === "heading" ) {
+      return cell;
+    } else {
+      // Find a heading cell that is not yet collapsed
+      var index = IPython.notebook.get_selected_index();
+      var is_collapsable = ( (cell.cell_type === "heading") && cell.metadata.heading_collapsed !== true );  
+
+      while( index > 0 && !is_collapsable ) {
+        index--;
+        cell = IPython.notebook.get_cell( index );
+        is_collapsable = ( (cell.cell_type === "heading") && cell.metadata.heading_collapsed !== true );  
+      }
+      if( index === 0 && !is_collapsable ) {
+        // No candidate was found, return the current cell
+        return IPython.notebook.get_selected_cell();
+      } else {
+        // select his cell and return
+        IPython.notebook.select(index);
+        return cell;
+      }
+    }
+
+  }
+
+
+  /**
    * Hide/reveal all cells in the section headed by cell.
    *
    * @param {Object} cell notebook cell
@@ -36,9 +73,30 @@
     var current_cell = IPython.notebook.get_cell( index );
     var cell_level = get_cell_level( current_cell );
 
+    var enable_toggle = true;
+    var switch_heading_level = 6;
+
     while( cell_level > section_level ) {
+
+      // Hide/reveal regular cells until a heading is found that is collapsed/revealed
+      // then stop collapsing/revealing until a new heading is found of that level
+      if( cell_level <= switch_heading_level ) {
+        if( current_cell.metadata.heading_collapsed === true ) {
+          enable_toggle = false;
+          // do toggle the heading
+          current_cell.element.slideToggle();
+          // mark the next level from which we can update enable_toggle
+          switch_heading_level = get_cell_level( current_cell );
+        } else {
+          enable_toggle = true;
+        }
+      }
+
       // Hide the current cell
-      current_cell.element.slideToggle();
+      if ( enable_toggle ) {
+        current_cell.element.slideToggle();
+      }
+
       // Proceed to the next cell
       index++;
       current_cell = IPython.notebook.get_cell( index );
@@ -57,7 +115,8 @@
       label:'toggle heading',
       icon:'icon-double-angle-up',
       callback: function () { 
-        var cell = IPython.notebook.get_selected_cell()
+        var cell = find_toggleable_cell();
+        //var cell = IPython.notebook.get_selected_cell();
         toggle_heading( cell );
 
         // Mark as collapsed
