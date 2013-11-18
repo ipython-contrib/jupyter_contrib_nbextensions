c3a803c84baf3fd32a3a972d925236a71d29aed9
Juergen Hasch <python@elbonia.de>
2013-11-18 13:17:42 +0100

    Update Codefolding

---
+++ usability/codefolding/firstline-fold.js
@@ -0,0 +1,14 @@
+/* allow folding of complete cell, if comment is in first line */
+CodeMirror.registerHelper("fold", "firstline", function(cm, start) {
+  var mode = cm.getMode(), Token = mode.lineComment;
+  if (start.line == 0) {
+      var lineText = cm.getLine(start.line);
+      var found = lineText.lastIndexOf(Token,0);
+      if (found == 0) {
+        end =  cm.lastLine(); 
+        return {from: CodeMirror.Pos(start.line, null),
+              to: CodeMirror.Pos(end, null)}; 
+        }
+    }
+    return ;  
+});
