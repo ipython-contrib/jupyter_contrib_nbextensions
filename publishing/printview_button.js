dda63c4459a9160470d0d139eccc7eaef7a39dab
juhasch <python@elbonia.de>
2013-11-27 13:02:06 +0100

    Add version check for notebook path

--- publishing/printview_button.js
+++ publishing/printview_button.js
@@ -11,13 +11,17 @@
 nbconvertPrintView = function(){
     var kernel = IPython.notebook.kernel;
     var name = IPython.notebook.notebook_name;
-    var path = IPython.notebook.notebookPath();
-    if (path.length > 0) { path = path.concat('/'); }
+    
+    if (IPython.version[0] == "2") {
+        var path = IPython.notebook.notebookPath();
+        if (path.length > 0) { path = path.concat('/'); }
+    } else {
+        var path = "";
+    }
     
     var command = 'import os; os.system(\"ipython nbconvert --to html ' + name + '\")';
     function callback(out_type, out_data)
         { 
-        console.log('out:', out_data);  
         var url = '/files/' + path + name.split('.ipynb')[0] + '.html';
         var win=window.open(url, '_blank');
         win.focus();
