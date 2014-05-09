9e71092ed1e398f8261e9938ff30c953477f2fcf
Juergen Hasch <python@elbonia.de>
2014-05-09 14:44:21 +0200

    Enable pasting graphics
    Pasting graphics is now possible using the drag and drop websocket
    server.

--- usability/chrome_clipboard.js
+++ usability/chrome_clipboard.js
@@ -14,6 +14,36 @@ chrome_clipboard = function() {
 
     if (window.chrome == undefined) return;
 
+    /* http://stackoverflow.com/questions/3231459/create-unique-id-with-javascript */
+    function uniqueid(){
+        // always start with a letter (for DOM friendlyness)
+        var idstr=String.fromCharCode(Math.floor((Math.random()*25)+65));
+        do {                
+            // between numbers and characters (48 is 0 and 90 is Z (42-48 = 90)
+            var ascicode=Math.floor((Math.random()*42)+48);
+            if (ascicode<58 || ascicode>64){
+                // exclude all chars between : (58) and @ (64)
+                idstr+=String.fromCharCode(ascicode);    
+            }                
+        } while (idstr.length<32);
+
+        return (idstr);
+    }
+
+    /* receive url of graphics from websocket */
+    dragdrop_event = function(evt){
+        console.log("Websock-Event:", evt);
+        var obj = $.parseJSON(evt.data);
+        if (obj.status == "OK") {
+            var new_cell = IPython.notebook.insert_cell_below('markdown');
+            var filename = obj.name;
+            var str = '<img  src="' + filename + '"/>';
+            new_cell.set_text(str);
+            new_cell.rendered = false;
+            new_cell.render();
+        }
+    }
+    
     /* 
      * override clipboard 'paste' and insert new cell from json data in clipboard
      */
@@ -21,34 +51,30 @@ chrome_clipboard = function() {
         var cell = IPython.notebook.get_selected_cell();
         if (cell.mode == "command" ) {
             event.preventDefault();
-            /* paste images */
-            if (event.dataTransfer != undefined) {
-                var items = event.clipboardData.items;
-                for (var i = 0; i < items.length; ++i) {
-                    if (items[i].kind == 'file' && items[i].type.indexOf('image/') !== -1) {
-                        var blob = items[i].getAsFile();
-                        if (blob.size > 100000 ) alert('Size exceeds 100K, not recommended');
-                        var reader = new FileReader();
-                        reader.onload = ( function(evt) {
-                            var new_cell = IPython.notebook.insert_cell_below('markdown');
-                            var str = '<img src="' + evt.target.result + '">';
-                            new_cell.set_text(str);
-                            new_cell.rendered = false;
-                            new_cell.read_only = true;
-                            new_cell.render();
-                            event.preventDefault();
-                            return;
-                            } );
-                        reader.readAsDataURL(blob);
-                    }
-                }
-            } else {
-            /* paste notebook cell */
-            var data = event.clipboardData.getData('notebook-cell/json');
-            if (data) {
-                var new_cell_data = JSON.parse(data);
-                var new_cell = IPython.notebook.insert_cell_below(new_cell_data.cell_type);
-                new_cell.fromJSON(new_cell_data);
+            var items = event.clipboardData.items;
+            for (var i = 0; i < items.length; i++) {
+                console.log("items:", items[i].type);
+                if (items[i].type == 'notebook-cell/json') {
+                    /* json data adds a new notebook cell */
+                    var data = event.clipboardData.getData('notebook-cell/json');
+                    var new_cell_data = JSON.parse(data);
+                    var new_cell = IPython.notebook.insert_cell_below(new_cell_data.cell_type);
+                    new_cell.fromJSON(new_cell_data);
+                } else if (items[i].type.indexOf('image/') !== -1) {
+                    /* images are transferred to the server as file and linked to */
+                    var blob = items[i].getAsFile();
+                    var reader = new FileReader();
+                    reader.onload = ( function(evt) {
+                        var filename = uniqueid();
+                        var msg = JSON.stringify({"type":"file",
+                                                  "name":filename, 
+                                                  "path":IPython.notebook.notebook_path,
+                                                  "url" : "",
+                                                  "data": evt.target.result});
+                        IPython.notebook.ws_dragdrop.send(msg); 
+                        event.preventDefault();
+                        } );
+                    reader.readAsDataURL(blob);
                 }
             }
         }
@@ -93,4 +119,27 @@ chrome_clipboard = function() {
         }
     });
 
+    function callback(out_data)
+    { 
+        var ul = out_data.content.data;
+        var webport = eval(ul['text/plain'])
+        console.log("webport:",webport); 
+        var wsUri = "ws://" + document.domain + ":" + webport + "/"+ "websocket"; 
+        var ws_dragdrop = new WebSocket(wsUri);
+        IPython.notebook.ws_dragdrop = ws_dragdrop;
+        ws_dragdrop.onmessage = dragdrop_event;
+    }
+
+    function getport() 
+    {
+        var code = 'drag_and_drop_webport';
+        var callbacks = { iopub : { output: callback } };
+           
+        IPython.notebook.kernel.execute(code, callbacks, {silent: false});     
+    }
+    
+        
+    $([IPython.events]).on('create.Cell',create_cell);     
+    $([IPython.events]).on('status_started.Kernel',function() { getport();});     
+    
 }();
