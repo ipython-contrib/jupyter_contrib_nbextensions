b6bd298924d14705cf5875215bc4afbab89f0850
juhasch <python@elbonia.de>
2014-10-13 14:25:58 +0200

    Make codefolding extension work on latest master and cleanup

--- usability/codefolding/codefolding.js
+++ usability/codefolding/codefolding.js
@@ -1,26 +1,43 @@
 // Allow codefolding in code cells
+//
+// This extension enables the CodeMirror feature
+// It works by adding a gutter area to each code cell. 
+// Fold-able code is marked using small triangles in the gutter.
+//
+// The current folding state is saved in the cell metadata as an array
+// of line numbers.
+// Format: cell.metadata.code_folding = [ line1, line2, line3, ...]
+//
 
-"using strict";
+define([
+    'base/js/namespace',
+    'jquery',
+    "base/js/events",
+], function(IPython, $, events) {
+    "use strict";
 
-define( function () {
-    var load_ipython_extension = function () {
+    var foldingKey = { "Alt-F" : toggleFolding };
+    
+    /*
+     * Toggle folding on/off at current line
+     *
+     * @method toggleFolding
+     * @param cm CodeMirror instance
+     *
+     */
+    function toggleFolding(cm) {
+        var pos = cm.getCursor();
+        var opts = cm.state.foldGutter.options;
+        cm.foldCode(pos, opts.rangeFinder);
+    }
 
-        var hotKey = "Alt-F";
-        
-        function toggleFolding(cm) {
-            var pos = cm.getCursor();
-            var opts = cm.state.foldGutter.options;
-            cm.foldCode(pos, opts.rangeFinder);
-        }
 
-        var foldingKey = { "Alt-F" : toggleFolding };
-
-        /**
-         * Concatenate associative array objects
-         *
-         * Source: http://stackoverflow.com/questions/2454295/javascript-concatenate-properties-from-multiple-objects-associative-array
-         */
-        function collect() {
+    /**
+     * Concatenate associative array objects
+     *
+     * Source: http://stackoverflow.com/questions/2454295/javascript-concatenate-properties-from-multiple-objects-associative-array
+     */
+    function collect() {
         var ret = {};
         var len = arguments.length;
         for (var i=0; i<len; i++) {
@@ -33,71 +50,82 @@ define( function () {
         return ret;
     }
 
-        /**
-         * Update cell metadata whith folding info, so folding state can be restored after reloading notebook
-         *
-         */
-        function update_metadata(cm) {
-            var list = cm.getAllMarks();
-            var lines = [];
-            for (var i = 0; i < list.length; i++) {
-                if (list[i].__isFold == true) {
-                    var range = list[i].find();
-                    lines.push(range.from.line);
-                }
-            }
-            /* User can click on gutter of unselected cells, so make sure we store metadata in the correct cell */
-            var cell = IPython.notebook.get_selected_cell();
-            if (cell.code_mirror != cm) {
-                var cells = IPython.notebook.get_cells();
-                for(var i in cells){
-                    var cell = cells[i];
-                    if (cell.code_mirror == cm ) { break; }
-                }
-            }
-            cell.metadata.code_folding = lines;
-        }
-               
-        /**
-         * Activate codefolding in CodeMirror options, don't overwrite other settings
-         *
-         */
-        function cellFolding(cell) {
-            if (CodeMirror.fold != undefined) { 
-                var keys = cell.code_mirror.getOption('extraKeys');
-                cell.code_mirror.setOption('extraKeys', collect(keys, foldingKey ));  
-                var mode = cell.code_mirror.getOption('mode');
-                if (mode.name == 'python' ) {
-                    cell.code_mirror.setOption('foldGutter',{rangeFinder: new CodeMirror.fold.combine(CodeMirror.fold.firstline, CodeMirror.fold.indent) });                        
-                } else {
-                    cell.code_mirror.setOption('foldGutter',{rangeFinder: new CodeMirror.fold.combine(CodeMirror.fold.firstline, CodeMirror.fold.brace) });            
-                }
-                var gutters = cell.code_mirror.getOption('gutters');
-                    var found = jQuery.inArray("CodeMirror-foldgutter", gutters);
-                    if ( found == -1) {
-                        cell.code_mirror.setOption('gutters', [ gutters , "CodeMirror-foldgutter"]);
-                    }            
-                cell.code_mirror.on('fold',update_metadata);
-                cell.code_mirror.on('unfold',update_metadata);
-            }
-        }
-        
-        /**
-         * Add codefolding to new cell
-         *
-         */
-        createCell = function (event,nbcell,nbindex) {
-            var cell = nbcell.cell;
-            if ((cell instanceof IPython.CodeCell)) {
-                cellFolding(cell)
-            }
-        };
-
     /**
-    * Add codefolding to existing cells
+     * Update cell metadata with folding info, so folding state can be restored after reloading notebook
+     *
+     * @method updateMetadata
+     * @param cm CodeMirror instance
+     */
+    function updateMetadata(cm) {
+        var list = cm.getAllMarks();
+        var lines = [];
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].__isFold == true) {
+                var range = list[i].find();
+                lines.push(range.from.line);
+            }
+        }
+        /* User can click on gutter of unselected cells, so make sure we store metadata in the correct cell */
+        var cell = IPython.notebook.get_selected_cell();
+        if (cell.code_mirror != cm) {
+            var cells = IPython.notebook.get_cells();
+            for(var i in cells){
+                var cell = cells[i];
+                if (cell.code_mirror == cm ) { break; }
+            }
+        }
+        cell.metadata.code_folding = lines;
+    }
+               
+    /**
+     * Activate codefolding in CodeMirror options, don't overwrite other settings
+     *
+     * @method cellFolding
+     * @param cell {CodeCell} code cell to activate folding gutter
+     */
+    function cellFolding(cell) {
+        if (CodeMirror.fold != undefined) { 
+            var keys = cell.code_mirror.getOption('extraKeys');
+            cell.code_mirror.setOption('extraKeys', collect(keys, foldingKey ));  
+            var mode = cell.code_mirror.getOption('mode');
+            //console.log("mode:",mode)
+            if (mode.name == 'ipython' ) {
+                cell.code_mirror.setOption('foldGutter',{rangeFinder: new CodeMirror.fold.combine(CodeMirror.fold.firstline, CodeMirror.fold.indent) });                        
+            } else {
+                cell.code_mirror.setOption('foldGutter',{rangeFinder: new CodeMirror.fold.combine(CodeMirror.fold.firstline, CodeMirror.fold.brace) });            
+            }
+            var gutters = cell.code_mirror.getOption('gutters');
+                var found = jQuery.inArray("CodeMirror-foldgutter", gutters);
+                if ( found == -1) {
+                    cell.code_mirror.setOption('gutters', [ gutters , "CodeMirror-foldgutter"]);
+                }            
+            cell.code_mirror.on('fold',updateMetadata);
+            cell.code_mirror.on('unfold',updateMetadata);
+        }
+    }
+    
+    /**
+     * Add codefolding to a new cell
+     *
+     * @method createCell
+     * @param event
+     * @param nbcell
      *
      */
-    initExtension = function(event) {
+    createCell = function (event,nbcell) {
+        var cell = nbcell.cell;
+        if ((cell instanceof IPython.CodeCell)) {
+            cellFolding(cell)
+        }
+    };
+
+    /*
+     * Initialize gutter in existing cells
+     *
+     * @method initExtension
+     *
+     */
+    initExtension = function() {
         var cells = IPython.notebook.get_cells();
         for(var i in cells){
             var cell = cells[i];
@@ -113,7 +141,7 @@ define( function () {
                 }
             }
         }
-        $([IPython.events]).on('create.Cell',createCell);
+        events.on('create.Cell',createCell);
     }
 
     /* Load my own CSS file */
@@ -132,11 +160,5 @@ define( function () {
              '/static/components/codemirror/addon/fold/brace-fold.js',
              '/nbextensions/usability/codefolding/indent-fold.js',
              '/nbextensions/usability/codefolding/firstline-fold.js'], initExtension)
-    };
-
-    return {
-        load_ipython_extension : load_ipython_extension,
-    };
-});
-
+})
 
