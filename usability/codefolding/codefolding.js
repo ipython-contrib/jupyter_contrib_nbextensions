4503c36e7b87636d726c720627eb1f0b4bd9734c
Juergen Hasch <python@elbonia.de>
2014-11-09 20:39:30 +0100

    Fix missing var declaration

--- usability/codefolding/codefolding.js
+++ usability/codefolding/codefolding.js
@@ -41,7 +41,7 @@ define([
         var ret = {};
         var len = arguments.length;
         for (var i=0; i<len; i++) {
-            for (p in arguments[i]) {
+            for (var p in arguments[i]) {
                 if (arguments[i].hasOwnProperty(p)) {
                     ret[p] = arguments[i][p];
                 }
