f99fd53aa0d50e8cff83853b63d96f5214da5245
Juergen Hasch <python@elbonia.de>
2014-11-15 23:38:03 +0100

    Use JS config API from https://github.com/ipython/ipython/pull/6874

--- custom.example.js
+++ custom.example.js
@@ -1,38 +1,30 @@
-// custom.js using REST API to load nbextensions
+// custom.js using config.js API to load nbextensions
 "use strict";
 
-// do not use notebook loaded  event as it is re-triggerd on
-// revert to checkpoint but this allow extension to be loaded
-// late enough to work.
+// get nbconfig/nbextensions.json from your local profile
+// retrieve name of path to nbextension and boolean activate flag
 
-require(["base/js/events"], function (events) {
-
-function httpGet(theUrl)
-{
-    var xmlHttp = null;
-
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-
-events.on("kernel_ready.Kernel", function () {    
-
-    var a = httpGet('http://' + location.host + '/api/config/nbextensions')
-    var b = JSON.parse(a)
-
-    var path = b.path
-    var activate = b.activate
-
-    for (var key in path) {
-        if (activate[key] === true) {
-            IPython.load_extensions(path[key])
-        }
-    }    
-
-    /* disable shortcuts for cell toolbar */
-    IPython.keyboard_manager.register_events($('#cell_type'));
-    IPython.keyboard_manager.register_events($('#ctb_select'));
-    });
-});
+define([
+    'base/js/namespace',
+    'jquery',
+    'services/config',
+    'base/js/utils',
+    'base/js/events',
+], function(IPython, $, configmod, utils, events) {
+    var config = new configmod.ConfigSection('nbextensions',
+                                    {base_url: utils.get_body_data("baseUrl")});
+    config.load();
+    
+    config.loaded.then(function() {
+        var path = config.data.path
+        var activate = config.data.activate
+        events.on('app_initialized.NotebookApp', function () {
+            for (var key in path) {
+                if (activate[key] === true) {
+                    IPython.load_extensions(path[key])
+                }
+            }
+        })
+    })
+})
+    
