6260290564209c6deb83f8312dd1a3125a11090f
Michael Boyle <michael.oliver.boyle@gmail.com>
2015-03-16 17:25:22 -0400

    Add AstroPy constants

--- python.js
+++ python.js
@@ -5,16 +5,18 @@ define([
     "./python/matplotlib",
     "./python/sympy",
     "./python/pandas",
+    "./python/astropy",
     "./python/h5py",
     "./python/numba",
     "./python/python",
-], function (require, numpy, scipy, matplotlib, sympy, pandas, h5py, numba, python) {
+], function (require, numpy, scipy, matplotlib, sympy, pandas, astropy, h5py, numba, python) {
     return {
         numpy:numpy,
         scipy:scipy,
         matplotlib:matplotlib,
         sympy:sympy,
         pandas:pandas,
+        astropy:astropy,
         h5py:h5py,
         numba:numba,
         python:python,
