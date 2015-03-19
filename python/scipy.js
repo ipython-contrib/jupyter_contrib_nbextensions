9a30a66b40b28afb24476b5f94606878e7fd5c07
Michael Boyle <michael.oliver.boyle@gmail.com>
2015-03-19 16:49:55 -0400

    Add function fitting example

--- python/scipy.js
+++ python/scipy.js
@@ -473,6 +473,23 @@ define([
                             },
                         ],
                     },
+                    {
+                        'name' : 'Fitting',
+                        'sub-menu' : [
+                            {
+                                'name' : 'Basic function fitting',
+                                'snippet' : [
+                                    'from scipy.optimize import curve_fit',
+                                    'def fitting_function(x, a, b, c):',
+                                    '    return a * np.exp(-b * x) + c',
+                                    'xdata = np.linspace(0, 4, 50)',
+                                    'ydata = fitting_function(xdata, 2.5, 1.3, 0.5) + 0.2 * np.random.normal(size=len(xdata))',
+                                    'optimal_parameters, estimated_covariance = curve_fit(fitting_function, xdata, ydata)',
+                                    'estimated_std_dev = np.sqrt(np.diag(estimated_covariance))',
+                                ],
+                            },
+                        ],
+                    },
                 ],
             },
 
