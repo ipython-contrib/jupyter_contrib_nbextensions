88a9e706beea5388aeda39b323feccefe1d5338f
Juergen Hasch <python@elbonia.de>
2015-07-25 09:32:33 +0200

    Fix nbextension configuration

--- config/nbextensions.py
+++ config/nbextensions.py
@@ -1,30 +1,31 @@
 # Copyright (c) IPython-Contrib Team.
 # Notebook Server Extension to activate/deactivate javascript notebook extensions
 #
-import IPython
-from IPython.utils.path import get_ipython_dir
-from IPython.html.utils import url_path_join as ujoin
-from IPython.html.base.handlers import IPythonHandler, json_errors
+from jupyter_core.paths import jupyter_data_dir
+from notebook.utils import url_path_join as ujoin
+from notebook.base.handlers import IPythonHandler, json_errors
+from notebook.nbextensions import _get_nbext_dir as get_nbext_dir
 from tornado import web
+from itertools import chain
 import os
 import yaml
 import json
+
 
 class NBExtensionHandler(IPythonHandler):
     """Render the notebook extension configuration interface."""
     @web.authenticated
     def get(self):
-        ipythondir = get_ipython_dir()        
-        nbextensions = os.path.join(ipythondir,'nbextensions') 
+        jupyterdir = jupyter_data_dir()
+        nbextensions = (get_nbext_dir(), os.path.join(jupyterdir,'nbextensions'))
         exclude = [ 'mathjax' ]
         yaml_list = []
         # Traverse through nbextension subdirectories to find all yaml files
-        for root, dirs, files in os.walk(nbextensions):
+        for root, dirs, files in chain.from_iterable(os.walk(root) for root in nbextensions):
             dirs[:] = [d for d in dirs if d not in exclude]
             for f in files:
                 if f.endswith('.yaml'):
                     yaml_list.append([ root, f] )
-                    
         # Build a list of extensions from YAML file description
         # containing at least the following entries:
         #   Type         - identifier
@@ -37,15 +38,18 @@ class NBExtensionHandler(IPythonHandler):
             stream = open(os.path.join(y[0],y[1]), 'r')
             extension = yaml.load(stream)
             if all (k in extension for k in ('Type', 'Compatibility', 'Name', 'Main', 'Description')):
-                if not extension['Type'].startswith('IPython Notebook Extension'):
+                if not extension['Type'].strip().startswith('IPython Notebook Extension'):
                     continue
-                if extension['Compatibility'][0] is not '3':
+                if not extension['Compatibility'].strip().startswith('3.'):
                     continue
                 # generate URL to extension
                 idx=y[0].find('nbextensions')
                 url = y[0][idx::].replace('\\', '/')
                 extension['url'] = url
+                # replace single quote with HTML representation
+                extension['Description'] = extension['Description'].replace("'","&#39;")
                 extension_list.append(extension)
+                self.log.info("Found extension %s" % extension['Name'])
             stream.close()
         json_list = json.dumps(extension_list)
         self.write(self.render_template('nbextensions.html',
