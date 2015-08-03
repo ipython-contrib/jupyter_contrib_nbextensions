8d013c73ccedab26fc715695e41e627dd4dc11c4
Juergen Hasch <python@elbonia.de>
2015-08-03 21:03:04 +0200

    Cleanup and add parameter for nbconfig

--- usability/limit_output/main.js
+++ usability/limit_output/main.js
@@ -10,17 +10,15 @@ define([
     'base/js/utils'
 ], function(IPython, $, oa, dialog, cc, configmod, utils) {
     "use strict";
-    var MAX_CHARACTERS = 10000;  // maximum number of characters the output area is allowed to print
+    var MAX_CHARACTERS = 10000;  // default maximum number of characters the output area is allowed to print
 
-    var base_url = utils.get_body_data("baseUrl")
+    var base_url = utils.get_body_data("baseUrl");
     var config = new configmod.ConfigSection('notebook', {base_url: base_url});
     config.load();
 
     config.loaded.then(function() {
-        console.log("config.data:",config.data)
         if (config.data.hasOwnProperty('limit_output') ){
             MAX_CHARACTERS = config.data.limit_output;
-            //console.log('limit_output to ', MAX_CHARACTERS, 'characters')
         }
     });
 
@@ -40,13 +38,13 @@ define([
             return 
         }
         return this._handle_output(msg);
-    }
+    };
 
     cc.CodeCell.prototype._execute = cc.CodeCell.prototype.execute;
-    cc.CodeCell.prototype.execute = function(stop_on_error){
+    cc.CodeCell.prototype.execute = function(){
         // reset counter on execution.
         this.output_area.count = 0;
         this.output_area.drop  = false;
-        return this._execute(stop_on_error);
+        return this._execute();
     }
 });
