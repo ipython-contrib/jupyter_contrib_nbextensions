49968cb351fd8293eceb893f90850f620fc752ba
Juergen Hasch <python@elbonia.de>
2015-08-16 09:19:19 +0200

    Move limit-output extension to 4.x

--- usability/limit_output/main.js
+++ usability/limit_output/main.js
@@ -14,37 +14,47 @@ define([
 
     var base_url = utils.get_body_data("baseUrl");
     var config = new configmod.ConfigSection('notebook', {base_url: base_url});
-    config.load();
+
+    function isNumber(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
 
     config.loaded.then(function() {
         if (config.data.hasOwnProperty('limit_output') ){
-            MAX_CHARACTERS = config.data.limit_output;
+            if (isNumber(config.data.limit_output)) MAX_CHARACTERS = config.data.limit_output;
         }
     });
 
-    
-    oa.OutputArea.prototype._handle_output = oa.OutputArea.prototype.handle_output;
-    oa.OutputArea.prototype.handle_output = function (msg){
-        if(!this.count){this.count=0}
-        if(!this.max_count){ this.max_count = MAX_CHARACTERS }
-        this.count = this.count+String(msg.content.text).length;        
-        if(this.count > this.max_count){
-            if(!this.drop){
-                console.log("Output exceeded", this.max_count, "characters. Further output muted.");
-                msg.content.text = msg.content.text.substr(0,this.max_count) + "**OUTPUT MUTED**";
-                this.drop=true;
-                return this._handle_output(msg);
+    var load_ipython_extension = function() {
+        config.load();
+        oa.OutputArea.prototype._handle_output = oa.OutputArea.prototype.handle_output;
+        oa.OutputArea.prototype.handle_output = function (msg){
+            if(!this.count){this.count=0}
+            if(!this.max_count){ this.max_count = MAX_CHARACTERS }
+            this.count = this.count+String(msg.content.text).length;
+            if(this.count > this.max_count){
+                if(!this.drop){
+                    console.log("Output exceeded", this.max_count, "characters. Further output muted.");
+                    msg.content.text = msg.content.text.substr(0,this.max_count) + "**OUTPUT MUTED**";
+                    this.drop=true;
+                    return this._handle_output(msg);
+                }
+                return
             }
-            return 
+            return this._handle_output(msg);
+        };
+
+        cc.CodeCell.prototype._execute = cc.CodeCell.prototype.execute;
+        cc.CodeCell.prototype.execute = function(){
+            // reset counter on execution.
+            this.output_area.count = 0;
+            this.output_area.drop  = false;
+            return this._execute();
         }
-        return this._handle_output(msg);
     };
 
-    cc.CodeCell.prototype._execute = cc.CodeCell.prototype.execute;
-    cc.CodeCell.prototype.execute = function(){
-        // reset counter on execution.
-        this.output_area.count = 0;
-        this.output_area.drop  = false;
-        return this._execute();
-    }
+    var extension = {
+        load_ipython_extension : load_ipython_extension
+    };
+    return extension;
 });
