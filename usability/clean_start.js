25c38c790d8bfe69645e4b47584c564bff5de5b4
Samuel Loury <konubinixweb@gmail.com>
2015-08-27 16:19:04 +0200

    Make the clean_start extension work with jupyter
    Add the needed load_ipython_extension property

--- usability/clean_start.js
+++ usability/clean_start.js
@@ -1,30 +1,38 @@
-var clean_restart = function(){
-    IPython.notebook.clear_all_output();
-    IPython.notebook.kernel.restart();
-}
+define([], function () {
 
-var confirm_dialog = function(){
-    var dialog = $('<div/>');
-    dialog.html('This will clear all output and restart the kernel, do you want to continue?');
-    dialog.dialog({
-      resizable: false,
-      modal: true,
-      title: 'Clear All',
-      buttons: {
-        "Reset All": function() {
-          clean_restart();
-          $( this ).dialog( "close" );
-        },
-        Cancel: function() {
-          $( this ).dialog( "close" );
-        }
-      }
-    });
-};
+	var clean_restart = function(){
+		IPython.notebook.clear_all_output();
+		IPython.notebook.kernel.restart();
+	}
 
-IPython.toolbar.add_buttons_group([{
-      label:'clear all, and restart',
-      icon:'fa-chevron-circle-down',
-      callback:confirm_dialog,
-}])
-console.log('loading clear and restart extension ok');
+	var confirm_dialog = function(){
+		var dialog = $('<div/>');
+		dialog.html('This will clear all output and restart the kernel, do you want to continue?');
+		dialog.dialog({
+			resizable: false,
+			modal: true,
+			title: 'Clear All',
+			buttons: {
+				"Reset All": function() {
+					clean_restart();
+					$( this ).dialog( "close" );
+				},
+				Cancel: function() {
+					$( this ).dialog( "close" );
+				}
+			}
+		});
+	};
+
+	var load_ipython_extension = function () {
+		IPython.toolbar.add_buttons_group([{
+			label:'clear all, and restart',
+			icon:'fa-chevron-circle-down',
+			callback:confirm_dialog,
+		}])
+		console.log('loading clear and restart extension ok');
+	}
+	return {
+		load_ipython_extension : load_ipython_extension
+	}
+});
