4932218cad41939846272bef89beee079c659003
Josh Barnes <joshuacookebarnes@gmail.com>
2015-08-31 15:56:21 +0100

    add extension to add an item to edit menu opening the nbextensions config page

---
+++ config/config_menu/main.js
@@ -0,0 +1,29 @@
+// extension by jcb91
+// Tiny extension to add an edit-menu item to open the NbExtensions config page
+
+define(["jquery"], function ($) {
+    "use strict";
+
+    var load_ipython_extension = function () {
+        var menu_item = $('<li/>').append(
+            $('<a/>', {
+                'target' : '_blank',
+                'title' : 'Opens in a new window',
+                'href' : '/nbextensions/',
+            })
+            .append(' ')
+            .append($('<i/>', {'class' : 'fa fa-cogs menu-icon pull-right'}))
+            .append($('<span/>').html('nbextension config'))
+        );
+
+        var edit_menu = $('#edit_menu');
+        edit_menu.append($('<li/>').addClass('divider'));
+        edit_menu.append(menu_item);
+    };
+
+    // export the extension so it can be loaded correctly
+    var extension = {
+        load_ipython_extension : load_ipython_extension
+    };
+    return extension;
+});
