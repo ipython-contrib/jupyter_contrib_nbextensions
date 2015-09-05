d4eec510b21544c1591c5bda7bcc006d866d2823
Juergen Hasch <python@elbonia.de>
2015-09-05 15:20:31 +0200

    Update ruler extension

--- nbextensions/usability/ruler/main.js
+++ nbextensions/usability/ruler/main.js
@@ -7,38 +7,58 @@ define([
     'require',
     'base/js/events',
     'services/config',
-    'base/js/utils',    
+    'base/js/utils',
     'codemirror/lib/codemirror',
     'codemirror/addon/display/rulers'
 ], function(IPython, $, require, events, configmod, utils, codemirror) {
     "use strict";
 
-    var RULERCOLUMN = 78;
-    var base_url = utils.get_body_data("baseUrl")
+    var ruler_column = 78;
+    var ruler_color = "#ff0000";
+    var ruler_linestyle = "dashed";
+
+    var base_url = utils.get_body_data("baseUrl");
     var config = new configmod.ConfigSection('notebook', {base_url: base_url});
     var rulers = [];
 
-    function isNumber(n) {
+    var isNumber = function(n) {
         return !isNaN(parseFloat(n)) && isFinite(n);
-    }
+    };
 
     config.loaded.then(function() {
-        if (config.data.hasOwnProperty('ruler_column') ){
-            if (isNumber(config.data.ruler_column)) RULERCOLUMN = config.data.ruler_column;
+        if (config.data.hasOwnProperty('ruler_color')) {
+            ruler_color = config.data.ruler_color;
         }
-        console.log("RULER:", RULERCOLUMN)
-        rulers.push({color: "#f00", column: RULERCOLUMN, lineStyle: "dashed"});
+        console.log("ruler_color:", ruler_color);
+
+        if (config.data.hasOwnProperty('ruler_column')) {
+            if (isNumber(config.data.ruler_column)) {
+                ruler_column = config.data.ruler_column;
+            }
+        }
+        console.log("ruler_column:", ruler_column);
+
+        if (config.data.hasOwnProperty('ruler_linestyle')) {
+            ruler_linestyle = config.data.ruler_linestyle;
+        }
+        console.log("ruler_linestyle:", ruler_linestyle);
+
+        rulers.push({
+            color: ruler_color,
+            column: ruler_column,
+            lineStyle: ruler_linestyle
+        });
         var cells = IPython.notebook.get_cells();
-        for(var i in cells){
+        for(var i in cells) {
             var cell = cells[i];
             if ((cell instanceof IPython.CodeCell)) {
                 cell.code_mirror.setOption('rulers', rulers);
             }
         }
-        events.on('create.Cell',createCell);
+        events.on('create.Cell', createCell);
     });
 
-    var createCell = function (event,nbcell) {
+    var createCell = function (event, nbcell) {
         var cell = nbcell.cell;
         if ((cell instanceof IPython.CodeCell)) {
             cell.code_mirror.setOption('rulers', rulers);
