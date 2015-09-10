ac45d40e0dd6a2d590403a351d3e188da5e8a2c8
Josh Barnes <joshuacookebarnes@gmail.com>
2015-09-10 12:44:06 +0100

    * place each extension into its own directory * remove nbextensions/usability/hide_input.js, which is a duplicate of nbextensions/usability/hide_input/hide_input.js

--- nbextensions/publishing/gist_it/main.js
+++ nbextensions/publishing/gist_it/main.js
@@ -1,15 +1,16 @@
 // Avoid server side code :
 // https://github.com/ipython/ipython/issues/2780
+
 var show_gist_link = function(type,data){
     var dialog = $('<div/>');
-    dialog.html('you can see this notebook at '+data.data);
+    dialog.html('you can see this notebook at ' + data.data);
 
-    if(IPython.notebook.metadata._draft == undefined){
-        IPython.notebook.metadata._draft = {}
+    if(IPython.notebook.metadata._draft === undefined){
+        IPython.notebook.metadata._draft = {};
     }
 
-    if(IPython.notebook.metadata._draft.nbviewer_url == undefined){
-        IPython.notebook.metadata._draft.nbviewer_url = data.data
+    if(IPython.notebook.metadata._draft.nbviewer_url === undefined){
+        IPython.notebook.metadata._draft.nbviewer_url = data.data;
     }
 
     $(document).append(dialog);
@@ -18,11 +19,11 @@ var show_gist_link = function(type,data){
         modal: true,
         title: "Shared on Github",
     });
-}
+};
 
 var cdict = {
     output: show_gist_link
-    }
+};
 
 IPython.toolbar.add_buttons_group([
     {
@@ -32,7 +33,7 @@ IPython.toolbar.add_buttons_group([
                  IPython.notebook.kernel.execute(
                         //'!jist -p ' + IPython.notebook.notebook_name + '.ipynb',
                         '!echo "gisting : ' + IPython.notebook.notebook_name + '.ipynb"',
-                        cdict)
+                        cdict);
 
     }
 }]);
