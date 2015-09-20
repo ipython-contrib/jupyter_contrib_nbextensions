5a0a58a3b4135605c82ef7baa89979214d7d08f9
Juergen Hasch <python@elbonia.de>
2015-09-20 11:00:27 +0200

    Add configuration options to printview extensions

--- nbextensions/publishing/printview/main.js
+++ nbextensions/publishing/printview/main.js
@@ -1,10 +1,32 @@
-// convert current notebook to html by calling "ipython nbconvert" and open static html file in new tab
+// call "ipython nbconvert" and open generated html file in new tab
 
 define([
     'base/js/namespace',
-    'jquery'
-], function(IPython, $) {
+    'jquery',
+    'services/config',
+    'base/js/utils'
+], function(IPython, $, configmod, utils) {
     "use strict";
+
+    var nbconvert_options = '--to html';
+	var open_tab = true;
+    var base_url = utils.get_body_data("baseUrl");
+    var config = new configmod.ConfigSection('notebook', {base_url: base_url});
+
+    /**
+     * Get option from config
+     */
+    config.loaded.then(function() {
+        console.log("config")
+        if (config.data.hasOwnProperty('printpreview_nbconvert_options') ) {
+            nbconvert_options = config.data.printpreview_nbconvert_options;
+        }
+        if (config.data.hasOwnProperty('printview_open_tab') ) {
+            if (typeof(config.data.printview_open_tab) === "boolean") {
+                open_tab = config.data.printview_open_tab;
+            }
+        }
+    });
 
     /**
      * Call nbconvert using the current notebook server profile
@@ -13,17 +35,20 @@ define([
 	var nbconvertPrintView = function () {
 		var kernel = IPython.notebook.kernel;
 		var name = IPython.notebook.notebook_name;
-		var command = 'import os; os.system(\"jupyter nbconvert --to html ' + name + '\")';
+		var command = 'import os; os.system(\"jupyter nbconvert ' + nbconvert_options + ' ' + name + '\")';
 		function callback(out_type, out_data) {
-			var url = name.split('.ipynb')[0] + '.html';
-			var win=window.open(url, '_blank');
-			win.focus();
+			if (open_tab === true) {
+				var url = name.split('.ipynb')[0] + '.html';
+				var win = window.open(url, '_blank');
+			}
 		}
+        console.log("cmd:", command)
 		kernel.execute(command, { shell: { reply : callback } });
         $('#doPrintView').blur()
 	};
 
 	var load_ipython_extension = function() {
+        config.load();
 		IPython.toolbar.add_buttons_group([
 			{
 				id: 'doPrintView',
@@ -33,8 +58,7 @@ define([
 			}
 		]);
 	};
-	var extension = {
+	return {
         load_ipython_extension : load_ipython_extension
     };
-    return extension;
 });
--- nbextensions/publishing/printview/readme.md
+++ nbextensions/publishing/printview/readme.md
@@ -1,2 +1,20 @@
-Add a toolbar button to call nbconvert --to html for current the notebook and display html in 
+
+Description
+-----------
+Add a toolbar button to call nbconvert for the current the notebook and optionally display the converted html file in a 
 new browser tab.
+
+The extensions has two options:
+
+* "nbconvert options": 
+Options to nbconvert. Default: `--to html`
+
+* "open a new tab in the browser to display nbconvert output (for html only)":
+After conversion to html, open a new tab. Only makes sense when converting to html output format.
+
+
+Internals
+---------
+
+The configuration is stored in the Jupyter configuration path `nbconfig/notebook.js` using two keys:
+*printpreview_nbconvert_options* and *printview_open_tab*.
