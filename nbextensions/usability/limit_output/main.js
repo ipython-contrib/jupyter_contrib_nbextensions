701b7853f1acd74ac8f4ff70e15d97a1a307bcf8
Josh Barnes <joshuacookebarnes@gmail.com>
2015-09-29 18:53:53 +0100

    parameterize the message in `usability/limit_output`

--- nbextensions/usability/limit_output/main.js
+++ nbextensions/usability/limit_output/main.js
@@ -10,47 +10,62 @@ define([
     'base/js/utils'
 ], function(IPython, $, oa, dialog, cc, configmod, utils) {
     "use strict";
-    var MAX_CHARACTERS = 10000;  // default maximum number of characters the output area is allowed to print
 
     var base_url = utils.get_body_data("baseUrl");
     var config = new configmod.ConfigSection('notebook', {base_url: base_url});
+
+    // define default values for config parameters
+    var params = {
+        // maximum number of characters the output area is allowed to print
+        limit_output : 10000,
+        // message to print when output is limited
+        limit_ouput_message : "**OUTPUT MUTED**"
+    };
+
+    // to be called once config is loaded, this updates default config vals
+    // with the ones specified by the server's config file
+    var update_params = function() {
+        for (var key in params) {
+            if (config.data.hasOwnProperty(key) ){
+                params[key] = config.data[key];
+            }
+        }
+    };
 
     function isNumber(n) {
         return !isNaN(parseFloat(n)) && isFinite(n);
     }
 
     config.loaded.then(function() {
-        if (config.data.hasOwnProperty('limit_output') ){
-            if (isNumber(config.data.limit_output)) MAX_CHARACTERS = config.data.limit_output;
-        }
-    });
+        var MAX_CHARACTERS = params.limit_output;
+        update_params();
+        if (isNumber(params.limit_output)) MAX_CHARACTERS = params.limit_output;
 
-    var load_ipython_extension = function() {
-        config.load();
         oa.OutputArea.prototype._handle_output = oa.OutputArea.prototype.handle_output;
-        oa.OutputArea.prototype.handle_output = function (msg){
-            if(!this.count){this.count=0}
-            if(!this.max_count){ this.max_count = MAX_CHARACTERS }
-            this.count = this.count+String(msg.content.text).length;
-            if(this.count > this.max_count){
-                if(!this.drop){
-                    console.log("Output exceeded", this.max_count, "characters. Further output muted.");
-                    msg.content.text = msg.content.text.substr(0,this.max_count) + "**OUTPUT MUTED**";
-                    this.drop=true;
-                    return this._handle_output(msg);
-                }
-                return
+        oa.OutputArea.prototype.handle_output = function (msg) {
+            if (this.count === undefined) { this.count=0; }
+            if (this.max_count === undefined) { this.max_count = MAX_CHARACTERS; }
+            this.count = this.count + String(msg.content.text).length;
+            if(this.count > this.max_count) {
+                if (this.drop) return;
+                console.log("limit_output: output exceeded", this.max_count, "characters. Further output muted.");
+                msg.content.text = msg.content.text.substr(0, this.max_count) + params.limit_ouput_message;
+                this.drop = true;
             }
             return this._handle_output(msg);
         };
 
         cc.CodeCell.prototype._execute = cc.CodeCell.prototype.execute;
-        cc.CodeCell.prototype.execute = function(){
+        cc.CodeCell.prototype.execute = function() {
             // reset counter on execution.
             this.output_area.count = 0;
             this.output_area.drop  = false;
             return this._execute();
-        }
+        };
+    });
+
+    var load_ipython_extension = function() {
+        config.load();
     };
 
     var extension = {
