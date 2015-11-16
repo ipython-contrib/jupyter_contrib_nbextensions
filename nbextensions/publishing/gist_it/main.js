b659c384aa4b8f29c33105e69bceb8a5fabd9fa3
Josh Barnes <joshuacookebarnes@gmail.com>
2015-11-16 18:18:14 +0000

    in `publishing/gist_it`: * add en editor to set the gist description and display success/failure messages * add support for authentication using Github personal access tokens * add readme, icon

--- nbextensions/publishing/gist_it/main.js
+++ nbextensions/publishing/gist_it/main.js
@@ -1,5 +1,28 @@
+/**
+ *
 // Avoid server side code :
 // https://github.com/ipython/ipython/issues/2780
+ *
+ * This essentially boils down to the following:
+ * Github authentication requires some server-side code for any 'app' which
+ * wants to authenticate over the Github API.
+ * When registering an app with Github, Github provides the app with what they
+ * call a 'client secret'.
+ * The client secret is then incorporated into the app, and the app sends it to
+ * Github as part of the authentication process, thus proving to Github's
+ * servers that the communicating app was written by someone with appropriate
+ * credentials.
+ *
+ * The issue with writing a single Github app for Gist-ing notebooks, is that
+ * it would need to include such a client secret. Since this would be part of
+ * the extension source code, anyone could use the client secret, potentially
+ * gaining the permissions that any given user has granted to the app.
+ *
+ * As a result, we only support:
+ * - anonymous (non-authenticated) API usage
+ * - client-side authentication using a personal access token
+ *   (see https://github.com/settings/tokens)
+ */
 
 define([
     'jquery',
@@ -18,7 +41,7 @@ define([
 
     // define default values for config parameters
     var params = {
-        gist_it_auth_token: ''
+        gist_it_personal_access_token: '',
     };
 
     // create config object to load parameters
@@ -28,9 +51,9 @@ define([
     config.loaded.then(function() {
         update_params();
         Jupyter.toolbar.add_buttons_group([{
-            label   : 'Gist Notebook',
+            label   : 'Create/Edit Gist of Notebook',
             icon    : 'fa-github',
-            callback: show_gist_editor
+            callback: show_gist_editor_modal
         }]);
     });
 
@@ -42,21 +65,65 @@ define([
         }
     };
 
-    var add_auth_token = function add_auth_token (xhr) {
-        if (params.gist_it_auth_token !== '') {
-            xhr.setRequestHeader("Authorization", "token " + params.gist_it_auth_token);
+    var default_metadata = {
+        id: '',
+        data: {
+            description: Jupyter.notebook.notebook_path,
+            public: false
         }
     };
 
-    var show_gist_link = function show_gist_link (response, textStatus, jqXHR) {
-        if(Jupyter.notebook.metadata._draft === undefined){
-            Jupyter.notebook.metadata._draft = {};
-        }
-        if(Jupyter.notebook.metadata._draft.nbviewer_url === undefined){
-            Jupyter.notebook.metadata._draft.nbviewer_url = response.html_url;
-        }
+    function ensure_default_metadata () {
+        Jupyter.notebook.metadata.gist = $.extend(
+            true, // deep-copy
+            default_metadata, //defaults
+            Jupyter.notebook.metadata.gist // overrides
+        );
+    }
 
-        var body = $('<div/>')
+    var add_auth_token = function add_auth_token (xhr) {
+        var token = '';
+        if (params.gist_it_personal_access_token !== '') {
+            token = params.gist_it_personal_access_token;
+        }
+        if (token !== '') {
+            xhr.setRequestHeader("Authorization", "token " + token);
+        }
+    };
+
+    function build_alert(alert_class) {
+        return $('<div/>')
+            .addClass('alert alert-dismissable')
+            .addClass(alert_class)
+            .append(
+                $('<button class="close" type="button" data-dismiss="alert" aria-label="Close"/>')
+                    .append($('<span aria-hidden="true"/>').html('&times;'))
+            );
+    }
+
+    function gist_error (jqXHR, textStatus, errorThrown) {
+        console.log('github ajax error:', jqXHR, textStatus, errorThrown);
+        var alert = build_alert('alert-danger')
+            .append(
+                $('<p/>').text('The ajax request to Github went wrong:')
+            )
+            .append(
+                $('<p/>').text(jqXHR.responseJSON ? jqXHR.responseJSON.message : errorThrown)
+            );
+        $('#gist_modal').find('.modal-body').append(alert);
+    }
+
+    function gist_success (response, textStatus, jqXHR) {
+        // if (Jupyter.notebook.metadata.gist.id === response.id) return;
+
+        Jupyter.notebook.metadata.gist.id = response.id;
+        Jupyter.notebook.metadata._draft = $.extend(
+            true, // deep copy
+            Jupyter.notebook.metadata._draft, // defaults
+            {nbviewer_url: response.html_url} // overrides
+        );
+
+        var alert = build_alert('alert-success')
             .append('Gist published to ')
             .append(
                 $('<a/>')
@@ -64,39 +131,229 @@ define([
                     .attr('target', '_blank')
                     .text(response.html_url)
             );
+        $('#gist_modal').find('.modal-body').append(alert);
+    }
 
-        return dialog.modal({
-            title: "Shared on Github",
-            body: body
-        });
-    };
+    function update_link () {
+        var id_input = $('#gist_id');
+        var id = id_input.val();
+        var link = $('#gist_link');
+        if (id) {
+            var html_url = 'https://gist.github.com/' + id;
+            link
+                .attr('href', html_url)
+                .attr('target', '_blank')
+                .text(html_url);
+        }
+        link.add(id_input).closest('.form-group').toggle(id !== '');
+    }
 
-    var make_gist = function make_gist () {
+    function update_gist_editor (gist_editor) {
+        if (gist_editor === undefined) gist_editor = $('#gist_editor');
 
-        var data = {
-            description: Jupyter.notebook.notebook_path,
-            public: true,
-            files: {}
-        };
+        var id = '';
+        var is_public = true;
+        if (params.gist_it_personal_access_token !== '') {
+            id = Jupyter.notebook.metadata.gist.id;
+            is_public = Jupyter.notebook.metadata.gist.data.public;
+        }
+
+        gist_editor.find('#gist_id')
+            .val(Jupyter.notebook.metadata.gist.id)
+            .prop('readonly', true);
+
+        gist_editor.find('#gist_public')
+            .prop('checked', is_public)
+            .prop('readonly', id === '');
+
+        gist_editor.find('#gist_description')
+            .val(Jupyter.notebook.metadata.gist.data.description);
+    }
+
+    function build_gist_editor () {
+        ensure_default_metadata();
+
+        var gist_editor = $('#gist_editor');
+
+        if (gist_editor.length > 0) return gist_editor;
+
+        gist_editor = $('<div/>').attr('id', 'gist_editor').append(controls);
+
+        var id = params.gist_it_personal_access_token !== '' ? Jupyter.notebook.metadata.gist.id : '';
+        var controls = $('<form/>')
+            .appendTo(gist_editor)
+            .addClass('form-horizontal');
+
+        $('<div/>')
+            .hide(0)
+            .appendTo(controls)
+            .append(
+                $('<label/>')
+                    .attr('for', 'gist_id')
+                    .text('Gist id')
+            )
+            .append(
+                $('<input/>')
+                    .addClass('form-control')
+                    .attr('id', 'gist_id')
+                    .on('change', update_link)
+                    .val(Jupyter.notebook.metadata.gist.id)
+                    .prop('readonly', true)
+            );
+        $('<div/>')
+            .hide(0)
+            .appendTo(controls)
+            .append(
+                $('<label/>')
+                    .attr('for', 'gist_link')
+                    .text('HTML view link')
+            )
+            .append(
+                $('<p/>')
+                    .addClass('form-control-static')
+                    .append(
+                        $('<a/>')
+                            .attr('id', 'gist_link')
+                    )
+            );
+        $('<div/>')
+            .appendTo(controls)
+            .append(
+                $('<div/>')
+                    .addClass('checkbox')
+                    .append(
+                        $('<label>')
+                            .text('Make the gist public')
+                            .prepend(
+                                $('<input/>')
+                                    .attr('type', 'checkbox')
+                                    .attr('id', 'gist_public')
+                                    .prop('checked', Jupyter.notebook.metadata.gist.data.public)
+                                    .prop('readonly', id === '')
+                            )
+                    )
+            )
+            .append(
+                $('<label/>')
+                    .attr('for', 'gist_public')
+                    .text('public')
+            );
+        $('<div/>')
+            .appendTo(controls)
+            .addClass('form-group')
+            .append(
+                $('<label/>')
+                    .attr('for', 'gist_description')
+                    .text('description')
+            )
+            .append(
+                $('<input/>')
+                    .addClass('form-control')
+                    .attr('id', 'gist_description')
+                    .attr('type', 'textarea')
+                    .val(Jupyter.notebook.metadata.gist.data.description)
+            );
+
+        var form_groups = controls.children('div').addClass('form-group');
+        form_groups
+            .children('label')
+                .addClass('col-sm-2 control-label')
+                .css('padding-right', '1em');
+        form_groups
+            .each(function (index, elem) {
+                $('<div/>')
+                    .appendTo(elem)
+                    .addClass('col-sm-10')
+                    .append($(elem).children(':not(label)'));
+            });
+
+        update_gist_editor(gist_editor);
+
+        return gist_editor;
+    }
+
+    function show_gist_editor_modal () {
+        var modal;
+        modal = dialog.modal({
+            show: false,
+            title: 'Share on Github',
+            notebook: Jupyter.notebook,
+            keyboard_manager: Jupyter.notebook.keyboard_manager,
+            body: build_gist_editor(),
+            buttons: {
+                ' Gist it!': {
+                    class : 'btn-primary',
+                    click: function() {
+                        modal.find('.btn').prop('disabled', true);
+                        var new_data = {
+                            public: $('#gist_public').prop('checked'),
+                            description: $('#gist_description').val()
+                        };
+                        $.extend(
+                            true,
+                            Jupyter.notebook.metadata.gist.data,
+                            new_data
+                        );
+                        // prevent the modal from closing. See github.com/twbs/bootstrap/issues/1202
+                        modal.data('bs.modal').isShown = false;
+                        var spinner = modal.find('.btn-primary .fa-github').addClass('fa-spin');
+                        make_gist(function (jqXHR, textStatus) {
+                            modal.find('.btn').prop('disabled', false);
+                            // allow the modal to close again. See github.com/twbs/bootstrap/issues/1202
+                            modal.data('bs.modal').isShown = true;
+                            spinner.removeClass('fa-spin');
+                            if (modal.find('.alert').length === 0) {
+                                modal.modal('hide');
+                            }
+                        });
+                    }
+                },
+                done: {}
+            }
+        })
+        .attr('id', 'gist_modal')
+        .on('shown.bs.modal', update_link);
+
+        modal.find('.btn-primary').prepend(
+            $('<i/>')
+                .addClass('fa fa-lg fa-github')
+        );
+
+        modal.modal('show');
+    }
+
+    var make_gist = function make_gist (complete_callback) {
+        ensure_default_metadata();
+
+        var data = $.extend(
+            true, // deep-copy
+            { files: {} }, // defaults
+            Jupyter.notebook.metadata.gist.data // overrides
+        );
         var filename = Jupyter.notebook.notebook_name;
         data.files[filename] = {
             content: JSON.stringify(Jupyter.notebook.toJSON(), null, 2)
         };
 
+        var id = params.gist_it_personal_access_token !== '' ? Jupyter.notebook.metadata.gist.id : '';
+        var method = id ? 'PATCH' : 'POST';
+
         // Create/edit the Gist
         $.ajax({
-            url: 'https://api.github.com/gists',
-            type: 'POST',
+            url: 'https://api.github.com/gists' + (id ? '/' + id : ''),
+            type: method,
             dataType: 'json',
             data: JSON.stringify(data),
             beforeSend: add_auth_token,
-            success: show_gist_link
+            success: gist_success,
+            error: gist_error,
+            complete: complete_callback
         });
     };
 
-    var load_jupyter_extension = function load_ipython_extension () {
+    function load_jupyter_extension () {
         config.load();
-    };
+    }
 
     return {
         load_jupyter_extension: load_jupyter_extension,
---
+++ nbextensions/publishing/gist_it/readme.md
@@ -0,0 +1,58 @@
+Publish notebooks as Github gists with a single button click!
+
+
+Authentication
+==============
+
+The extension works with no special settings, publishing anonymous,
+non-modifiable gGists.
+
+To create gists owned by your Github user, or to modify existing gists (useful
+for multiple revisions of a notebook!), you need to be authenticate with Github.
+
+
+Anonymous
+---------
+
+You can publish anaonymous gists without any authentication (the default mode).
+Anonymous gists can't be edited, so every time you click the button, a new gist
+is created.
+
+
+
+Personal access tokens
+----------------------
+
+At the moment, the only supported method of authentication is client-side,
+using Github _personal access tokens_.
+
+__Important:__ using personal access token authentication __only makes sense if
+you are the only user of the notebook server__, and control the server.
+Otherwise, other users of the server may use your token
+(either accidentally or maliciously) to create/edit/delete gists,
+or exercise any other permissions you might have given to the token.
+If the server is only for your personal use, then you can create a github
+personal access token at https://github.com/settings/tokens.
+It makes sense to only grant the token the minimum permissions (scopes)
+necessary for the extension to work, in this case, the `gists` scope.
+Once you've got your token from Github, enter it on the
+[extension configuration page](../../nbextensions#_nbext-ext-Gist-it) for it to
+be stored in the server config.
+
+
+Full Github OAuth
+-----------------
+
+Github's full OAuth authentication
+(which would be required to make this extension useful for authenticating users
+in a multi-notebook setup)
+requires some server-side code.
+There are some issues with implementing this directly (
+essentially related to a secret which the App server must know, and can't be
+published as part of open-source code, see [here]() for details
+) as a Jupyter extension.
+I ([@jcb91](https://github.com/jcb91)) started writing this `Gist it` extension
+to work with a 3rd-party authentication app, but didn't finish it or test it,
+so it's not functional at the moment.
+If you'd like to have the full OAuth model, I'd be happy to help with any
+attempt you make - drop me a line on github.
