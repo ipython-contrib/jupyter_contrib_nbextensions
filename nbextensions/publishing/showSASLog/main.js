4c2e48ecf473bdac1b10103f36ddc189aec18d33
jld23 <jadean@d7b445.na.SAS.com>
2016-02-02 14:18:55 -0500

    clean up comments and extra code

--- nbextensions/publishing/showSASLog/main.js
+++ nbextensions/publishing/showSASLog/main.js
@@ -6,18 +6,11 @@ define([
     'base/js/utils'
 ], function(IPython, $, configmod, events, utils) {
     "use strict";
-
-    var extension = '.html';
-    var open_tab = true;
-    var base_url = utils.get_body_data("baseUrl");
-    var config = new configmod.ConfigSection('notebook', {base_url: base_url});
-
-
+    //var base_url = utils.get_body_data("baseUrl");
+    //var config = new configmod.ConfigSection('notebook', {base_url: base_url});
     
     function handle_output(out){
-       //console.log(out);
        var res = null;
-       // if output is a python object
        if(out.msg_type === "execute_result"){
            res = out.content.data["text/plain"];
        var logWindow = window.open('SASLog.html','_blank');
@@ -25,7 +18,6 @@ define([
        // strip the leading and trailing "'"
        logWindow.document.write(res.substring(1,res.length-1));
        logWindow.document.close();
-       //console.log(res);
        }
     }
 
@@ -35,36 +27,6 @@ define([
         var kernel = IPython.notebook.kernel;
         var callbacks = { 'iopub' : {'output' : handle_output}};
         var msg_id = kernel.execute(code_input, callbacks, {silent:false});
-        //console.log(msg_id, kernel.id);
-
- //       var kernel = IPython.notebook.kernel;
- //       var name = IPython.notebook.notebook_name;
- //       var id = IPython.notebook.kernel.id;
- //       var command ='showSASLog_11092015';
- //       console.log(command+' '+id);
-        function callback() {
-            if (open_tab === true) {
-                var url = name.split('.ipynb')[0] + extension;
-                console.log("before msg")
-                console.log(msg)
-                //console.log(msg.content.payload[0].tostring())
-                console.log("after msg")
-                var SASLog = `
-                <html>
-                <header><title>This is title</title></header>
-                <body>
-                Hello world
-                </body>
-                </html>
-                `;
-                var logWindow = window.open('SASLog.html','_blank');
-                logWindow.document.open();
-                logWindow.document.write(SASLog);
-                logWindow.document.close();
-            }
-        }
-//        kernel.execute(command, { shell: { reply : callback } });
-        //$('#showSASLog').blur()
     };
 
     var load_ipython_extension = function() {
