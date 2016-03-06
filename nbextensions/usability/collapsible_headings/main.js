d4bc25419d5dcbb70fd36300cafd48add9b35b3f
Juergen Hasch <python@elbonia.de>
2016-03-06 20:40:36 +0100

    Only toggle collapse when clicking on icon directly

--- nbextensions/usability/collapsible_headings/main.js
+++ nbextensions/usability/collapsible_headings/main.js
@@ -155,8 +155,9 @@ define([
 						.addClass('collapsible_headings_toggle')
 						.css('color', params.collapsible_headings_toggle_color)
 						.append('<div><i class="fa fa-fw"></i></div>')
-						.on('click', function () { toggle_heading(cell); })
 						.appendTo(cell.element.find('.input_prompt'));
+						var icon = cht.find('i');
+						icon.on('click', function () { toggle_heading(cell);})                         
 					if (params.collapsible_headings_make_toggle_controls_buttons) {
 						cht.addClass('btn btn-default');
 					}
