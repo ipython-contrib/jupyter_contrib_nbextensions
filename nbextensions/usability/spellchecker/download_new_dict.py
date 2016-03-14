c0cee44393f0e04d2aebbf9b87bb64e19f1339da
Josh Barnes <joshuacookebarnes@gmail.com>
2016-03-14 16:00:54 +0000

    fix bug in spellchecker language downloader, I hadn't realised that chromium only supplies base64-encoded text

--- nbextensions/usability/spellchecker/download_new_dict.py
+++ nbextensions/usability/spellchecker/download_new_dict.py
@@ -1,15 +1,17 @@
 from __future__ import print_function
 import os.path
 import sys
+import base64
 from jupyter_core.paths import jupyter_data_dir
 from notebook.services.config import ConfigManager
+
 cm = ConfigManager()
 
 try:
-    from urllib.request import urlretrieve # Py3
+    from urllib.request import urlopen # Py3
     from urllib.parse import urljoin
 except ImportError:
-    from urllib import urlretrieve # Py2
+    from urllib import urlopen # Py2
     from urlparse import urljoin
 
 remote_base_url = 'https://chromium.googlesource.com/chromium/deps/hunspell_dictionaries/+/master'
@@ -19,11 +21,11 @@ lang_code = 'de_DE'
 
 for ext in ('dic', 'aff'):
     dict_fname = lang_code + '.' + ext
-    remote_path = remote_base_url + '/' + dict_fname
+    remote_path = remote_base_url + '/' + dict_fname + '?format=TEXT'
     local_path = os.path.join(local_base_url, dict_fname)
-
     print('saving {!r}\n    to {!r}'.format(remote_path, local_path))
-    urlretrieve(remote_path, local_path)
+    with open(local_path, 'wb') as loc_file:
+        base64.decode(urlopen(remote_path), loc_file)
     rel_path = './typo/dictionaries/' + dict_fname
     cm.update('notebook', {'spellchecker': {ext + '_url': rel_path}})
 
