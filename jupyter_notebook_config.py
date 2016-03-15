286e8cf40a0fd56f4c83c7c82e1476574315d68b
Josh Barnes <joshuacookebarnes@gmail.com>
2016-03-15 15:27:53 +0000

    remove old, unused configuration scripts

--- jupyter_notebook_config.py
+++
@@ -1,9 +0,0 @@
-from jupyter_core.paths import jupyter_config_dir, jupyter_data_dir
-import os
-import sys
-
-sys.path.append(os.path.join(jupyter_data_dir(), 'extensions'))
-
-c = get_config()
-c.NotebookApp.extra_template_paths = [os.path.join(jupyter_data_dir(),'templates') ]
-
