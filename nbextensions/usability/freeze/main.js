54b4977bd5804926545f1056c5a3c4ff0f6b6500
Anastasia Kukanova <kukanova.anastasia@gmail.com>
2016-03-17 23:13:17 +0300

    [Freeze] Initial commit

---
+++ nbextensions/usability/freeze/main.js
@@ -0,0 +1,126 @@
+define([
+	'base/js/namespace',
+	'base/js/events',
+	'notebook/js/codecell'
+], function (
+	Jupyter,
+	events,
+	codecell
+){
+	'use strict';
+	
+	var CodeCell = codecell.CodeCell;
+	
+	function patch_CodeCell_execute () {
+		console.log('[Freeze] patching CodeCell.prototype.execute')	
+		CodeCell.prototype.old_execute = CodeCell.prototype.execute;
+		
+		CodeCell.prototype.execute = function () {
+			if (!this.metadata.run_control.frozen) {
+				this.old_execute(arguments);
+			}
+		}
+	}
+	
+	function set_state(cell, state) { 
+		if (cell instanceof CodeCell) {
+			if (cell.metadata.run_control == undefined)
+				cell.metadata.run_control = {};
+			if (cell.metadata.run_control.frozen == undefined)
+				cell.metadata.run_control.frozen = false;
+			if (cell.metadata.run_control.read_only === undefined)
+				cell.metadata.run_control.read_only = false;
+			if (state == undefined)
+				state = 'normal';			
+			switch(state) {
+				case 'normal':
+					cell.metadata.run_control.frozen = false;
+					cell.metadata.run_control.read_only = false;
+					var bg = "#f5f5f5";
+					break;
+				case 'read_only':
+					cell.metadata.run_control.frozen = false;
+					cell.metadata.run_control.read_only = true;
+					var bg = "#FFFEF0";
+					break;
+				case 'frozen':
+					cell.metadata.run_control.frozen = true;
+					cell.metadata.run_control.read_only = true;
+					var bg = "#f0feff";
+					break;
+			}	
+			cell.code_mirror.setOption('readOnly', cell.metadata.run_control.read_only);
+			var prompt = cell.element.find('div.input_area');
+			prompt.css("background-color", bg);
+		}
+	}
+
+	function set_state_selected (state) {
+		var cells = Jupyter.notebook.get_selected_cells();
+		for (var i = 0; i < cells.length; i++) {
+			set_state(cells[i], state)
+		}
+	}
+	
+	function make_normal_selected () {
+		set_state_selected('normal');
+	}
+
+	function make_read_only_selected () {
+		set_state_selected('read_only');
+	}
+	
+	function make_frozen_selected () {
+		set_state_selected('frozen');
+	}
+	
+	
+	function init () {
+		var cells = Jupyter.notebook.get_cells();
+		for (var i in cells) {
+			var cell = cells[i];
+			if (cell instanceof CodeCell) {
+				if (cell.metadata.run_control != undefined) {
+					if (cell.metadata.run_control.read_only) {
+						if (cell.metadata.run_control.frozen) {
+							set_state(cell, 'frozen')
+						} else {
+							set_state(cell, 'read_only')
+						}
+					} else {
+						set_state(cell, 'normal')
+					}
+				} else {
+					set_state(cell, 'normal');
+				}
+			}
+		}
+	}
+	
+	
+	Jupyter.toolbar.add_buttons_group([
+		{
+			id : 'make_normal', 
+			label : 'lift restrictions from selected cells',
+			icon : 'fa-unlock-alt',
+			callback : make_normal_selected
+		},
+		{
+			id : 'make_read_only',
+			label : 'make selected cells read-only',
+			icon: 'fa-lock',
+			callback : make_read_only_selected
+			
+		},
+		{
+			id : 'freeze_cells', 
+			label : 'freeze selected cells',
+			icon : 'fa-asterisk',
+			callback : make_frozen_selected
+		}
+	]);
+	
+	init();
+	
+	patch_CodeCell_execute();
+});
