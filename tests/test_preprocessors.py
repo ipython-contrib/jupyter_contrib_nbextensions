bc8b38335192254659cd7d3ea908434273026ea5
Josh Barnes <joshuacookebarnes@gmail.com>
2016-04-25 12:02:56 +0100

    update preprocessor tests * consolidate common operations into functions * move svg2pdf inkscape-finding into a function which doesn't raise exceptions, allowing us to skip test if inkscape can't be found * use urllib instead of requests in svg2pdf preprocessor

--- tests/test_preprocessors.py
+++ tests/test_preprocessors.py
@@ -1,56 +1,62 @@
 ﻿# -*- coding: utf-8 -*-
+"""Tests for themysto.preprocessors."""
+
 import os
-import sys
 
 import nbformat
 from nbconvert import LatexExporter, RSTExporter
+from nose.plugins.skip import SkipTest
+from nose.tools import assert_in, assert_not_in, assert_true
 from traitlets.config import Config
 
-sys.path.append('extensions')
-c = Config()
+
+def path_in_data(rel_path):
+    """Return an absolute path from a relative path in tests/data."""
+    return os.path.join(os.path.dirname(__file__), 'data', rel_path)
+
+
+def export_through_preprocessor(
+        preproc_rel_name, nb_rel_path, exporter_class, export_format):
+    """Export a notebook through a given preprocessor."""
+    nb_path = path_in_data(nb_rel_path)
+    notebook_node = nbformat.read(nb_path, as_version=4)
+    exporter = exporter_class(
+        preprocessors=['themysto.preprocessors.' + preproc_rel_name],
+        config=Config(NbConvertApp={'export_format': export_format}))
+    return exporter.from_notebook_node(notebook_node)
 
 
 def test_pymarkdown_preprocessor():
-    """ Test python markdown preprocessor """
-    nb_name = 'tests/data/pymarkdown.ipynb'
-    with open(nb_name, 'r') as f:
-        notebook_json = f.read()
-    notebook = nbformat.reads(notebook_json, as_version=4)
-    c.RSTExporter.preprocessors = ["pre_pymarkdown.PyMarkdownPreprocessor"]
-    c.NbConvertApp.export_format = 'rst'
-    rst_exporter = RSTExporter(config=c)
-    body = rst_exporter.from_notebook_node(notebook)
-    with open('test.txt', 'wb') as f:
-        f.write(body[0].encode('utf8'))
-    assert 'Hello world' in body[0]
-    pass
+    """Test python markdown preprocessor."""
+    body = export_through_preprocessor(
+        'pre_pymarkdown.PyMarkdownPreprocessor', 'pymarkdown.ipynb',
+        RSTExporter, 'rst')
+    expected = 'Hello world'
+    assert_in(
+        expected, body[0], 'first cell should contain {}'.format(expected))
 
 
 def test_codefolding():
-    """ Test codefolding preprocessor """
-    nb_name = 'tests/data/codefolding.ipynb'
-    with open(nb_name, 'r') as f:
-        notebook_json = f.read()
-    notebook = nbformat.reads(notebook_json, as_version=4)
-    c.RSTExporter.preprocessors = ["pre_codefolding.CodeFoldingPreprocessor"]
-    c.NbConvertApp.export_format = 'rst'
-    rst_exporter = RSTExporter(config=c)
-    body = rst_exporter.from_notebook_node(notebook)
-    assert 'AXYZ12AXY' not in body[0]  # firstline fold
-    assert 'GR4CX32ZT' not in body[0]  # function fold
+    """Test codefolding preprocessor."""
+    body = export_through_preprocessor(
+        'pre_codefolding.CodeFoldingPreprocessor', 'codefolding.ipynb',
+        RSTExporter, 'rst')
+    assert_not_in('AXYZ12AXY', body[0], 'check firstline fold has worked')
+    assert_not_in('GR4CX32ZT', body[0], 'check function fold has worked')
 
 
 def test_svg2pdf_preprocessor():
-    """ Test svg2pdf preprocessor for markdown cell images """
-    nb_name = 'tests/data/svg2pdf.ipynb'
-    pdf_file = 'tests/data/test.pdf'
-    with open(nb_name, 'r') as f:
-        notebook_json = f.read()
-    notebook = nbformat.reads(notebook_json, as_version=4)
-    c.LatexExporter.preprocessors = ["pre_svg2pdf.SVG2PDFPreprocessor"]
-    c.NbConvertApp.export_format = 'latex'
-    latex_exporter = LatexExporter(config=c)
-    body = latex_exporter.from_notebook_node(notebook)
-    assert os.path.isfile(pdf_file)
-    os.remove(pdf_file)
-    assert 'test.pdf' in body[0]
+    """Test svg2pdf preprocessor for markdown cell svg images."""
+    from themysto.preprocessors.pre_svg2pdf import get_inkscape_executable_path
+    if get_inkscape_executable_path() is None:
+        raise SkipTest('No inkscape executable found')
+    body = export_through_preprocessor(
+        'pre_svg2pdf.SVG2PDFPreprocessor', 'svg2pdf.ipynb',
+        LatexExporter, 'latex')
+
+    pdf_path = path_in_data('test.pdf')
+    assert_true(os.path.isfile(pdf_path), 'exported pdf should exist')
+    if os.path.isfile(pdf_path):
+        os.remove(pdf_path)
+    assert_in('test.pdf', body[0],
+              'exported pdf should be referenced in exported notebook')
