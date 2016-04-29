5b0ec20dbf6c9c05ca3f24d563a2d45ebc36ba1b
Josh Barnes <joshuacookebarnes@gmail.com>
2016-04-29 02:02:44 +0100

    test all possible installation modes

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,57 +1,116 @@
 # -*- coding: utf-8 -*-
 """Tests for the main themysto app."""
 
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals
+)
+
 import json
 import os
 import shutil
+import sys
 import tempfile
 from unittest import TestCase
 
 import nose.tools as nt
+from jupyter_core.paths import jupyter_config_dir, jupyter_data_dir
 from traitlets.config import Config
 from traitlets.tests.utils import check_help_all_output, check_help_output
 
+import themysto.install
 from themysto.application import main as main_app
-from themysto.application import InstallThemystoApp
+from themysto.application import (
+    InstallThemystoApp, UninstallThemystoApp, BaseThemystoApp,
+)
+
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch  # py2
+
+
+def reset_app_class(app_class):
+    """Reset all Themysto app traits and clear the instance."""
+    for name, traitlet in app_class._instance.traits().items():
+        if isinstance(traitlet.this_class, BaseThemystoApp):
+            setattr(app_class._instance, name, traitlet.default_value)
+    app_class.clear_instance()
 
 
 class AppTest(TestCase):
     """Tests for the main themysto app."""
 
-    env_vars = {'JUPYTER_CONFIG_DIR': 'config', 'JUPYTER_DATA_DIR': 'data'}
+    def make_dirs(self, base_dir):
+        """Return a dict of root, config and data directory paths."""
+        return {
+            'root': os.path.join(self.test_dir, base_dir),
+            'conf': os.path.join(self.test_dir, base_dir, 'config'),
+            'data': os.path.join(self.test_dir, base_dir, 'data'),
+        }
+
+    def remove_dirs(self):
+        """Remove any temporary directories created."""
+        shutil.rmtree(self.test_dir)
 
     def setUp(self):
         """Set up test fixtures."""
-        self.jupyter_dir = tempfile.mkdtemp(prefix='jupyter_')
+        self.test_dir = tempfile.mkdtemp(prefix='jupyter_')
+        self.patches = []
 
-    def tearDown(self):
-        """Tear down test fixtures."""
-        shutil.rmtree(self.jupyter_dir)
+        self.dirs = {
+            name: self.make_dirs(name) for name in (
+                'user_home', 'env_vars', 'system', 'sys_prefix', 'custom')}
 
-    def check_install(self, extra_args=None):
+        self.patches.append(patch.dict('os.environ', {
+            'HOME': self.dirs['user_home']['root'],
+            'JUPYTER_CONFIG_DIR': self.dirs['env_vars']['conf'],
+            'JUPYTER_DATA_DIR': self.dirs['env_vars']['data'],
+        }))
+
+        self.patches.append(patch.multiple(
+            sys.modules[themysto.install._get_config_dir.__module__],
+            SYSTEM_JUPYTER_PATH=[self.dirs['system']['data']],
+            SYSTEM_CONFIG_PATH=[self.dirs['system']['conf']],
+            ENV_JUPYTER_PATH=[self.dirs['sys_prefix']['data']],
+            ENV_CONFIG_PATH=[self.dirs['sys_prefix']['conf']],
+        ))
+
+        for ptch in self.patches:
+            ptch.start()
+            self.addCleanup(ptch.stop)
+        self.addCleanup(self.remove_dirs)
+
+    def check_install(self, argv=None, dirs=None):
         """Check files were installed in the correct place."""
-        if extra_args is None:
-            extra_args = []
-        data_dir = os.path.join(self.jupyter_dir, 'data')
-        conf_dir = os.path.join(self.jupyter_dir, 'config')
+        print('\n', file=sys.stderr)
+
+        if argv is None:
+            argv = []
+        if dirs is None:
+            dirs = {
+                'conf': jupyter_config_dir(),
+                'data': jupyter_data_dir(),
+            }
+        conf_dir, data_dir = dirs['conf'], dirs['data']
 
         # do install
-        main_app(argv=['install'] + extra_args)
+        main_app(argv=['install'] + argv)
 
         # list everything that got installed
         installed_files = []
-        for root, subdirs, files in os.walk(self.jupyter_dir):
+        for root, subdirs, files in os.walk(dirs['conf']):
             installed_files.extend([os.path.join(root, f) for f in files])
-        nt.assert_true(
-            installed_files,
-            'Install should create files in {}'.format(self.jupyter_dir))
+        for root, subdirs, files in os.walk(dirs['data']):
+            installed_files.extend([os.path.join(root, f) for f in files])
+        nt.assert_true(installed_files,
+                       'Install should create files in {} or {}'.format(
+                           dirs['conf'], dirs['data']))
 
         # a bit of a hack to allow initializing a new app instance
-        for cls in InstallThemystoApp._walk_mro():
-            cls._instance = None
+        reset_app_class(InstallThemystoApp)
 
         # do uninstall
-        main_app(argv=['uninstall'] + extra_args)
+        main_app(argv=['uninstall'] + argv)
         # check that nothing remains in the data directory
         data_installed = [
             path for path in installed_files
@@ -75,12 +134,14 @@ class AppTest(TestCase):
             nt.assert_false(
                 {k: v for k, v in nbservext.items() if v},
                 'Uninstall should disable all '
-                'nbserver_extensions file {}'.format(path))
+                'nbserver_extensions in file {}'.format(path))
             confstrip = {}
             confstrip.update(conf)
             confstrip.pop('NotebookApp', None)
             confstrip.pop('version', None)
             nt.assert_false(confstrip, 'Uninstall should leave config empty.')
+
+        reset_app_class(UninstallThemystoApp)
 
     def test_help_output(self):
         """Check that themysto app help works."""
@@ -91,23 +152,24 @@ class AppTest(TestCase):
         with nt.assert_raises(SystemExit):
             main_app()
 
-    def test_kwarg_install(self):
-        """Check that install works correctly using --config-dir arg."""
-        self.check_install([
-            '--config-dir=' + os.path.join(self.jupyter_dir, 'config')])
+    def test_default_install(self):
+        """Check that install works correctly using defaults."""
+        self.check_install()
 
-    def test_env_var_install(self):
-        """Check that install works correctly using environment variables."""
-        for var in self.env_vars:
-            new_dir = self.env_vars[var]
-            self.env_vars[var] = os.environ.get(var, None)
-            os.environ[var] = os.path.join(self.jupyter_dir, new_dir)
-            if not os.path.exists(os.environ[var]):
-                os.makedirs(os.environ[var])
-        try:
-            self.check_install()
-        finally:
-            for var, val in self.env_vars.items():
-                self.env_vars[var] = os.path.basename(os.environ.pop(var))
-                if val is not None:
-                    os.environ[var] = val
+    def test_user_install(self):
+        """Check that install works correctly using --user flag."""
+        self.check_install(argv=['--user'], dirs=self.dirs['env_vars'])
+
+    def test_sys_prefix_install(self):
+        """Check that install works correctly using --sys-prefix flag."""
+        self.check_install(argv=['--sys-prefix'], dirs=self.dirs['sys_prefix'])
+
+    def test_system_install(self):
+        """Check that install works correctly using --system flag."""
+        self.check_install(argv=['--system'], dirs=self.dirs['system'])
+
+    def test_config_dir_install(self):
+        """Check that install works correctly using --config-dir arg."""
+        self.check_install(
+            argv=['--config-dir=' + self.dirs['custom']['conf']],
+            dirs=self.dirs['custom'])
