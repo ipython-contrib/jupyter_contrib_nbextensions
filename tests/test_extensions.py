25345337d05f77dbe927fb25aab47880e061d27f
Josh Barnes <joshuacookebarnes@gmail.com>
2016-05-04 02:12:21 +0100

    add simple tests for module imports

---
+++ tests/test_extensions.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Tests for themysto jupyter server extensions.
+
+Empty until somebody figures out what to put in here.
+"""
+
+
+def test_import_extensions():
+    import themysto.extensions  # noqa
---
+++ tests/test_postprocessors.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Empty until somebody figures out what to put in here"""
+
+
+def test_import_postprocessors():
+    import themysto.postprocessors  # noqa
