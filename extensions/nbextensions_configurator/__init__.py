52ba5cc0aea40b7030c20bd68e8cf567abd62bd3
Josh Barnes <joshuacookebarnes@gmail.com>
2016-05-19 23:49:49 +0100

    get nbextensions_path from web_app.settings if possible

--- extensions/nbextensions_configurator/__init__.py
+++ extensions/nbextensions_configurator/__init__.py
@@ -39,22 +39,33 @@ def get_nbextensions_path():
     or, if we can't instantiate one
       - the default config used, found by spawning a subprocess
     """
-    # Attempt to get the path for the currently-running config, or the default
+    # Attempt to get the path for the currently-running server, or the default
     # if one isn't running.
     # If there's already a non-NotebookApp traitlets app running, e.g. when
     # we're inside an IPython kernel there's a KernelApp instantiated, then
     # attempting to get a NotebookApp instance will raise a
     # MultipleInstanceError.
     try:
-        return NotebookApp.instance().nbextensions_path
+        nbapp = NotebookApp.instance()
     except MultipleInstanceError:
         # So, we spawn a new python process to get paths for the default config
         cmd = "from {0} import {1}; [print(p) for p in {1}()]".format(
             get_nbextensions_path.__module__, get_nbextensions_path.__name__)
 
-        return subprocess.check_output([
+        nbext_path = subprocess.check_output([
             sys.executable, '-c', cmd
         ]).decode(sys.stdout.encoding).split('\n')
+    else:
+        # try to get web_app.settings if possible (e.g. when a NotebookApp
+        # instance is running correctly).
+        try:
+            webapp = nbapp.web_app
+        except AttributeError:
+            nbext_path = nbapp.nbextensions_path
+        else:
+            nbext_path = webapp.settings['nbextensions_path']
+    finally:
+        return nbext_path
 
 
 def get_configurable_nbextensions(
