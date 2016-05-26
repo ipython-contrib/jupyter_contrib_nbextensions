d02c30b774139d941c173ec19b1fb6694ce52c3f
Josh Barnes <joshuacookebarnes@gmail.com>
2016-05-26 01:12:53 +0100

    fix for py27 io.write

--- tests/test_nbextensions_configurator.py
+++ tests/test_nbextensions_configurator.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import, print_function
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals,
+)
 
 import io
 import json
