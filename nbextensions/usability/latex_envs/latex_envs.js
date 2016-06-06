30334d2c54a7aadbc076357dc511a066ca00e376
Josh Barnes <joshuacookebarnes@gmail.com>
2016-06-06 20:13:57 +0100

    use relative require paths in latex_env to fix issues with custom base_url e.g. https://github.com/ipython-contrib/IPython-notebook-extensions/issues/619

--- nbextensions/usability/latex_envs/latex_envs.js
+++ nbextensions/usability/latex_envs/latex_envs.js
@@ -45,11 +45,19 @@ Authors can be formatted according to the following keywords:
 // *****************************************************************************
 
 
-define(["require",
+define([
+    'require',
 	'base/js/namespace',
-	"nbextensions/usability/latex_envs/thmsInNb4",
-	"nbextensions/usability/latex_envs/bibInNb4",
-	"nbextensions/usability/latex_envs/initNb"], function (require,Jupyter, thmsInNb, bibsInNb,initNb) {    
+	'./thmsInNb4',
+	'./bibInNb4',
+	'./initNb'
+], function (
+    require,
+    Jupyter,
+    thmsInNb,
+    bibsInNb,
+    initNb
+) {
 
     var maps = initmap();
     environmentMap=maps[0];
