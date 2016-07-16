9e843f2875977d73e7fa432bf1e61463ea92c74f
Josh Barnes <joshuacookebarnes@gmail.com>
2016-07-16 03:04:39 +0100

    update preprocessor tests * consolidate common operations into functions * test imports in test_preprocessors * skip rather than fail on tests which require pandoc if we can't find it * move svg2pdf inkscape-finding into a function which doesn't raise exceptions, allowing us to skip test if inkscape can't be found, and skip accordingly * use urllib instead of requests in svg2pdf preprocessor * fix flake8 complaints

--- src/jupyter_contrib_nbextensions/nbconvert_support/pre_svg2pdf.py
+++ src/jupyter_contrib_nbextensions/nbconvert_support/pre_svg2pdf.py
@@ -6,28 +6,56 @@ This preprocessor converts svg graphics embedded in markdown as
 '![My graphic](graphics.svg)'
 to PDF using Inkscape
 """
+
+import errno
 import io
 import os
 import re
 import subprocess
 import sys
 
-import requests
+from ipython_genutils.py3compat import which
 from ipython_genutils.tempdir import TemporaryDirectory
 from nbconvert.preprocessors import Preprocessor
 from traitlets import Unicode
 
+try:
+    from urllib.request import urlopen  # py3
+except ImportError:
+    from urllib2 import urlopen  # py2
 
-INKSCAPE_APP = '/Applications/Inkscape.app/Contents/Resources/bin/inkscape'
 
-if sys.platform == "win32":
-    try:
-        import winreg
-    except ImportError:
-        import _winreg as winreg
+def get_inkscape_executable_path():
+    """Return the path of the system inkscape exectuable."""
+    inkscape = which('inkscape')
+    if inkscape is not None:
+        return inkscape
+
+    if sys.platform == 'darwin':
+        default_darwin_inkscape_path = (
+            '/Applications/Inkscape.app/Contents/Resources/bin/inkscape')
+        if os.path.isfile(default_darwin_inkscape_path):
+            return default_darwin_inkscape_path
+    elif sys.platform == 'win32':
+        try:
+            import winreg  # py3 stdlib on windows
+        except ImportError:
+            import _winreg as winreg  # py2 stdlib on windows
+        win32_inkscape_reg_key = "SOFTWARE\\Classes\\inkscape.svg\\DefaultIcon"
+        wr_handle = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+        try:
+            rkey = winreg.OpenKey(wr_handle, win32_inkscape_reg_key)
+            inkscape = winreg.QueryValueEx(rkey, "")[0]
+        except OSError as ex:
+            if ex.errno == errno.ENOENT:
+                return None
+            raise
+        return inkscape
+    return None
 
 
 class SVG2PDFPreprocessor(Preprocessor):
+    """Preprocessor to convert svg graphics embedded in markdown to PDF."""
 
     def _from_format_default(self):
         return 'image/svg+xml'
@@ -52,30 +80,21 @@ class SVG2PDFPreprocessor(Preprocessor):
     def _command_default(self):
         return (
             self.inkscape +
-            ' --without-gui --export-pdf="{to_filename}" "{from_filename}"')
+            ' --without-gui --export-pdf="{to_filename}" "{from_filename}"'
+        )
 
     inkscape = Unicode(config=True, help="The path to Inkscape, if necessary")
 
     def _inkscape_default(self):
-        if sys.platform == "darwin":
-            if os.path.isfile(INKSCAPE_APP):
-                return INKSCAPE_APP
-        if sys.platform == "win32":
-            wr_handle = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
-            try:
-                rkey = winreg.OpenKey(
-                    wr_handle, "SOFTWARE\\Classes\\inkscape.svg\\DefaultIcon")
-                inkscape = winreg.QueryValueEx(rkey, "")[0]
-            except FileNotFoundError:
-                raise FileNotFoundError("Inkscape executable not found")
-            return inkscape
-        return "inkscape"
+        return get_inkscape_executable_path()
 
     def convert_figure(self, name, data):
-        """Convert a single SVG figure to PDF.  Returns converted data."""
+        """Convert a single SVG figure to PDF. Returns converted data."""
         # self.log.info(name, data)
-        # Work in a temporary directory
+        if not self.inkscape:
+            raise OSError('Inkscape executable not found')
         # self.log.info('Inkscape:', self.inkscape)
+        # Work in a temporary directory
         with TemporaryDirectory() as tmpdir:
             # Write fig to temp file
             input_filename = os.path.join(tmpdir, 'figure' + '.svg')
@@ -117,12 +136,12 @@ class SVG2PDFPreprocessor(Preprocessor):
         """
         url = match.group(2) + '.svg'
         if url.startswith('http'):
-            data = requests.get(url)
+            data = urlopen(url).read()
         else:
             with open(url, 'rb') as f:
                 data = f.read()
         if (self.output_files_dir is not None and
-                os.path.exists(self.output_files_dir) is False):
+                not os.path.exists(self.output_files_dir)):
             os.makedirs(self.output_files_dir)
         else:
             self.output_files_dir = ''
