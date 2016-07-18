4bc65651e3254325d9e69869dd53e2e90e651d04
Josh Barnes <joshuacookebarnes@gmail.com>
2016-07-18 01:04:30 +0100

    fixes for nbconvert_support package * add function to find templates directory from nbconvert_support package * update config_scripts to use new package structure * update some readme files which refer to templates directory

--- src/jupyter_contrib_nbextensions/config_scripts/highlight_latex_cfg.py
+++ src/jupyter_contrib_nbextensions/config_scripts/highlight_latex_cfg.py
@@ -1,14 +1,19 @@
-from jupyter_core.paths import jupyter_data_dir
 import os
-import sys
 
-sys.path.append(os.path.join(jupyter_data_dir(), 'extensions'))
+import jupyter_contrib_nbextensions.nbconvert_support
+
+jcnbe_dir = os.path.dirname(jupyter_contrib_nbextensions.__file__)
+pp_mod_name = 'jupyter_contrib_nbextensions.nbconvert_support.pp_highlighter'
 
 c = get_config()  # noqa
 c.NbConvertApp.export_format = "latex"
-c.Exporter.template_path = ['.', os.path.join(jupyter_data_dir(), 'templates')]
-c.Exporter.preprocessors = ['pp_highlighter.HighlighterPreprocessor']
-c.NbConvertApp.postprocessor_class = 'pp_highlighter.HighlighterPostProcessor'
+c.Exporter.template_path = [
+    '.',
+    jupyter_contrib_nbextensions.nbconvert_support.templates_directory(),
+    os.path.join(jcnbe_dir, 'nbextensions', 'highlighter')
+]
+c.Exporter.preprocessors = [pp_mod_name + '.HighlighterPreprocessor']
+c.NbConvertApp.postprocessor_class = pp_mod_name + '.HighlighterPostProcessor'
 # latex
 c.Exporter.template_file = 'highlighter.tplx'
 # html
