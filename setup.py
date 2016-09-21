ee6e8a86b210ddd00a2e778a9c9b0b832375c60c
Josh Barnes <joshuacookebarnes@gmail.com>
2016-09-21 12:23:07 +0100

    also remove latex_envs entry-points

--- setup.py
+++ setup.py
@@ -90,11 +90,9 @@ encounter any problems.
                 'nbextension = jupyter_contrib_nbextensions.application:jupyter_contrib_core_app_subcommands',  # noqa
             ],
             'nbconvert.exporters': [
-                'html_lenvs = jupyter_contrib_nbextensions.nbconvert_support.latex_envs:LenvsHTMLExporter',  # noqa
-                'latex_lenvs = jupyter_contrib_nbextensions.nbconvert_support.latex_envs:LenvsLatexExporter',  # noqa
                 'html_toc = jupyter_contrib_nbextensions.nbconvert_support.toc2:TocExporter',  # noqa
                 'html_embed = jupyter_contrib_nbextensions.nbconvert_support.embedhtml:EmbedHTMLExporter',  # noqa
-                 ],
+            ],
         },
         scripts=[os.path.join('scripts', p) for p in [
             'jupyter-contrib-nbextension',
