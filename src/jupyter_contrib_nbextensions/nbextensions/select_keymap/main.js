6aed1a8f33d79a0e7694af89e0a1350b61e2f4f7
Diego Fernandez <difernan@redhat.com>
2016-09-26 15:29:44 -0400

    Make better use of APIs
    Also made minor formatting changes

--- src/jupyter_contrib_nbextensions/nbextensions/select_keymap/main.js
+++ src/jupyter_contrib_nbextensions/nbextensions/select_keymap/main.js
@@ -12,13 +12,13 @@ define([
 ], function(Jupyter, utils, Cell, configmod, CodeMirror) {
     "use_strict";
 
-    var previous_mode = '';
+    var previous_mode = "";
 
     var available_keymaps = [
-        'default',
-        'emacs',
-        'vim',
-        'sublime'
+        "default",
+        "emacs",
+        "vim",
+        "sublime"
     ];
 
     var starting_state = {
@@ -31,96 +31,75 @@ define([
     var edit_shortcuts = Jupyter.keyboard_manager.edit_shortcuts;
     var command_shortcuts = Jupyter.keyboard_manager.command_shortcuts;
 
-    var modes = {
+    var mods = {
         vim: {
-            extraKeys: {
-                "Esc": "leave_current_mode",
-                "Ctrl-[": "leave_current_mode"
+            add: {
+                extraKeys: {
+                    "Esc": "leave_current_mode",
+                    "Ctrl-[": "leave_current_mode"
+                },
+                command_shortcuts: {
+                    "ctrl-c": "jupyter-notebook:interrupt-kernel",
+                    "ctrl-z": "jupyter-notebook:restart-kernel",
+
+                    "d,d": "jupyter-notebook:cut-cell",
+                    "y,y": "jupyter-notebook:copy-cell",
+                    "u": "jupyter-notebook:undo-cell-deletion",
+
+                    "p": "jupyter-notebook:paste-cell-below",
+                    "shift-p": "jupyter-notebook:paste-cell-above",
+
+                    "o": "jupyter-notebook:insert-cell-below",
+                    "shift-o": "jupyter-notebook:insert-cell-above",
+
+                    "i": "jupyter-notebook:enter-edit-mode",
+                    "enter": "jupyter-notebook:enter-edit-mode",
+
+                    "shift-j": "jupyter-notebook:move-cell-down",
+                    "shift-k": "jupyter-notebook:move-cell-up",
+
+                    "shift-/": "jupyter-notebook:show-keyboard-shortcuts",
+                    "h": "jupyter-notebook:toggle-cell-output-collapsed",
+                    "shift-h": "jupyter-notebook:toggle-cell-output-scrolled",
+
+                    "`": "jupyter-notebook:change-cell-to-code",
+                    "0": "jupyter-notebook:change-cell-to-markdown"
+                },
+                edit_shortcuts: {
+                    "shift-esc": "jupyter-notebook:enter-command-mode"
+                }
             },
-            command_shortcuts: {
-                "ctrl-c": "jupyter-notebook:interrupt-kernel",
-                "ctrl-z": "jupyter-notebook:restart-kernel",
-
-                "d,d": "jupyter-notebook:cut-cell",
-                "y,y": "jupyter-notebook:copy-cell",
-                "u": "jupyter-notebook:undo-cell-deletion",
-
-                "p": "jupyter-notebook:paste-cell-below",
-                "shift-p": "jupyter-notebook:paste-cell-above",
-
-                "o": "jupyter-notebook:insert-cell-below",
-                "shift-o": "jupyter-notebook:insert-cell-above",
-
-                "i": "jupyter-notebook:enter-edit-mode",
-                "enter": "jupyter-notebook:enter-edit-mode",
-
-                "shift-j": "jupyter-notebook:move-cell-down",
-                "shift-k": "jupyter-notebook:move-cell-up",
-
-                "shift-/": "jupyter-notebook:show-keyboard-shortcuts",
-                "h": "jupyter-notebook:toggle-cell-output-collapsed",
-                "shift-h": "jupyter-notebook:toggle-cell-output-scrolled",
-
-                "`": "jupyter-notebook:change-cell-to-code",
-                "0": "jupyter-notebook:change-cell-to-markdown"
-            },
-            edit_shortcuts: {
-                "shift-esc": "jupyter-notebook:enter-command-mode",
-                "esc": undefined
+            remove: {
+                edit_shortcuts: ["esc"]
             },
             custom: function() {
-                // Disable keyboard manager for code mirror dialogs, handles ':' triggered ex-mode dialog box in vim mode.
-                // Manager is re-enabled by re-entry into notebook edit mode + cell normal mode after dialog closes
+                // Disable keyboard manager for code mirror dialogs, handles ':'
+                // triggered ex-mode dialog box in vim mode.
+                // Manager is re-enabled by re-entry into notebook edit mode +
+                // cell normal mode after dialog closes
+                this.openDialog = CodeMirror.prototype.openDialog;
+
                 function openDialog_keymap_wrapper(target, template, callback, options) {
                     Jupyter.keyboard_manager.disable();
                     return target.call(this, template, callback, options);
                 }
-                CodeMirror.defineExtension("openDialog", _.wrap(CodeMirror.prototype.openDialog,
+
+                CodeMirror.defineExtension("openDialog", _.wrap(this.openDialog,
                     openDialog_keymap_wrapper));
-
-                function flatten_shortcuts(shortcut_tree) {
-                    var result = {};
-                    for (var p in shortcut_tree) {
-                        if (typeof(shortcut_tree[p]) == "string") {
-                            result[p] = shortcut_tree[p];
-                        } else {
-                            var subresult = flatten_shortcuts(shortcut_tree[p]);
-                            for (var subp in subresult) {
-                                result[p + "," + subp] = subresult[subp];
-                            }
-                        }
-                    }
-                    return result;
-                }
-
-                function update_shortcuts(shortcut_manager, updated_shortcuts) {
-                    var current_shortcuts = _.invert(flatten_shortcuts(shortcut_manager._shortcuts));
-
-                    for (var shortcut_action in _.invert(updated_shortcuts)) {
-                        if (_.has(current_shortcuts, shortcut_action) &&
-                            shortcut_manager.get_shortcut(current_shortcuts[shortcut_action])) {
-
-                            shortcut_manager.remove_shortcut(current_shortcuts[shortcut_action]);
-                        }
-                    }
-
-                    shortcut_manager.add_shortcuts(updated_shortcuts);
-                }
-                update_shortcuts(command_shortcuts, this.command_shortcuts);
             },
             custom_teardown: function() {
-                CodeMirror.defineExtension("openDialog", CodeMirror.prototype.openDialog);
+                CodeMirror.defineExtension("openDialog", this.openDialog);
             }
         },
         emacs: {
-            extraKeys: {
-                "Ctrl-Y": "yank_no_selection"
+            add: {
+                extraKeys: {
+                    "Ctrl-Y": "yank_no_selection"
+                }
             },
-            edit_shortcuts: {
-                'ctrl-shift-minus': undefined
-            },
-            custom: function() {
-                delete CodeMirror.keyMap.emacs["Ctrl-V"];
+            remove: {
+                edit_shortcuts: ["ctrl-shift-minus"],
+                keyMap: ["Ctrl-V"]
             }
         }
     };
@@ -139,15 +118,44 @@ define([
     // make sure config is loaded before making changes
     config.loaded.then(function() {
         for (var key in conf) {
-            var config_key = 'select_keymap_' + key;
+            var config_key = "select_keymap_" + key;
             if (config.data.hasOwnProperty(config_key)) {
                 conf[key] = config.data[config_key];
             }
         }
         save_starting_state();
         // initialize last stored value or default
-        switch_keymap(window.localStorage.getItem('jupyter_keymap') || 'default');
+        switch_keymap(window.localStorage.getItem("jupyter_keymap") || "default");
     });
+
+    function flatten_shortcuts(shortcut_tree) {
+        var result = {};
+        for (var p in shortcut_tree) {
+            if (typeof(shortcut_tree[p]) == "string") {
+                result[p] = shortcut_tree[p];
+            } else {
+                var subresult = flatten_shortcuts(shortcut_tree[p]);
+                for (var subp in subresult) {
+                    result[p + "," + subp] = subresult[subp];
+                }
+            }
+        }
+        return result;
+    }
+
+    function update_shortcuts(shortcut_manager, updated_shortcuts) {
+        var current_shortcuts = _.invert(flatten_shortcuts(shortcut_manager._shortcuts));
+
+        for (var shortcut_action in _.invert(updated_shortcuts)) {
+            if (_.has(current_shortcuts, shortcut_action) &&
+                shortcut_manager.get_shortcut(current_shortcuts[shortcut_action])) {
+
+                shortcut_manager.remove_shortcut(current_shortcuts[shortcut_action]);
+            }
+        }
+
+        shortcut_manager.add_shortcuts(updated_shortcuts);
+    }
 
     // start custom CodeMirror command definitions
     function leave_current_mode(cm) {
@@ -208,17 +216,47 @@ define([
     function reset_state() {
         // FIXME: if this extension loads before other extensions that alter
         // key shortcuts, this will probably reset those as well
-        var options = modes[previous_mode];
         cm_default.extraKeys = _.clone(starting_state.extraKeys);
-        edit_shortcuts._shortcuts = _.clone(starting_state.edit_shortcuts);
-        command_shortcuts._shortcuts = _.clone(starting_state.command_shortcuts);
-        if (options && options.custom_teardown) options.custom_teardown();
+        update_shortcuts(edit_shortcuts, starting_state.edit_shortcuts);
+        update_shortcuts(command_shortcuts, starting_state.command_shortcuts);
+        var prev_mode_mods = mods[previous_mode];
+        if (prev_mode_mods && prev_mode_mods.custom_teardown)
+            prev_mode_mods.custom_teardown();
     }
 
     function save_starting_state() {
         starting_state.extraKeys = _.clone(cm_default.extraKeys);
-        starting_state.edit_shortcuts = _.clone(edit_shortcuts._shortcuts);
-        starting_state.command_shortcuts = _.clone(command_shortcuts._shortcuts);
+        starting_state.edit_shortcuts = flatten_shortcuts(edit_shortcuts._shortcuts);
+        starting_state.command_shortcuts = flatten_shortcuts(command_shortcuts._shortcuts);
+    }
+
+    function add_bindings(add, mode) {
+        _.extend(CodeMirror.keyMap[mode], add.keyMap);
+        _.extend(cm_default.extraKeys, add.extraKeys);
+
+        if (add.command_shortcuts)
+            update_shortcuts(command_shortcuts, add.command_shortcuts);
+
+        if (add.edit_shortcuts)
+            update_shortcuts(edit_shortcuts, add.edit_shortcuts);
+    }
+
+    function remove_bindings(remove, mode) {
+        _.forEach(remove.keyMap, function(key) {
+            delete CodeMirror.keyMap[mode][key];
+        });
+
+        _.forEach(remove.extraKeys, function(key) {
+            delete cm_default.extraKeys[key];
+        });
+
+        _.forEach(remove.edit_shortcuts, function(key) {
+            edit_shortcuts.remove_shortcut(key);
+        });
+
+        _.forEach(remove.command_shortcuts, function(key) {
+            command_shortcuts.remove_shortcut(key);
+        });
     }
 
     function change_defaults(mode) {
@@ -227,28 +265,37 @@ define([
         cm_default.vimMode = mode == 'vim';
         cm_default.lineWrapping = conf.line_wrap;
 
-        var options = modes[mode];
-        if (options) {
-            _.extend(cm_default.extraKeys, options.extraKeys);
-            _.extend(edit_shortcuts._shortcuts, options.edit_shortcuts);
-            if (options.custom) options.custom();
+        var mode_mods = mods[mode];
+        if (mode_mods) {
+            if (mode_mods.add)
+                add_bindings(mode_mods.add, mode);
+
+            if (mode_mods.remove)
+                remove_bindings(mode_mods.remove, mode);
+
+            if (mode_mods.custom)
+                mode_mods.custom();
         }
     }
 
+    function change_cells_to_defaults() {
+        Jupyter.notebook.get_cells().map(update_cm_instance_to_defaults);
+    }
+
     function switch_keymap(mode) {
-        // make sure we store previous configs
+        // don't run if selecting currently selected mode
         if (mode == previous_mode) return;
 
-        // save selection in localStorage for later retrieval
+        // store selection in localStorage for later retrieval
         window.localStorage.setItem('jupyter_keymap', mode);
 
-        highlight_selection(mode);
-
         reset_state();
+
         change_defaults(mode);
 
-        // apply changes to existing cells
-        Jupyter.notebook.get_cells().map(update_cm_instance_to_defaults);
+        change_cells_to_defaults();
+
+        highlight_selection(mode);
 
         previous_mode = mode;
     }
