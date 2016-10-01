dd5899daf6db7f2d2376577735637a3f50ed612c
Diego Fernandez <difernan@redhat.com>
2016-10-01 00:04:25 -0400

    Add option to store keymap on server config
    Also made other code cleanup changes

--- src/jupyter_contrib_nbextensions/nbextensions/select_keymap/main.js
+++ src/jupyter_contrib_nbextensions/nbextensions/select_keymap/main.js
@@ -1,14 +1,14 @@
 // change the mode of all current and future CodeMirror instances
 
 define([
-    'base/js/namespace',
-    'base/js/utils',
-    'notebook/js/cell',
-    'services/config',
-    'codemirror/lib/codemirror',
-    'codemirror/keymap/emacs',
-    'codemirror/keymap/vim',
-    'codemirror/keymap/sublime'
+    "base/js/namespace",
+    "base/js/utils",
+    "notebook/js/cell",
+    "services/config",
+    "codemirror/lib/codemirror",
+    "codemirror/keymap/emacs",
+    "codemirror/keymap/vim",
+    "codemirror/keymap/sublime"
 ], function(Jupyter, utils, Cell, configmod, CodeMirror) {
     "use_strict";
 
@@ -109,28 +109,42 @@ define([
     };
 
     var base_url = utils.get_body_data("baseUrl");
-    var config = new configmod.ConfigSection('notebook', {
+    var server_config = new configmod.ConfigSection("notebook", {
         base_url: base_url
     });
 
-    config.load();
+    server_config.load();
 
-    var conf = {
-        line_wrap: true
-    };
-
-    // make sure config is loaded before making changes
-    config.loaded.then(function() {
-        for (var key in conf) {
-            var config_key = "select_keymap_" + key;
-            if (config.data.hasOwnProperty(config_key)) {
-                conf[key] = config.data[config_key];
-            }
-        }
+    // make sure config is loaded before making initial changes
+    server_config.loaded.then(function() {
         save_starting_state();
         // initialize last stored value or default
-        switch_keymap(window.localStorage.getItem("jupyter_keymap") || "default");
+        switch_keymap(get_stored_keymap());
     });
+
+    function get_config(key) {
+        return server_config.data["select_keymap_" + key];
+    }
+
+    function get_stored_keymap() {
+        var keymap;
+        if (get_config("local_storage")) {
+            keymap = window.localStorage.getItem("jupyter_keymap");
+        } else {
+            keymap = server_config.data.stored_keymap;
+        }
+        return keymap || "default";
+    }
+
+    function store_keymap(mode) {
+        if (get_config("local_storage")) {
+            window.localStorage.setItem("jupyter_keymap", mode);
+        } else {
+            server_config.update({
+                stored_keymap: mode
+            });
+        }
+    }
 
     function flatten_shortcuts(shortcut_tree) {
         var result = {};
@@ -165,10 +179,10 @@ define([
     function leave_current_mode(cm) {
         if (cm.state.vim.insertMode) {
             // Move from insert mode into command mode.
-            CodeMirror.keyMap['vim-insert'].call('Esc', cm);
+            CodeMirror.keyMap["vim-insert"].call("Esc", cm);
         } else if (cm.state.vim.visualMode) {
             // Move from visual mode to command mode.
-            CodeMirror.keyMap['vim'].call('Esc', cm);
+            CodeMirror.keyMap["vim"].call("Esc", cm);
         } else {
             // Move to notebook command mode.
             Jupyter.notebook.command_mode();
@@ -190,19 +204,19 @@ define([
 
     function create_menu() {
         var menu = [
-            '<li class="divider"></li>',
-            '<li class="dropdown-submenu">',
-            '<a href="#">Keymaps</a>',
-            '<ul class="dropdown-menu" id="keymap_menu">'
+            "<li class='divider''></li>",
+            "<li class='dropdown-submenu'>",
+            "<a href='#'>Keymaps</a>",
+            "<ul class='dropdown-menu'' id='keymap_menu'>"
         ];
 
         available_keymaps.forEach(function(keymap) {
-            menu.push(`<li><a id="keymap-${keymap}" href="#" title="Toggle ${keymap} keybindings" onClick="switch_keymap('${keymap}')" style="text-transform: capitalize;">${keymap}</a></li>`);
+            menu.push(`<li><a id="keymap-${keymap}" href="#" title="Toggle ${keymap} keybindings" onClick="switch_keymap("${keymap}")" style="text-transform: capitalize;">${keymap}</a></li>`);
         });
 
-        menu.push('</ul></li>');
+        menu.push("</ul></li>");
 
-        $('#move_cell_down').after(menu.join('\n'));
+        $("#move_cell_down").after(menu.join("\n"));
     }
 
     function update_cm_instance_to_defaults(cell) {
@@ -214,8 +228,8 @@ define([
     };
 
     function highlight_selection(mode) {
-        $("[id^=keymap]").css('font-weight', 'normal');
-        $('[id=keymap-' + mode + ']').css('font-weight', 'bold');
+        $("[id^=keymap]").css("font-weight", "normal");
+        $("[id=keymap-" + mode + "]").css("font-weight", "bold");
     }
 
     function reset_state() {
@@ -224,9 +238,12 @@ define([
         cm_default.extraKeys = _.clone(starting_state.extraKeys);
         update_shortcuts(edit_shortcuts, starting_state.edit_shortcuts);
         update_shortcuts(command_shortcuts, starting_state.command_shortcuts);
+
+        // if changing from another mode, run the teardown function
         var prev_mode_mods = mods[previous_mode];
-        if (prev_mode_mods && prev_mode_mods.custom_teardown)
+        if (prev_mode_mods && prev_mode_mods.custom_teardown) {
             prev_mode_mods.custom_teardown();
+        }
     }
 
     function save_starting_state() {
@@ -239,11 +256,13 @@ define([
         _.extend(CodeMirror.keyMap[mode], add.keyMap);
         _.extend(cm_default.extraKeys, add.extraKeys);
 
-        if (add.command_shortcuts)
+        if (add.command_shortcuts) {
             update_shortcuts(command_shortcuts, add.command_shortcuts);
+        }
 
-        if (add.edit_shortcuts)
+        if (add.edit_shortcuts) {
             update_shortcuts(edit_shortcuts, add.edit_shortcuts);
+        }
     }
 
     function remove_bindings(remove, mode) {
@@ -264,22 +283,25 @@ define([
         });
     }
 
-    function change_defaults(mode) {
+    function change_notebook_defaults(mode) {
         // make changes to cm defaults
         cm_default.keyMap = mode;
-        cm_default.vimMode = mode == 'vim';
-        cm_default.lineWrapping = conf.line_wrap;
+        cm_default.vimMode = mode == "vim";
+        cm_default.lineWrapping = get_config('line_wrap');
 
         var mode_mods = mods[mode];
         if (mode_mods) {
-            if (mode_mods.add)
+            if (mode_mods.add) {
                 add_bindings(mode_mods.add, mode);
+            }
 
-            if (mode_mods.remove)
+            if (mode_mods.remove) {
                 remove_bindings(mode_mods.remove, mode);
+            }
 
-            if (mode_mods.custom)
+            if (mode_mods.custom) {
                 mode_mods.custom();
+            }
         }
     }
 
@@ -291,12 +313,12 @@ define([
         // don't run if selecting currently selected mode
         if (mode == previous_mode) return;
 
-        // store selection in localStorage for later retrieval
-        window.localStorage.setItem('jupyter_keymap', mode);
+        // store selection only when it changes
+        if (previous_mode) store_keymap(mode);
 
         reset_state();
 
-        change_defaults(mode);
+        change_notebook_defaults(mode);
 
         change_cells_to_defaults();
 
