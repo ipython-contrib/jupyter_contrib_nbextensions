a6e056da9ef4126692cdf7281007f963694b0079
Juergen Hasch <python@elbonia.de>
2016-10-03 21:16:37 +0200

    Fix html_embed test

--- tests/test_exporters.py
+++ tests/test_exporters.py
@@ -6,13 +6,18 @@ from nbformat import v4, write
 import io
 import os
 
+def path_in_data(rel_path):
+    """Return an absolute path from a relative path in tests/data."""
+    return os.path.join(os.path.dirname(__file__), 'data', rel_path)
+
 class TestNbConvertExporters(TestsBase):
 
     def test_embedhtml(self):
+        """Test exporter for embedding images into HTML"""
         with self.create_temp_cwd():
             nb = v4.new_notebook(cells=[
                 v4.new_code_cell(source="a = 'world'"),
-                v4.new_markdown_cell(source="![test.png](data/test.png)"),
+                v4.new_markdown_cell(source="![testimage]({})".format(path_in_data('icon.png'))),
             ])
             with io.open('notebook2.ipynb', 'w', encoding='utf-8') as f:
                 write(nb, f, 4)
@@ -30,6 +35,7 @@ class TestNbConvertExporters(TestsBase):
             assert statinfo_e.st_size > statinfo.st_size
 
     def test_htmltoc2(self):
+        """Test exporter for adding table of contents"""
         with self.create_temp_cwd():
             nb = v4.new_notebook(cells=[
                 v4.new_code_cell(source="a = 'world'"),
