14e9eadb580c5b543770d3b61ffdbbf9a2226160
Josh Barnes <joshuacookebarnes@gmail.com>
2016-11-24 13:10:51 +0000

    add local conda recipe

---
+++ conda.recipe/post-link.bat
@@ -0,0 +1,6 @@
+@echo off
+:: We redirect stderr & stdout to conda's .messages.txt; for details, see
+::     http://conda.pydata.org/docs/building/build-scripts.html
+(
+  "%PREFIX%\Scripts\jupyter-contrib-nbextension.exe" install --sys-prefix
+) >>"%PREFIX%\.messages.txt" 2>&1
---
+++ conda.recipe/post-link.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# We redirect stderr & stdout to conda's .messages.txt; for details, see
+#     http://conda.pydata.org/docs/building/build-scripts.html
+{
+  "${PREFIX}/bin/jupyter-contrib-nbextension" install --sys-prefix
+} >>"${PREFIX}/.messages.txt" 2>&1
