0be866e0b0d7b5f0ff9fc57f7903a381e1d07f4c
Lyndon White <oxinabox@ucc.asn.au>
2016-11-24 08:15:42 +0000

    =AddBefore

--- src/jupyter_contrib_nbextensions/nbextensions/addbefore/main.js
+++ src/jupyter_contrib_nbextensions/nbextensions/addbefore/main.js
@@ -1,33 +1,30 @@
-// Add rulers to a codecell
-//
-
 define([
     'base/js/namespace',
     'jquery',
     'require',
     'base/js/events',
     'base/js/utils',
-], function(IPython, $, require, events, configmod, utils) {
+], function(Jupyter, $, require, events, configmod, utils) {
     "use strict";
 
-    var load_ipython_extension = function() {
-             IPython.toolbar.add_buttons_group([
+    var load_extension = function() {
+             Jupyter.toolbar.add_buttons_group([
                  {
                       'label'   : 'Insert Cell Above',
                       'icon'    : 'fa-arrow-circle-o-up',
                       'callback': function () {
-                                                    IPython.notebook.insert_cell_above('code');
-                                                    IPython.notebook.select_prev();
-                                                    IPython.notebook.focus_cell();
+                                                    Jupyter.notebook.insert_cell_above('code');
+                                                    Jupyter.notebook.select_prev();
+                                                    Jupyter.notebook.focus_cell();
                       }
                  },
                  {
                       'label'   : 'Insert Cell Below',
                       'icon'    : 'fa-arrow-circle-o-down',
                       'callback': function () {
-                                                    IPython.notebook.insert_cell_below('code');
-                                                    IPython.notebook.select_next();
-                                                    IPython.notebook.focus_cell();
+                                                    Jupyter.notebook.insert_cell_below('code');
+                                                    Jupyter.notebook.select_next();
+                                                    Jupyter.notebook.focus_cell();
                       }
                  }
                  ]);
@@ -37,9 +34,9 @@ define([
 
 
 
-
     var extension = {
-        load_ipython_extension : load_ipython_extension
+        load_jupyter_extension : load_extension,
+        load_ipython_extension : load_extension
     };
     return extension;
 });
