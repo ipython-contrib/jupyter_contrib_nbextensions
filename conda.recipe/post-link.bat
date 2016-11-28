fd9082dea3cef6c1225bc220a3d01831dc8aca66
Josh Barnes <joshuacookebarnes@gmail.com>
2016-11-28 01:53:13 +0000

    update conda recipe to take advantage of new install switches install files as part of build step, so only config is altered in post-link

--- conda.recipe/post-link.bat
+++ conda.recipe/post-link.bat
@@ -2,5 +2,5 @@
 :: We redirect stderr & stdout to conda's .messages.txt; for details, see
 ::     http://conda.pydata.org/docs/building/build-scripts.html
 (
-  "%PREFIX%\Scripts\jupyter-contrib-nbextension.exe" install --sys-prefix
+  "%PREFIX%\Scripts\jupyter-contrib-nbextension.exe" install --only-config --sys-prefix
 ) >>"%PREFIX%\.messages.txt" 2>&1
--- conda.recipe/pre-unlink.sh
+++ conda.recipe/pre-unlink.sh
@@ -2,5 +2,5 @@
 # We redirect stderr & stdout to conda's .messages.txt; for details, see
 # http://conda.pydata.org/docs/building/build-scripts.html
 {
-  "${PREFIX}/bin/jupyter-contrib-nbextension" uninstall --sys-prefix
+  "${PREFIX}/bin/jupyter-contrib-nbextension" uninstall --only-config --sys-prefix
 } >>"${PREFIX}/.messages.txt" 2>&1
