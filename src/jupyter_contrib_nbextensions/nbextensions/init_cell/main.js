a3003f00b21677945eed5d26c40839a7f4084c4b
Josh Barnes <joshuacookebarnes@gmail.com>
2016-12-02 15:34:17 +0000

    [init_cell] add option to prevent running on kernel_ready.Kernel

--- src/jupyter_contrib_nbextensions/nbextensions/init_cell/main.js
+++ src/jupyter_contrib_nbextensions/nbextensions/init_cell/main.js
@@ -3,10 +3,15 @@ define(function (require, exports, module) {
     var $ = require('jquery');
     var events = require('base/js/events');
     var Jupyter = require('base/js/namespace');
+    var utils = require('base/js/utils');
     var CellToolbar = require('notebook/js/celltoolbar').CellToolbar;
     var CodeCell = require('notebook/js/codecell').CodeCell;
+    var ConfigSection = require('services/config').ConfigSection;
 
     var log_prefix = '[' + module.id + ']';
+    var options = { // updated from server's config on loading nbextension
+        run_on_kernel_ready: true,
+    };
 
     var init_cell_ui_callback = CellToolbar.utils.checkbox_ui_generator(
         'Initialisation Cell',
@@ -55,8 +60,27 @@ define(function (require, exports, module) {
         // Register a preset of UI elements forming a cell toolbar.
         CellToolbar.register_preset('Initialisation Cell', ['init_cell.is_init_cell']);
 
-        // whenever a (new) kernel  becomes ready, run all initialization cells
-        events.on('kernel_ready.Kernel', run_init_cells);
+        var base_url = utils.get_body_data('baseUrl');
+        var conf_section = new ConfigSection('notebook', {'base_url': base_url});
+        conf_section.load()
+            .then(
+                function on_success (new_config_data) {
+                    // update options from server config
+                    $.extend(true, options, new_config_data.init_cell);
+                }, function on_error (err) {
+                    console.warn(log_prefix, 'error loading options from config:', err);
+                    console.warn(log_prefix, 'Using default options:', options);
+                })
+            .then(function () {
+                if (options.run_on_kernel_ready) {
+                    if (Jupyter.notebook && Jupyter.notebook.kernel && Jupyter.notebook.kernel.info_reply.status === 'ok') {
+                        // kernel is already ready
+                        run_init_cells();
+                    }
+                    // whenever a (new) kernel  becomes ready, run all initialization cells
+                    events.on('kernel_ready.Kernel', run_init_cells);
+                }
+            });
     };
 
     return {
