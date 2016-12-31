266fc324976010964529824ed742aabdc7fa356a
Josh Barnes <joshuacookebarnes@gmail.com>
2016-12-31 16:50:40 +0000

    [2to3] update 2to3 to use shared library

--- 2to3.js
+++ 2to3.js
@@ -1,137 +1,48 @@
 // Copyright (c) Jupyter-Contrib Team.
 // Distributed under the terms of the Modified BSD License.
-// Author: Erik Wouters
+// Authors: @EWouters, @jfbercher and @jcb91
 // Based on: https://github.com/jfbercher/code_prettify and
-//			 https://gist.github.com/takluyver/c8839593c615bb2f6e80
+//           https://gist.github.com/takluyver/c8839593c615bb2f6e80
 
 define(function(require, exports, module) {
     'use strict';
 
-    var Jupyter = require('base/js/namespace');
-    var keyboard = require('base/js/keyboard');
-    var utils = require('base/js/utils');
-    var configmod = require('services/config');
-    var Cell = require('notebook/js/cell').Cell;
-    var CodeCell = require('notebook/js/codecell').CodeCell;
+    var kernel_exec_on_cell = require('./kernel_exec_on_cell');
 
-    var add_edit_shortcuts = {};
-    var exec_code_verbose = true;
-    var kernelLanguage; // language associated with kernel
+    var mod_name = '2to3';
 
+    // gives default settings
     var cfg = {
-        hotkey: 'Ctrl-Q',
-    }
-
-    var convert_2to3_lib =
-"\
-import lib2to3 \n\
-from lib2to3.refactor import RefactoringTool, get_fixers_from_package \n\
-avail_fixes = set(get_fixers_from_package('lib2to3.fixes')) \n\
-refactoringTool = RefactoringTool(avail_fixes) \n\
-del avail_fixes, get_fixers_from_package, RefactoringTool \n\
-def refactor_cell(src): \n\
-    try: \n\
-        tree = refactoringTool.refactor_string(src+'\\n', '<dummy_name>') \n\
-    except (lib2to3.pgen2.parse.ParseError, \n\
-            lib2to3.pgen2.tokenize.TokenError): \n\
-        return src \n\
-    else: \n\
-        return str(tree)[:-1] \n\
-"
-
-    function initialize() {
-        // create config object to load parameters
-        var base_url = utils.get_body_data("baseUrl");
-        var config = new configmod.ConfigSection('notebook', { base_url: base_url });
-        config.load();
-        config.loaded.then(function config_loaded_callback() {
-            cfg = $.extend(true, cfg, config.data.conv2to3)
-            convert_2to3_hotkey(); //initialize hotkey
-        })
-    }
-
-	function code_exec_callback(msg) {
-		if (msg.msg_type == "error") {
-	        if (exec_code_verbose) alert("CODE 2to3 extension\n Error: " + msg.content.ename + "\n" + msg.content.evalue)
-			return
-		}
-		var ret = msg.content.data['text/plain'];
-		var ret = ret.substr(1, ret.length - 2) //suppress quotes 
-		ret = ret.replace(/([^\\])\\n/g, "$1\n").replace(/([^\\])\\n/g, "$1\n") 
-			.replace(/([^\\])\\\\\\n/g, "$1\\\n") // [continuation line] replace \ at eol -- probably useless in 2to3
-			.replace(/\\'/g, "'") // replace simple quotes
-			.replace(/\\\\/g, "\\") // unescape
-		var selected_cell = Jupyter.notebook.get_selected_cell();
-		selected_cell.set_text(String(ret));
-	}
-
-    function exec_code(code_input) {
-        Jupyter.notebook.kernel.execute(code_input, { iopub: { output: code_exec_callback } }, { silent: false });
-    }
-
-	function convert_2to3(index) {
-		Jupyter.notebook.select(index);
-		var selected_cell = Jupyter.notebook.get_selected_cell();
-		if (selected_cell instanceof CodeCell) {
-			var text = selected_cell.get_text()
-			text = JSON.stringify(text)    
-			var code_input = 'refactor_cell(' + text + ')'
-			exec_code(code_input, index)
-		}
-	}
-
-    function autoConvert() {
-        convert_2to3();
-    }
-
-    function convert_2to3_button() {
-        if ($('#convert_2to3_button').length == 0) {
-            Jupyter.toolbar.add_buttons_group([{
-                'label': 'Convert current cell from Python 2 to 3',
-                'icon': 'fa-space-shuttle',
-                'callback': autoConvert,
-                'id': 'convert_2to3_button'
-            }]);
-        }
-    }
-
-    function convert_2to3_hotkey() {
-        add_edit_shortcuts[cfg['hotkey']] = {
-            help: "Convert current cell from Python 2 to 3",
-            help_index: 'yf',
-            handler: autoConvert
-        };
-    }
-
-    function getKernelInfos() {
-        kernelLanguage = Jupyter.notebook.metadata.kernelspec.language.toLowerCase()
-        if (kernelLanguage != "python") {
-            $('#convert_2to3_button').remove()
-            //alert("Sorry; Convert 2to3 nbextension only works with a Python kernel");
-
-        } else {
-            convert_2to3_button();
-            Jupyter.keyboard_manager.edit_shortcuts.add_shortcuts(add_edit_shortcuts);
-            exec_code(convert_2to3_lib)
-        }
-    }
-
-    function load_notebook_extension() {
-
-        initialize();
-
-        if (typeof Jupyter.notebook.kernel !== "undefined" && Jupyter.notebook.kernel != null) {
-            getKernelInfos();
-        }
-
-        // only if kernel_ready (but kernel may be loaded before)
-        $([Jupyter.events]).on("kernel_ready.Kernel", function() {
-            console.log("convert_2to3: restarting")
-            getKernelInfos();
-        });
-    }
-
-    return {
-        load_ipython_extension: load_notebook_extension
+        add_toolbar_button: true,
+        hotkey: 'Ctrl-M',
+        process_all_hotkey: 'Ctrl-Shift-M',
+        register_hotkey: true,
+        show_alerts_for_errors: true,
+        button_icon: 'fa-space-shuttle',
+        button_label: 'Convert Python 2 to 3',
+        kbd_shortcut_text: 'Convert Python 2 to 3 in' // ' current cell(s)'
     };
+
+    cfg.kernel_config_map = { // map of parameters for supported kernels
+        "python": {
+            "library": [
+                "import lib2to3.refactor, json",
+                "_2to3_refactoring_tool = lib2to3.refactor.RefactoringTool(",
+                "    set(lib2to3.refactor.get_fixers_from_package('lib2to3.fixes')))",
+                "def _2to3_refactor_cell(src):",
+                "    try:",
+                "        tree = _2to3_refactoring_tool.refactor_string(src+'\\n', '<dummy_name>')",
+                "    except (lib2to3.pgen2.parse.ParseError, lib2to3.pgen2.tokenize.TokenError):",
+                "        return src ",
+                "    else:",
+                "        return str(tree)[:-1]",
+            ].join('\n'),
+            "prefix": "print(json.dumps(_2to3_refactor_cell(u",
+            "postfix": ")))"
+        }
+    };
+
+    var plugin = kernel_exec_on_cell.define_plugin_functions(mod_name, cfg);
+    plugin.load_ipython_extension = plugin.initialize_plugin;
+    return plugin;
 });
--- README_2to3.md
+++ README_2to3.md
@@ -3,25 +3,12 @@
 This extension converts python2 code in a notebook's code cell to python3 code. 
 Under the hood, it uses Pythons build in [2to3](https://docs.python.org/3/library/2to3.html) function.
 
-The project was forked from [code_prettify](https://github.com/jfbercher/code_prettify) by [@jfbercher](https://github.com/jfbercher), retaining most of the code.
+The project was forked by @EWouters from [code_prettify](https://github.com/jfbercher/code_prettify) by [@jfbercher](https://github.com/jfbercher), retaining most of the code. It now shares with `code_prettify` a library of functions dedicated to kernel processing of cells text in Jupyter notebooks. 
+
 The 2to3 conversion is based on [2to3_nb.py](https://gist.github.com/takluyver/c8839593c615bb2f6e80) by [@takluyver](https://github.com/takluyver) and [@fperez](https://github.com/fperez).
 
-[//]: # (Possibly it will be extended to use the [futurize](http://python-future.org/automatic_conversion.html) functions so it can convert both ways.)
+Possibly it will be extended to use the [futurize](http://python-future.org/automatic_conversion.html) functions so it can convert both ways.
 
-![](demo.gif)
+![](demo_2to3.gif)
 
-Installation
-------------
-
-If you use [jupyter-contrib-nbextensions](https://github.com/ipython-contrib/jupyter_contrib_nbextensions), proceed as usual. 
-
-Otherwise, you can still install/try the extension from my personal repo, using
-```
-jupyter nbextension install https://github.com/EWouters/2to3_cell/archive/master.zip --user
-jupyter nbextension enable 2to3_cell-master/2to3_cell
-```
-
-To remove
-```
-jupyter nbextension uninstall 2to3_cell-master/2to3_cell
-```
+See `code_prettify`'s [README](README.md) for the internals used by the extension and a description of the options. 
