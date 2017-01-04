1e30869769df0a98746d46d35b2e0476cbb90a95
Jean-François Bercher <jf.bercher@gmail.com>
2017-01-04 18:24:30 +0100

    [code_prettify] use the updated shared library with registred actions

--- code_prettify.js
+++ code_prettify.js
@@ -12,8 +12,10 @@ define(function(require, exports, module) {
     // gives default settings
     var cfg = {
         add_toolbar_button: true,
-        hotkey: 'Ctrl-L',
-        process_all_hotkey: 'Ctrl-Shift-L',
+        hotkeys: {
+            process_selected: 'Ctrl-L',
+            process_all: 'Ctrl-Shift-L',
+        },
         register_hotkey: true,
         show_alerts_for_errors: true,
         button_label: 'Code prettify',
@@ -39,55 +41,9 @@ define(function(require, exports, module) {
             "postfix": ")));"
         }
     };
-    // set default json string, will later be updated from config
-    // before it is parsed into an object
-    cfg.kernel_config_map_json = JSON.stringify(cfg.kernel_config_map);
 
-    // Keyboard shortcuts ---------
-    var assign_hotkeys = function(plugin) {
-
-            var autofmt = function(mod_name, indices) {
-                if (indices === undefined)
-                    return plugin.autoformat_cells()
-                else
-                    return plugin.autoformat_cells(indices)
-            }
-
-
-            var kbd_shortcut_text = plugin.cfg.kbd_shortcut_text;
-            var that = plugin;
-
-            plugin.mod_edit_shortcuts[plugin.cfg.hotkey] = {
-                help: kbd_shortcut_text + ' selected cell(s)',
-                help_index: 'yf',
-                handler: function(evt) { autofmt('code_prettify') },
-            };
-
-            plugin.mod_edit_shortcuts[plugin.cfg.process_all_hotkey] = {
-                help: kbd_shortcut_text + " the whole notebook",
-                help_index: 'yf',
-                handler: function(evt) {
-                    var indices = [],
-                        N = Jupyter.notebook.ncells();
-                    for (var i = 0; i < N; i++) {
-                        indices.push(i);
-                    }
-                    autofmt('code_prettify', indices);
-                },
-            };
-
-            // use modify-all hotkey in either command or edit mode
-            plugin.mod_cmd_shortcuts[plugin.cfg.process_all_hotkey] = plugin.mod_edit_shortcuts[plugin.cfg.process_all_hotkey];
-
-            if (plugin.cfg.register_hotkey) {
-                Jupyter.keyboard_manager.edit_shortcuts.add_shortcuts(plugin.mod_edit_shortcuts);
-                Jupyter.keyboard_manager.command_shortcuts.add_shortcuts(plugin.mod_cmd_shortcuts);
-            }
-
-        }
-
-    var plugin = new kernel_exec_on_cell.define_plugin(mod_name, cfg);
-    assign_hotkeys(plugin);
-    plugin.load_ipython_extension = plugin.initialize_plugin;
-    return plugin;
+ 
+    var prettifier = new kernel_exec_on_cell.define_plugin(mod_name, cfg);
+    prettifier.load_ipython_extension = prettifier.initialize_plugin;
+    return prettifier;
 });
