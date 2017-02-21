c86bdb4b5491888aee89cdb54cf7b59190adfc8e
Josh Barnes <joshuacookebarnes@gmail.com>
2017-02-21 12:06:46 +0000

    [skill] provide the required load_ipython_extension function otherwise notebook throws an error, and the nbextension gets loaded anyway.

---
+++ src/jupyter_contrib_nbextensions/nbextensions/skill/main.js
@@ -0,0 +1,14 @@
+define(function() {
+	"use strict";
+	// jupyter nbextensions must export a load_ipython_extension function to
+	// avoid throwing an error. Also, loading the module should do nothing
+	// unless the function is called, so we wrap requiring the codemirror mode
+	// in the load call.
+	return {
+		load_ipython_extension: function () {
+			require(['./skill'], function () {
+				console.log('[SKILL Syntax] loaded');
+			});
+		}
+	};
+});
