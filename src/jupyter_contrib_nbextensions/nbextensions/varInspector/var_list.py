8dfd493a7935de43f69a84c72da012d0040f9c38
Jean-François Bercher <jf.bercher@gmail.com>
2017-04-04 22:09:48 +0200

    [varInspector] added json requirement / improved code styling

--- src/jupyter_contrib_nbextensions/nbextensions/varInspector/var_list.py
+++ src/jupyter_contrib_nbextensions/nbextensions/varInspector/var_list.py
@@ -1,20 +1,32 @@
 from __future__ import print_function
 from sys import getsizeof
 from IPython.core.magics.namespace import NamespaceMagics
+from IPython import get_ipython
+import json
 _nms = NamespaceMagics()
 _Jupyter = get_ipython()
 _nms.shell = _Jupyter.kernel.shell
-def _trunc(x,L): return x[:L-3]+'...' if len(x)>L else x[:L]
+
+
 def _getsizeof(x):
-   if type(x).__name__ in ['ndarray', 'Series']:  return x.nbytes
-   elif type(x).__name__ == 'DataFrame': return x.memory_usage().sum()
-   else: return getsizeof(x)
+    # return the size of variable x. Amended version of sys.getsizeof
+    # which also supports ndarray, Series and DataFrame
+    if type(x).__name__ in ['ndarray', 'Series']:
+        return x.nbytes
+    elif type(x).__name__ == 'DataFrame':
+        return x.memory_usage().sum()
+    else:
+        return getsizeof(x)
+
 
 def var_dic_list():
-    types_to_exclude = ['module', 'function', 'builtin_function_or_method', 'instance', '_Feature']    
+    types_to_exclude = ['module', 'function', 'builtin_function_or_method',
+                        'instance', '_Feature']
     values = _nms.who_ls()
-    vardic = [{'varName':v, 'varType': type(eval(v)).__name__, 'varSize': _getsizeof(eval(v)), 'varContent': str(eval(v))[:200]} for v in 
-    values if (v not in ['_html', '_nms', 'NamespaceMagics', '_Jupyter']) & (type(eval(v)).__name__ not in types_to_exclude)]   
-    return json.dumps(vardic)  
-     
+    vardic = [{'varName': v, 'varType': type(eval(v)).__name__, 'varSize': _getsizeof(eval(v)), 'varContent': str(eval(v))[:200]} # noqa
+    for v in values if (v not in ['_html', '_nms', 'NamespaceMagics', '_Jupyter']) & (type(eval(v)).__name__ not in types_to_exclude)] # noqa 
+    return json.dumps(vardic)
+
+
+# command to refresh the list of variables
 print(var_dic_list())
