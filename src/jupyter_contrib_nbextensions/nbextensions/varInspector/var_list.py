fa93fdddfebb6d81c5b84565e32ca77675439581
Jean-François Bercher <jf.bercher@gmail.com>
2017-07-08 22:38:48 +0200

    [varInspector] added support for pandas dataframe (again ?)

--- src/jupyter_contrib_nbextensions/nbextensions/varInspector/var_list.py
+++ src/jupyter_contrib_nbextensions/nbextensions/varInspector/var_list.py
@@ -22,7 +22,7 @@ def var_dic_list():
     types_to_exclude = ['module', 'function', 'builtin_function_or_method',
                         'instance', '_Feature', 'type', 'ufunc']
     values = _nms.who_ls()
-    vardic = [{'varName': v, 'varType': type(eval(v)).__name__, 'varSize': _getsizeof(eval(v)), 'varContent': str(eval(v))[:200]} # noqa
+    vardic = [{'varName': v, 'varType': type(eval(v)).__name__, 'varSize': str(_getsizeof(eval(v))), 'varContent': str(eval(v))[:200]} # noqa
     for v in values if (v not in ['_html', '_nms', 'NamespaceMagics', '_Jupyter']) & (type(eval(v)).__name__ not in types_to_exclude)] # noqa 
     return json.dumps(vardic)
 
