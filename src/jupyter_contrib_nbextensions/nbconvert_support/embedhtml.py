b6531c27793c5264465755a982b3167b5d959a5f
Jürgen Hasch <juergen.hasch@elbonia.de>
2017-08-12 16:00:22 +0200

    Fix html_embed exporter

--- src/jupyter_contrib_nbextensions/nbconvert_support/embedhtml.py
+++ src/jupyter_contrib_nbextensions/nbconvert_support/embedhtml.py
@@ -3,6 +3,7 @@
 import base64
 import re
 from nbconvert.exporters.html import HTMLExporter
+from ipython_genutils.ipstruct import Struct
 
 try:
     from urllib.request import urlopen  # py3
@@ -31,6 +32,17 @@ class EmbedHTMLExporter(HTMLExporter):
         elif url.startswith('data'):
             img = '<img src="' + url + '"'
             return img
+        elif url.startswith('attachment'):
+            imgname = url.split(':')[1]
+            available_formats = self.attachments[imgname]
+            # get the image based on the configured image type priority
+            for imgformat in self.config.NbConvertBase.display_data_priority:
+                if imgformat in available_formats.keys():
+                    b64_data = self.attachments[imgname][imgformat]
+                    img = '<img src="data:' + imgformat + \
+                          ';base64,' + b64_data + '"'
+                    return img
+            raise ValueError('Could not find attachment for image "%s" in notebook' % imgname)
         else:
             with open(url, 'rb') as f:
                 data = f.read()
@@ -52,6 +64,10 @@ class EmbedHTMLExporter(HTMLExporter):
         output, resources = super(
             EmbedHTMLExporter, self).from_notebook_node(nb, resources)
 
+        self.attachments = Struct()
+        for cell in nb.cells:
+            if 'attachments' in cell.keys():
+                self.attachments += cell['attachments']
         regex = re.compile('<img\s+src="([^"]+)"')
 
         embedded_output = regex.sub(self.replfunc, output)
