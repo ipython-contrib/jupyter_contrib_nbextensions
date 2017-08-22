b5322b75c3c12c0583d8cfc325b2bc7e3fa75962
Colton R Bukowsky <colton.bukowsky@gmail.com>
2017-08-21 17:51:30 -0700

    Fixed some typos and PEP8 lint flags

--- src/jupyter_contrib_nbextensions/nbconvert_support/embedhtml.py
+++ src/jupyter_contrib_nbextensions/nbconvert_support/embedhtml.py
@@ -1,8 +1,7 @@
 """Embed graphics into HTML Exporter class"""
 
 import base64
-import re
-import lxml.etree as ET
+import lxml.etree as et
 from nbconvert.exporters.html import HTMLExporter
 from ipython_genutils.ipstruct import Struct
 import os
@@ -31,7 +30,7 @@ class EmbedHTMLExporter(HTMLExporter):
         imgformat = url.split('.')[-1]
         
         if url.startswith('data'):
-            return #Already in base64 Format
+            return  # Already in base64 Format
         
         self.log.info("try embedding url: %s, format: %s" % (url, imgformat))
 
@@ -53,12 +52,11 @@ class EmbedHTMLExporter(HTMLExporter):
             with open(filename, 'rb') as f:
                 data = f.read()
 
-        
         b64_data = base64.b64encode(data).decode("utf-8")
         if imgformat == "svg":
             prefix = "data:image/svg+xml;base64,"
         elif imgformat == "pdf":
-            prefix= "data:application/pdf;base64,"
+            prefix = "data:application/pdf;base64,"
         else:
             prefix = "data:image/" + imgformat + ';base64,'
 
@@ -70,20 +68,20 @@ class EmbedHTMLExporter(HTMLExporter):
 
         self.path = resources['metadata']['path']
         
-        #Get attachements
+        # Get attachments
         self.attachments = Struct()
         for cell in nb.cells:
             if 'attachments' in cell.keys():
                 self.attachments += cell['attachments']
 
         # Parse HTML and replace <img> tags with the embedded data
-        parser = ET.HTMLParser()
-        root = ET.fromstring(output, parser = parser)
+        parser = et.HTMLParser()
+        root = et.fromstring(output, parser=parser)
         nodes = root.findall(".//img")
         for n in nodes:
             self.replfunc(n)
         
         # Convert back to HTML
-        embedded_output = ET.tostring(root, method="html")
+        embedded_output = et.tostring(root, method="html")
 
         return embedded_output, resources
