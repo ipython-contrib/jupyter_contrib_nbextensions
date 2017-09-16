0d68281d476965e6bbb2648cef1e5c2f0e09f542
juhasch <juergen.hasch@gmail.com>
2017-09-16 18:14:35 +0200

    Add image resizing option

--- src/jupyter_contrib_nbextensions/nbconvert_support/pre_embedimages.py
+++ src/jupyter_contrib_nbextensions/nbconvert_support/pre_embedimages.py
@@ -1,7 +1,7 @@
 """Nbconvert preprocessor for the python-markdown nbextension."""
 
 from nbconvert.preprocessors import Preprocessor
-from traitlets import Bool, Float
+from traitlets import Bool, Float, Unicode
 import re
 import os
 import base64
@@ -38,17 +38,23 @@ class EmbedImagesPreprocessor(Preprocessor):
     to additionally embeds all images referenced by an url (e.g. http://jupyter.org/assets/nav_logo.svg) instead
     of a local file name. Also
 
-        EmbedImagesPreprocessor.dpi_scaling
+        EmbedImagesPreprocessor.resize=small
 
-    Let's you scale the size of an image. This is interesting if you want to save space by not embedding large
+    Let's you scale-down the size of an image. This is useful if you want to save space by not embedding large
     images and instead use a smaller (scaled) version. Works only for raster images (i.e. png, jpg).
+    Valid resize settings are: small = 500px, mid = 1000px, large = 2000px for maximum size in length or width
+    No upscaling of small images will be performed.
+
+    Example::
+
+            $ jupyter nbconvert --to html --EmbedImagesPreprocessor.embed_images=True --EmbedImagesPreprocessor.scale=large mynotebook.ipynb
 
     *Note:* To embed images after conversion to HTML you can also use the `html_embed` exporter
     """
 
     embed_images = Bool(False, help="Embed images as attachment").tag(config=True)
     embed_remote_images = Bool(False, help="Embed images referenced by an url as attachment").tag(config=True)
-    dpi_scaling = Float(0, help="Resize images to a certain DPI number (reduce size)").tag(config=True)
+    resize = Unicode('', help="Resize images to save space (reduce size)").tag(config=True)
 
     def preprocess(self, nb, resources):
         """Skip preprocessor if not enabled"""
@@ -59,7 +65,7 @@ class EmbedImagesPreprocessor(Preprocessor):
     def replfunc_md(self, match):
         """Read image and store as base64 encoded attachment"""
         url = match.group(2)
-        imgformat = url.split('.')[-1]
+        imgformat = url.split('.')[-1].lower()
         if url.startswith('http'):
             if self.embed_remote_images:
                 data = urlopen(url).read()
@@ -71,6 +77,30 @@ class EmbedImagesPreprocessor(Preprocessor):
             filename = os.path.join(self.path, url)
             with open(filename, 'rb') as f:
                 data = f.read()
+
+        # resize settings: small -> 500px, mid -> 1000px, large -> 200px
+        imgsizes = {'small': 500, 'mid': 1000, 'large': 2000}
+        if self.resize in imgsizes.keys()  and imgformat in ['png', 'jpg']:
+            from io import BytesIO
+            try:
+                from PIL import Image
+            except ImportError:
+                self.log.info("Pillow library not available to resize images")
+                Image = None
+            if Image:
+                # Only make images smaller when rescaling
+                im = Image.open(BytesIO(data))
+                size = im.size
+                factor = imgsizes[self.resize]/max(size)
+                if factor < 1.0:
+                    newsize = ( int(size[0]*factor), int(size[1]*factor))
+                    newim = im.resize(newsize)
+                    fp = BytesIO()
+                    newim.save(fp, format=imgformat.replace('jpg', 'jpeg')) # PIL requires JPEG instead of JPG
+                    data = fp.getvalue()
+                    fp.close()
+                    self.log.debug("Resized %d x %d image %s to size %d x %d pixels" %
+                                  (size[0], size[1], url, newsize[0], newsize[1]) )
 
         self.log.debug("embedding url: %s, format: %s" % (url, imgformat))
         b64_data = base64.b64encode(data).decode("utf-8")
--- tests/test_preprocessors.py
+++ tests/test_preprocessors.py
@@ -92,7 +92,7 @@ def test_preprocessor_svg2pdf():
               'exported pdf should be referenced in exported notebook')
 
 
-def test_preprocessor_embedigmages():
+def test_preprocessor_embedimages():
     """Test python embedimages preprocessor."""
     # check import shortcut
     from jupyter_contrib_nbextensions.nbconvert_support import EmbedImagesPreprocessor # noqa E501
@@ -109,3 +109,53 @@ def test_preprocessor_embedigmages():
 
     expected = 'image/png'
     assert_in(expected, body, 'Attachment {} is missing'.format(expected))
+
+def test_preprocessor_embedimages_resize():
+    """Test python embedimages preprocessor."""
+    # check import shortcut
+    from jupyter_contrib_nbextensions.nbconvert_support import EmbedImagesPreprocessor # noqa E501
+
+    try:
+        from PIL import Image
+    except ImportError:
+        raise SkipTest('PIL not found')
+
+    notebook_node = nbf.new_notebook(cells=[
+        nbf.new_code_cell(source="a = 'world'"),
+        nbf.new_markdown_cell(
+            source="![testimage]({})".format(path_in_data('large_image.png'))
+        ),
+    ])
+    body, resources = export_through_preprocessor(
+        notebook_node, EmbedImagesPreprocessor, NotebookExporter, 'ipynb')
+    len_noembed = len(body)
+
+    customconfig = Config(EmbedImagesPreprocessor={'embed_images': True, 'resize': 'small'})
+    body, resources = export_through_preprocessor(
+        notebook_node, EmbedImagesPreprocessor, NotebookExporter, 'ipynb',
+        customconfig)
+    len_small = len(body)
+
+    customconfig = Config(EmbedImagesPreprocessor={'embed_images': True, 'resize': 'mid'})
+    body, resources = export_through_preprocessor(
+        notebook_node, EmbedImagesPreprocessor, NotebookExporter, 'ipynb',
+        customconfig)
+    len_mid = len(body)
+
+    customconfig = Config(EmbedImagesPreprocessor={'embed_images': True, 'resize': 'large'})
+    body, resources = export_through_preprocessor(
+        notebook_node, EmbedImagesPreprocessor, NotebookExporter, 'ipynb',
+        customconfig)
+    len_large = len(body)
+
+
+    customconfig = Config(EmbedImagesPreprocessor={'embed_images': True})
+    body, resources = export_through_preprocessor(
+        notebook_node, EmbedImagesPreprocessor, NotebookExporter, 'ipynb',
+        customconfig)
+    len_noresize = len(body)
+
+    assert(len_noembed < len_small)
+    assert(len_small < len_mid)
+    assert(len_mid < len_large)
+    assert(len_large < len_noresize)
