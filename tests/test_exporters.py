3279e77a80d96e9355616cadbbcccce9439cd036
Gabriel Nuetzi <gnuetzi@gmail.com>
2017-09-24 00:05:20 +0200

    rewrote tests, -> check_html with functor

--- tests/test_exporters.py
+++ tests/test_exporters.py
@@ -7,7 +7,7 @@ from functools import wraps
 
 from nbconvert.tests.base import TestsBase
 from nbformat import v4, write
-
+from lxml import etree as et
 
 def path_in_data(rel_path):
     """Return an absolute path from a relative path in tests/data."""
@@ -21,44 +21,23 @@ def _with_tmp_cwd(func):
             return func(self, *args, **kwargs)
     return func_wrapper
 
-
-def _filesize_without_cr(name):
-    """Calculate file size without additional CR (Windows) """
-    with io.open(name, 'r', encoding='utf-8') as f:
-	print(data.encode("utf-8"))
-        data = f.read().replace('\r', '')
-    size = len(data)
-    return size
-
-
 class TestNbConvertExporters(TestsBase):
 
-    def check_stuff_gets_embedded(self, nb, exporter_name, to_be_included=[]):
+    def check_html(self, nb, exporter_name, check_func):
         nb_basename = 'notebook'
         nb_src_filename = nb_basename + '.ipynb'
         with io.open(nb_src_filename, 'w', encoding='utf-8') as f:
             write(nb, f, 4)
 
-        # convert with default exporter
-        (stdout, stderr) = self.nbconvert('--to {} "{}"'.format('html', nb_src_filename))
-        print(stdout)
-        nb_dst_filename = nb_basename + '.html'
-        assert os.path.isfile(nb_dst_filename)
-        filesize = _filesize_without_cr(nb_dst_filename)
-        os.remove(nb_dst_filename)
-
         # convert with embedding exporter
-        (stdout, stderr) = self.nbconvert('--to {} "{}"'.format(exporter_name, nb_src_filename))
-        print(stdout)
-        filesize_e = _filesize_without_cr(nb_dst_filename)
-        assert os.path.isfile(nb_dst_filename)
-        assert filesize_e > filesize
+        nb_dst_filename = nb_basename + '.html'
+        self.nbconvert('--to {} "{}"'.format(exporter_name, nb_src_filename))
 
-        with io.open(nb_dst_filename, 'r', encoding='utf-8') as f:
+        with open(nb_dst_filename, 'rb') as f:
             embedded_nb = f.read()
-
-        for txt in to_be_included:
-            assert txt in embedded_nb
+            parser = et.HTMLParser()
+            root = et.fromstring(embedded_nb, parser=parser)
+            check_func(byte_string=embedded_nb, root_node=root)
 
     @_with_tmp_cwd
     def test_embedhtml(self):
@@ -69,8 +48,14 @@ class TestNbConvertExporters(TestsBase):
                 source="![testimage]({})".format(path_in_data('icon.png'))
             ),
         ])
-        self.check_stuff_gets_embedded(
-            nb, 'html_embed', to_be_included=['base64'])
+
+        def check(byte_string, root_node):
+            nodes = root_node.findall(".//img")
+            for n in nodes:
+                url = n.attrib["src"]
+                assert url.startswith('data')
+
+        self.check_html(nb, 'html_embed', check_func=check)
 
     @_with_tmp_cwd
     def test_htmltoc2(self):
@@ -79,8 +64,11 @@ class TestNbConvertExporters(TestsBase):
             v4.new_code_cell(source="a = 'world'"),
             v4.new_markdown_cell(source="# Heading"),
         ])
-        self.check_stuff_gets_embedded(
-            nb, 'html_toc', to_be_included=['toc2'])
+
+        def check(byte_string, root_node):
+            assert b'toc2' in byte_string
+
+        self.check_html(nb, 'html_toc', check_func=check)
 
     @_with_tmp_cwd
     def test_html_collapsible_headings(self):
@@ -93,5 +81,8 @@ class TestNbConvertExporters(TestsBase):
             v4.new_markdown_cell(source=('### level 3 heading')),
             v4.new_code_cell(source='a = range(1,10)'),
         ])
-        self.check_stuff_gets_embedded(
-            nb, 'html_ch', to_be_included=['collapsible_headings'])
+
+        def check(byte_string, root_node):
+            assert b'collapsible_headings' in byte_string
+
+        self.check_html(nb, 'html_ch', check_func=check)
