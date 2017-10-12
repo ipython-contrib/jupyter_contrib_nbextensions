4a1279bfffdd00a32b242259a562cbb9c1f9acff
Josh Barnes <joshuacookebarnes@gmail.com>
2017-10-12 01:52:38 +0100

    [python-markdown] allow whitespace before/after wrapping <p> tags

--- src/jupyter_contrib_nbextensions/nbextensions/python-markdown/main.js
+++ src/jupyter_contrib_nbextensions/nbextensions/python-markdown/main.js
@@ -63,7 +63,7 @@ define([
                             html = marked(text);
                         } else if (out_data.msg_type === "stream") {
                             html = marked(out_data.content.text);
-                            var t = html.match(/^<p>([\s\S]*?)<\/p>$/); //strip <p> and </p> that marked (maybe) adds and we don't want
+                            var t = html.match(/^\s*<p>([\s\S]*?)<\/p>\s*$/); //strip <p> and </p> that marked (maybe) adds and we don't want
                             html = t !== null ? t[1] : html;
                             var q = html.match(/^&#39;([\s\S]*?)&#39;$/); // strip quotes from strings
                             if (q !== null) html = q[1]
@@ -90,7 +90,7 @@ define([
                                 } else {
                                     html = marked(ul['text/plain']);
                                     // [\s\S] is used to also catch newlines
-                                    var t = html.match(/^<p>([\s\S]*?)<\/p>$/); //strip <p> and </p> that marked adds and we don't want
+                                    var t = html.match(/^\s*<p>([\s\S]*?)<\/p>\s*$/); //strip <p> and </p> that marked adds and we don't want
                                     html = t !== null ? t[1] : html;
                                     var q = html.match(/^&#39;([\s\S]*?)&#39;$/); // strip quotes from strings
                                     if (q !== null) html = q[1]
