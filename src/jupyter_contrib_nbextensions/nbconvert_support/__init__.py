f966b38ad9bbfb276626045be23c91ce9eb46c96
Jürgen Hasch <juergen.hasch@elbonia.de>
2018-02-25 17:06:53 +0100

    Remove double entry

--- src/jupyter_contrib_nbextensions/nbconvert_support/__init__.py
+++ src/jupyter_contrib_nbextensions/nbconvert_support/__init__.py
@@ -19,7 +19,6 @@ __all__ = [
     'templates_directory',
     'CodeFoldingPreprocessor',
     'EmbedHTMLExporter',
-    'EmbedHTMLExporter',
     'EmbedSlidesExporter',
     'ExecuteTimePreprocessor',
     'ExporterCollapsibleHeadings',
