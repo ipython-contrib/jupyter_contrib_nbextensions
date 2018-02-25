859bb0372ed5ad9e961575b70568d5d907252235
Jürgen Hasch <juergen.hasch@elbonia.de>
2018-02-25 10:34:11 +0100

    Fix slides_embed

--- src/jupyter_contrib_nbextensions/nbconvert_support/exporter_embedslides.py
+++ src/jupyter_contrib_nbextensions/nbconvert_support/exporter_embedslides.py
@@ -1,7 +1,9 @@
 """Embed graphics into Slides Exporter class"""
 
 from nbconvert.exporters.slides import SlidesExporter
-from .exporter_embedhtml import EmbedImages
+from nbconvert.exporters.html import Config
+from .exporter_embedhtml import EmbedImages, MakeAttachmentsUnique
+from .pre_embedimages import EmbedImagesPreprocessor
 
 
 class EmbedSlidesExporter(SlidesExporter, EmbedImages):
@@ -20,4 +22,32 @@ class EmbedSlidesExporter(SlidesExporter, EmbedImages):
             EmbedSlidesExporter, self).from_notebook_node(nb, resources)
 
         embedded_output = self.embed_images_into_notebook(output, resources)
+        return embedded_output, resources
+
+    @property
+    def default_preprocessors(self):
+        return super(EmbedSlidesExporter, self).default_preprocessors + \
+               [EmbedImagesPreprocessor, MakeAttachmentsUnique]
+
+    @property
+    def default_config(self):
+        c = Config({
+            'EmbedImagesPreprocessor': {
+                'enabled': True,
+                'embed_images': True
+            },
+            'MakeAttachmentsUnique': {
+                'enabled': True
+            }
+        })
+        c.merge(super(EmbedSlidesExporter, self).default_config)
+        return c
+
+    def from_notebook_node(self, nb, resources=None, **kw):
+
+        output, resources = super(
+            EmbedSlidesExporter, self).from_notebook_node(nb, resources)
+
+        embedded_output = self.embed_images_into_notebook(output, resources)
+
         return embedded_output, resources
