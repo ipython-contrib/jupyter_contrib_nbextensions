a8ca63011cea68988f0eeb4b2d960ec8b88a118c
noklam <channoklam@yahoo.com.hk>
2018-12-30 19:27:08 +0800

    add follow running cell, tested shortcut works

--- src/jupyter_contrib_nbextensions/nbextensions/go_to_current_running_cell/main.js
+++ src/jupyter_contrib_nbextensions/nbextensions/go_to_current_running_cell/main.js
@@ -1,14 +1,13 @@
 // Copyright (c) Jupyter-Contrib Team.
 // Distributed under the terms of the Modified BSD License.
 
-// This is a quick (and dirty) extension - move up or down several selected cells
-// Dirty because it would be better to act on dom elements and write a correct 
-// move_cells() function.
-// Updated to Jupyter 4.2+, taking advantage of 
-// `Jupyter.notebook.move_selection_{down, up}` new functions
+// This is an extension allows you to jump to the current running cell.
+//  You can also activate this functionality automatically,
+//  i.e., your view is always scolling to the current cell.
+
 //
-// Keyboard shortcuts: Alt-up and Alt-down (works with single cells also -- this is useful!)
-// Cells can be selected using the rubberband (needs rubberband extension) or via Shift-up/Shift-down or Shift-K/Shift-J
+// Keyboard shortcuts: Alt-I and Alt-down (works with single cells also -- this is useful!)
+
 
 
 define([
@@ -16,68 +15,54 @@ define([
     'jquery',
     'require',
     'base/js/events'
-], function(Jupyter, $, requirejs, events, rubberband) {
+], function (Jupyter, $, requirejs, events) {
     "use strict";
 
-    if (parseFloat(Jupyter.version.substr(0, 3)) >= 4.2) {
-        var add_cmd_shortcuts = {
-            'Alt-down': {
-                help: 'Move selected cells down',
-                help_index: 'ht',
-                handler: function() { Jupyter.notebook.move_selection_down() }
-            },
-            'Alt-up': {
-                help: 'Move selected cells up',
-                help_index: 'ht',
-                handler: function() { Jupyter.notebook.move_selection_up() }
+    function scrollIntoRunningCell(evt, data) {
+        $('.running')[0].scrollIntoView({behavior: 'smooth', inline: 'center'});
+    }
+    
+    // Go to Running cell shortcut
+    var go_to_running_cell = {
+        'Alt-I': {
+            help: 'Go to first executing cell',
+            help_index: 'zz',
+            handler: function (event) {
+                setTimeout(function () {
+                    // Find running cell and click the first one
+                    if ($('.running').length > 0) {
+                        //alert("found running cell");
+                        $('.running')[0].scrollIntoView();
+                    }
+                }, 250);
+                return false;
             }
-        }
+        },
 
-    } else { // Jupyter version < 4.2
-        var add_cmd_shortcuts = {
-            'Alt-down': {
-                help: 'Move selected cells down',
-                help_index: 'ht',
-                handler: function(event) {
-                    var ncells = Jupyter.notebook.ncells();
-                    var s = Jupyter.notebook.get_selected_indices();
-                    //ensure cells indices are reverse sorted
-                    var ss = s.sort(function(x, y) {
-                        return x - y }).reverse();
-                    if (ss[0] + 1 < ncells) {
-                        for (var k in ss) {
-                            Jupyter.notebook.move_cell_down(ss[k]);
-                        }; //The second loop is needed because move_cell deselect
-                        for (var k in ss) {
-                            Jupyter.notebook.get_cell(ss[k] + 1).select();
-                        }
-                    }
-                }
-            },
-            'Alt-up': {
-                help: 'Move selected cells up',
-                help_index: 'ht',
-                handler: function(event) {
-                    var s = Jupyter.notebook.get_selected_indices();
-                    //ensure cells indices are sorted 
-                    var ss = s.sort(function(x, y) {
-                        return x - y });
-                    if (ss[0] - 1 >= 0) {
-                        for (var k in ss) {
-                            Jupyter.notebook.move_cell_up(ss[k]);
-                        };
-                        for (var k in ss) {
-                            Jupyter.notebook.get_cell(ss[k] - 1).select();
-                        }
-                    }
-                }
+        'Meta-[': {
+            help: 'Follow Executing Cell On',
+            help_index: 'zz',
+            handler: function (event) {
+                Jupyter.notebook.events.on('finished_execute.CodeCell', scrollIntoRunningCell);
+                //console.log("Follow Executing Cell On")
+                return false;
+            }
+        },
+
+        'Meta-]': {
+            help: 'Follow Executing Cell Off',
+            help_index: 'zz',
+            handler: function (event) {
+                Jupyter.notebook.events.off('finished_execute.CodeCell', scrollIntoRunningCell);
+                //console.log("Follow Executing Cell Off")
+                return false;
             }
         }
     }
 
     function load_ipython_extension() {
-        Jupyter.keyboard_manager.command_shortcuts.add_shortcuts(add_cmd_shortcuts);
-        console.log("[move_selected_cells] loaded")
+        Jupyter.keyboard_manager.command_shortcuts.add_shortcuts(go_to_running_cell);
+        console.log("[go to current running cell] loaded")
     }
 
     return {
