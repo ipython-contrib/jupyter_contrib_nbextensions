5222445478c3c98cf5c056e2448e7975b8f6d1f8
Joshua Zeltser <joshua.zeltser@gmail.com>
2019-07-04 11:42:56 +0100

    created basic planner popup with close button

--- src/jupyter_contrib_nbextensions/nbextensions/accessibility_toolbar/main.js
+++ src/jupyter_contrib_nbextensions/nbextensions/accessibility_toolbar/main.js
@@ -8,6 +8,45 @@ define([
 ], function(Jupyter, $, requirejs, events, utils) {
     "use strict";
 
+
+
+    var Planner = function (nb) {
+        var planner = this;
+        this.notebook = nb;
+        this.kernel = nb.kernel;
+        this.km = nb.keyboard_manager;
+        this.open = false;
+
+        this.element = $("<div id='nbextension-planner'>");
+
+        this.close_button = $("<i>").addClass('fa fa-close');
+        this.close_button.click(function() {
+           planner.close_planner();
+        });
+
+        this.element.append(this.close_button);
+
+        $("body").append(this.element);
+    }
+
+    Planner.prototype.open_planner = function () {
+        this.open = true;
+        var site_height = $("#site").height();
+        this.element.animate({
+            height: site_height,
+        }, 200);
+        this.element.show();
+    }
+
+    Planner.prototype.close_planner = function () {
+        this.open = false;
+        this.element.animate({
+            height: 0,
+        }, 100);
+    }
+
+
+
     var load_ipython_extension = function() {
              Jupyter.toolbar.add_buttons_group([
                  Jupyter.keyboard_manager.actions.register ({
@@ -38,7 +77,14 @@ define([
                     'help'   : 'Planner',
                     'icon'   : 'fas fa-sticky-note',
                     'handler': function () {
-                           //TODO     
+
+                        var link = document.createElement("link");
+                        link.type = "text/css";
+                        link.rel = "stylesheet";
+                        link.href = requirejs.toUrl("./planner.css");
+                        document.getElementsByTagName("head")[0].appendChild(link);
+                        new Planner(Jupyter.notebook).open_planner();
+                        console.log('new planner')
                     }
                  }, 'planner', 'toolbar'),
 
