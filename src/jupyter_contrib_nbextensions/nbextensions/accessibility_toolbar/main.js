874537b7f7527950d395166b478cf5fe2559e56c
tao <45403704+taohan16@users.noreply.github.com>
2019-07-08 14:14:10 +0100

    Update main.js

--- src/jupyter_contrib_nbextensions/nbextensions/accessibility_toolbar/main.js
+++ src/jupyter_contrib_nbextensions/nbextensions/accessibility_toolbar/main.js
@@ -12,11 +12,6 @@ define([
     var load_ipython_extension = function() {
         //spell-checker inital
         var s=new SPC();
-        // var boostrap_toggle="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css";
-        // var boostrap_toggle_js="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js";
-        // s.spc_css_initial("../../nbextensions/accessibility_toolbar/spellchecker.css");
-        // s.spc_css_initial(boostrap_toggle);
-        // s.spc_js_initail(boostrap_toggle_js);
         Jupyter.toolbar.add_buttons_group([
             Jupyter.keyboard_manager.actions.register ({
                 'help'   : 'Customise font',
