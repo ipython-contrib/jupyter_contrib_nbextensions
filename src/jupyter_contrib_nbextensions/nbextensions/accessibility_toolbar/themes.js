99b14a6ba9568792bd724c49877fa6265f081b83
Joshua Zeltser <joshua.zeltser@gmail.com>
2019-07-10 11:37:48 +0100

    added formatting changes

--- src/jupyter_contrib_nbextensions/nbextensions/accessibility_toolbar/themes.js
+++ src/jupyter_contrib_nbextensions/nbextensions/accessibility_toolbar/themes.js
@@ -1,44 +1,47 @@
-define([
-    'base/js/namespace',
-    'jquery',
-    'require',
-], function(Jupyter, $, requirejs) {
-    "use strict";
+define(["base/js/namespace", "jquery", "require"], function(
+  Jupyter,
+  $,
+  requirejs
+) {
+  "use strict";
 
-    var ThemeObj = function() {};
+  var ThemeObj = function() {};
 
-    ThemeObj.prototype.createThemeMenu = function () {
-       var themeButton = $("[title='Custom themes']");
-       var themediv = $("<div>",{"style":"display:inline","class":"btn-group"});
-       themeButton.parent().append(themediv);
-       themediv.append(themeButton)
-       themeButton.addClass('dropdown-toggle').attr("data-toggle","dropdown");
+  ThemeObj.prototype.createThemeMenu = function() {
+    var themeButton = $("[title='Custom themes']");
+    var themediv = $("<div>", { style: "display:inline", class: "btn-group" });
+    themeButton.parent().append(themediv);
+    themediv.append(themeButton);
+    themeButton.addClass("dropdown-toggle").attr("data-toggle", "dropdown");
 
-       var dropDownMenu = $('<ul/>').addClass('dropdown-menu');
-       var menuItem1 = $('<li/>');
-       var label1 = $('<label/>');
-       label1.text("Dark mode")
+    var dropDownMenu = $("<ul/>").addClass("dropdown-menu");
+    var menuItem1 = $("<li/>");
+    var label1 = $("<label/>");
+    label1.text("Dark mode");
 
-       var switchToggle = $('<input/>').attr("type", "checkbox").attr("data-toggle", "toggle");
-       label1.append(switchToggle);
-       menuItem1.append(label1);
-       dropDownMenu.append(menuItem1);
+    var switchToggle = $("<input/>")
+      .attr("type", "checkbox")
+      .attr("data-toggle", "toggle");
+    label1.append(switchToggle);
+    menuItem1.append(label1);
+    dropDownMenu.append(menuItem1);
 
-       var line_break = $('<br/>');
-       dropDownMenu.append(line_break);
+    var line_break = $("<br/>");
+    dropDownMenu.append(line_break);
 
-       var menuItem2 = $('<li/>');
-       var label2 = $('<label/>');
-       label2.text( "High Contrast");
-       var switchToggle2 = $('<input/>').attr("type", "checkbox").attr("data-toggle", "toggle");
+    var menuItem2 = $("<li/>");
+    var label2 = $("<label/>");
+    label2.text("High Contrast");
+    var switchToggle2 = $("<input/>")
+      .attr("type", "checkbox")
+      .attr("data-toggle", "toggle");
 
-       label2.append(switchToggle2)
-       menuItem2.append(label2);
-       dropDownMenu.append(menuItem2);
+    label2.append(switchToggle2);
+    menuItem2.append(label2);
+    dropDownMenu.append(menuItem2);
 
-       themeButton.parent().append(dropDownMenu);
-       console.log("Menu created");
-    };
-
-    return ThemeObj;
+    themeButton.parent().append(dropDownMenu);
+    console.log("Menu created");
+  };
+  return ThemeObj;
 });
