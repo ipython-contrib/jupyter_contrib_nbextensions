30bb3b873203451dd3ca9e42bafb748cc272513e
ednut15 <babatundeadeola@gmail.com>
2019-07-18 17:03:10 +0100

    Reducing size of functions

--- src/jupyter_contrib_nbextensions/nbextensions/accessibility_toolbar/font_style/font_spacing.js
+++ src/jupyter_contrib_nbextensions/nbextensions/accessibility_toolbar/font_style/font_spacing.js
@@ -39,7 +39,14 @@ define(["base/js/namespace", "jquery", "base/js/utils", "require"], function(
         1
       );
       fs_style = style_file.cssRules;
+      that.reduce_line_height();
+      that.increase_line_height();
+      that.reduce_letter_space();
+      that.increase_letter_space();
     }
+  };
+  Font_spacing.prototype.reduce_line_height = function() {
+    var that = this;
     $("#reduce_line_height").click(function() {
       var current_lh = parseInt(
         $(".cell")
@@ -59,6 +66,9 @@ define(["base/js/namespace", "jquery", "base/js/utils", "require"], function(
         return false;
       }
     });
+  };
+  Font_spacing.prototype.increase_line_height = function() {
+    var that = this;
     $("#increase_line_height").click(function() {
       var current_lh = parseInt(
         $(".cell")
@@ -78,13 +88,16 @@ define(["base/js/namespace", "jquery", "base/js/utils", "require"], function(
         return false;
       }
     });
+  };
+  Font_spacing.prototype.reduce_letter_space = function() {
+    var that = this;
     $("#reduce_letter_space").click(function() {
       var current = parseInt(
         $(".cell")
           .css("letter-spacing")
           .replace(/[^\d.-]/g, "")
       );
-      that.set_letter_spacing(current_lh - 2 + "px");
+      that.set_letter_spacing(current - 2 + "px");
       if (current - 2 == that.min_ls) {
         $(this).attr("disabled", true);
         return false;
@@ -97,6 +110,9 @@ define(["base/js/namespace", "jquery", "base/js/utils", "require"], function(
         return false;
       }
     });
+  };
+  Font_spacing.prototype.increase_letter_space = function() {
+    var that = this;
     $("#increase_letter_space").click(function() {
       var current = parseInt(
         $(".cell")
@@ -116,30 +132,31 @@ define(["base/js/namespace", "jquery", "base/js/utils", "require"], function(
         return false;
       }
     });
-
-    Font_spacing.prototype.set_line_height = function(size) {
-      for (var i = 0; i < fs_style.length; i++) {
-        if (/line\-height/.test(fs_style[i].cssText)) {
-          var index = i;
-        }
+  };
+  Font_spacing.prototype.set_line_height = function(size) {
+    var that = this;
+    for (var i = 0; i < fs_style.length; i++) {
+      if (/line\-height/.test(fs_style[i].cssText)) {
+        var index = i;
       }
-      style_file.deleteRule(index);
-      style_file.insertRule(
-        ".cell, .text_cell_render, .CodeMirror-code, .CodeMirror-line{ line-height:" +
-          size +
-          "; }",
-        1
-      );
-    };
-    Font_spacing.prototype.set_letter_spacing = function(size) {
-      for (var i = 0; i < fs_style.length; i++) {
-        if (/letter\-spacing/.test(fs_style[i].cssText)) {
-          var index = i;
-        }
+    }
+    style_file.deleteRule(index);
+    style_file.insertRule(
+      ".cell, .text_cell_render, .CodeMirror-code, .CodeMirror-line{ line-height:" +
+        size +
+        "; }",
+      1
+    );
+  };
+  Font_spacing.prototype.set_letter_spacing = function(size) {
+    var that = this;
+    for (var i = 0; i < fs_style.length; i++) {
+      if (/letter\-spacing/.test(fs_style[i].cssText)) {
+        var index = i;
       }
-      style_file.deleteRule(index);
-      style_file.insertRule(".cell{ letter-spacing:" + size + "; }", 0);
-    };
+    }
+    style_file.deleteRule(index);
+    style_file.insertRule(".cell{ letter-spacing:" + size + "; }", 0);
   };
   return Font_spacing;
 });
