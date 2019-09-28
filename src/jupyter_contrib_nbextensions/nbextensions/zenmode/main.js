5b7a49f21a809fd9b90cd227e505c9a3a1c64998
Lakkam Sai Krishna Reddy <lakkam.saikrishnareddy@gmail.com>
2019-09-28 17:03:37 +0530

    Zenmode: Updated main.js, fixed indentation, name.

--- src/jupyter_contrib_nbextensions/nbextensions/zenmode/main.js
+++ src/jupyter_contrib_nbextensions/nbextensions/zenmode/main.js
@@ -1,6 +1,6 @@
 /**
 * ----------------------------------------------------------------------------
-* Copyright (c) 2013 - DamiÃ¡n Avila
+* Copyright (c) 2013 - Damián Avila
 * Copyright (c) 2015 - Joshua Cooke Barnes (jcb91)
 *
 * Distributed under the terms of the Modified BSD License.
@@ -62,12 +62,12 @@ define([
 
 
             // Remove zenmode css settings only when changes were made.
-	    if (backgrounds.length != 0) {
-		$('body').css({
-		    'background-image': 'none'
-		})
-	    }
-	    
+            if (backgrounds.length != 0) {
+                $('body').css({
+                    'background-image': 'none'
+                })
+            }
+            
             // This should be changed at some point in the future to preserve non-zenmode visibility settings
             $(menu_pattern).toggle(true);
             $(header_pattern).toggle(true);
@@ -88,20 +88,20 @@ define([
                 background = requirejs.toUrl("./images/" + background);
             }
 
-	    // Apply zenmode css when there are images to be used.
-	    if (backgrounds.length != 0) {
-		$('body').css({
-		    'background-image': 'url(' + background + ')',
-		    'background-repeat': 'no-repeat',
-		    'background-position': 'center center',
-		    'background-attachment': 'fixed',
-		    '-webkit-background-size': 'cover',
-		    '-moz-background-size': 'cover',
-		    '-o-background-size': 'cover',
-		    'background-size': 'cover'
-		});
-	    }
-	    
+            // Apply zenmode css when there are images to be used.
+            if (backgrounds.length != 0) {
+                $('body').css({
+                    'background-image': 'url(' + background + ')',
+                    'background-repeat': 'no-repeat',
+                    'background-position': 'center center',
+                    'background-attachment': 'fixed',
+                    '-webkit-background-size': 'cover',
+                    '-moz-background-size': 'cover',
+                    '-o-background-size': 'cover',
+                    'background-size': 'cover'
+                });
+            }
+
             if (hide_menubar)
                 {$(menu_pattern).toggle(false);}
             if (hide_header)
