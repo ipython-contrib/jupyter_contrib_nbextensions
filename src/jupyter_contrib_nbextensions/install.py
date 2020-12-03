cb0d5f3e9ba36fc3662ad97aa223132f5ad354da
Marcos Duarte <duartexyz@gmail.com>
2020-12-02 23:57:28 -0300

    Update install.py
    Drop support for python < 3.6

--- src/jupyter_contrib_nbextensions/install.py
+++ src/jupyter_contrib_nbextensions/install.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 """API to install/remove all jupyter_contrib_nbextensions."""
 
-from __future__ import (
-    absolute_import, division, print_function, unicode_literals,
-)
-
 import errno
 import os
 
